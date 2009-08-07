cc5735dbec02b185bcede17f5db5c4b2a99c3a8e
David Mytton <david@mytton.net>
2009-08-07 10:27:29 +0100

    Moved logging output to just 1 line

--- agent.py
+++ agent.py
@@ -84,17 +84,13 @@ class agent(Daemon):
 		import platform
 		systemStats = {'machine': platform.machine(), 'platform': sys.platform, 'processor': platform.processor(), 'pythonV': platform.python_version()}
 		
-		agentLogger.debug('Python version: ' + platform.python_version())
-		
-		if sys.platform == 'linux2':
-			agentLogger.debug('Platform: linux2 / ' + str(platform.dist()))
-			
+		if sys.platform == 'linux2':			
 			systemStats['nixV'] = platform.dist()
 			
 		elif sys.platform == 'darwin':
-			agentLogger.debug('Platform: darwin / ' + str(platform.mac_ver()))
-		
 			systemStats['macV'] = platform.mac_ver()
+		
+		agentLogger.debug('System: ' + str(systemStats))
 			
 		agentLogger.debug('Creating checks instance')
 		
