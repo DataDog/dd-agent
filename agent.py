f3b9adb98fea423dd6f1956f26fc383a65d32f50
rduffield <duffield@gmail.com>
2010-06-03 10:03:58 -0400

    Updated agent version

--- agent.py
+++ agent.py
@@ -14,7 +14,7 @@ agentConfig = {}
 agentConfig['debugMode'] = 0
 agentConfig['checkFreq'] = 60
 
-agentConfig['version'] = '1.6.1'
+agentConfig['version'] = '1.7.0'
 
 # Core modules
 import ConfigParser
