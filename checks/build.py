c42884f2d8bb6f6828305aae833ca1d148028c2d
Carlo Cabanilla <carlo@datadoghq.com>
2010-11-04 10:44:30 -0400

    clean up the code a bit

--- checks/build.py
+++ checks/build.py
@@ -7,6 +7,9 @@ from collections import defaultdict
 from datetime import datetime
 from glob import glob
 
+class Continue(Exception):
+	pass
+
 class Hudson(object):
 	datetime_format = '%Y-%m-%d_%H-%M-%S'
 	result_pattern = re.compile('\s*<result>(?P<result>[A-Za-z]*)</result>\s*')
@@ -14,70 +17,67 @@ class Hudson(object):
 	def __init__(self):
 		self.high_watermarks = defaultdict(lambda: 0)
 	
-	def _extract_timestamp(self, dir_name):
+	def _extract_timestamp(self, job_name, dir_name):
 		try:
+			# Parse the timestamp from the directory name
 			date_str = os.path.basename(dir_name)
 			time_tuple = time.strptime(date_str, self.datetime_format)
 			timestamp = time.mktime(time_tuple)
 		except ValueError:
-			return None
+			raise Continue("Skipping non-timestamp dir: {0}".format(dir_name))
 		else:
-			return timestamp
+			# Check if it's a build we've seen already
+			if timestamp <= self.high_watermarks[job_name]:
+				raise Continue("Skipping old build: {0} at {1}".format(job_name, timestamp))
+			else:
+				return timestamp
 	
-	def _is_new_build(self, job_name, timestamp):
-		return timestamp > self.high_watermarks[job_name]
-	
-	def _get_build_metadata(self, dir_name):
+	def _get_result(self, dir_name):
+		# Read the build.xml metadata file that Hudson generates
 		build_metadata = os.path.join(dir_name, 'build.xml')
 		
 		if not os.access(build_metadata, os.R_OK):
-			return None
+			raise Continue("Can't read build file at {0}".format(build_metadata))
 		else:
-			return build_metadata
+			# Lazy xml parsing. Since each element is on a newline,
+			# run a regexp on each line to find the result.
+			matches = (
+				self.result_pattern.match(line) 
+				for line in open(build_metadata)
+			)
 	
-	def _get_result(self, build_metadata):
-		matches = (
-			self.result_pattern.match(line) 
-			for line in open(build_metadata)
-		)
-	
-		matched = [match for match in matches if match]
+			matched = [match for match in matches if match]
 
-		if len(matched) != 1:
-			result = None
-		else:
-			result = matched[0].group('result')
+			if len(matched) != 1:
+				result = None
+			else:
+				result = matched[0].group('result')
 		
-		return result
+			return result
 	
 	def _update_high_watermark(self, job_name, timestamp):
 		self.high_watermarks[job_name] = max(timestamp, self.high_watermarks[job_name])
 
-	def _get_build_results(self, job_dir):
+	def _get_build_results(self, logger, job_dir):
 		job_name = os.path.basename(job_dir)
 		
 		for dir_name in glob(os.path.join(job_dir, 'builds', '*')):
-			timestamp = self._extract_timestamp(dir_name)
-			
-			if not timestamp:
-				continue
-			
-			if not self._is_new_build(job_name, timestamp):
-				continue
 
-			build_metadata = self._get_build_metadata(dir_name)
-			
-			if not build_metadata:
-				continue
+			try:
+				timestamp = self._extract_timestamp(job_name, dir_name)
+				result = self._get_result(dir_name)
+				self._update_high_watermark(job_name, timestamp)
 
-			result = self._get_result(build_metadata)
-			
-			if not result:
-				continue
-			
-			self._update_high_watermark(job_name, timestamp)
-
-			yield timestamp, job_name, result	
+			except Continue, e:
+				logger.debug(str(e))
+				
+			except Exception:
+				# Catchall so that the agent loop doesn't die
+				# if there are unexpected errors.
+				logger.error(traceback.format_exc())
+				
+			else:
+				yield job_name, timestamp, result	
 			
 					
 	def check(self, logger, agentConfig):
@@ -89,15 +89,19 @@ class Hudson(object):
 		job_dirs = glob(os.path.join(hudson_home, 'jobs', '*'))
 		
 		for job_dir in job_dirs:
-			for output in self._get_build_results(job_dir):
+			for output in self._get_build_results(logger, job_dir):
 				print output
 
 				
 
 
 if __name__ == '__main__':
+	import logging
+	logger = logging.getLogger('hudson')
+	logger.setLevel(logging.INFO)
+	logger.addHandler(logging.StreamHandler())
 	hudson = Hudson()
 	while True:
-		hudson.check(None, {'hudson_home': '/Users/hudson/.hudson/'})
+		hudson.check(logger, {'hudson_home': '/Users/hudson/.hudson/'})
 		time.sleep(1)
 		
