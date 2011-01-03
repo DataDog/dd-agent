9596e79812629561a72bca0258492396debfd8c5
alq <alq666@gmail.com>
2011-01-03 14:50:56 -0500

    Left a mention to old config behind

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(name='datadog-agent',
       version='1.9.0',
       description='Datatadog monitoring agent',
       author='Datadog',
-      author_email='datadog@datadoghq.com',
+      author_email='info@datadoghq.com',
       url='http://www.datadoghq.com/',
       packages=['checks'],
       scripts=['agent.py', 'daemon.py', 'minjson.py'],
-      data_files=[('/etc/dd-agent/', ['config.cfg.example'])]
+      data_files=[('/etc/dd-agent/', ['datadog.conf.example'])]
      )
