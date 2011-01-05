2b09a2b02f1c91582ded95d2ebbb37fc5033f7c2
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2011-01-05 15:32:41 -0500

    hudson: send build number and build duration

--- checks/build.py
+++ checks/build.py
@@ -7,6 +7,7 @@ import traceback
 from collections import defaultdict
 from datetime import datetime
 from glob import glob
+from xml.etree.ElementTree import ElementTree
 
 class Continue(Exception):
     pass
@@ -14,7 +15,6 @@ class Continue(Exception):
 class Hudson(object):
     key = 'Hudson'
     datetime_format = '%Y-%m-%d_%H-%M-%S'
-    result_pattern = re.compile('\s*<result>(?P<result>[A-Za-z]*)</result>\s*')
     
     def __init__(self):
         self.high_watermarks = None
@@ -34,64 +34,60 @@ class Hudson(object):
             else:
                 return timestamp
     
-    def _get_result(self, dir_name):
+    def _get_build_metadata(self, dir_name):
         # Read the build.xml metadata file that Hudson generates
         build_metadata = os.path.join(dir_name, 'build.xml')
         
         if not os.access(build_metadata, os.R_OK):
             raise Continue("Can't read build file at {0}".format(build_metadata))
         else:
-            # Lazy xml parsing. Since each element is on a newline,
-            # run a regexp on each line to find the result.
-            matches = (
-                self.result_pattern.match(line) 
-                for line in open(build_metadata)
-            )
+            tree = ElementTree()
+            tree.parse(build_metadata)
             
-            matched = [match for match in matches if match]
-
-            if len(matched) != 1:
-                result = None
-            else:
-                result = matched[0].group('result')
-        
-            return result
+            keys = ['result', 'number', 'duration']
+            
+            kv_pairs = ((k, tree.find(k)) for k in keys)
+            d = dict([(k, v.text) 
+                        for k, v in kv_pairs 
+                        if v is not None])
+            return d
     
     def _update_high_watermark(self, job_name, timestamp):
         self.high_watermarks[job_name] = max(timestamp, self.high_watermarks[job_name])
-
+    
     def _get_build_results(self, logger, job_dir):
         job_name = os.path.basename(job_dir)
         
         for dir_name in glob(os.path.join(job_dir, 'builds', '*')):
-
+            
             try:
                 timestamp = self._extract_timestamp(job_name, dir_name)
-                result = self._get_result(dir_name)
+                build_metadata = self._get_build_metadata(dir_name)
                 self._update_high_watermark(job_name, timestamp)
-
+            
             except Continue, e:
                 logger.debug(str(e))
-                
+            
             except Exception:
                 # Catchall so that the agent loop doesn't die
                 # if there are unexpected errors.
                 logger.error(traceback.format_exc())
-                
-            else:
-                yield {
-                    'job_name': job_name,
-                    'timestamp': timestamp,
-                    'result': result,
-                    'event_type': 'build result'
-                }
             
-                    
+            else:
+                output = {
+                    'job_name':     job_name,
+                    'timestamp':    timestamp,
+                    'event_type':   'build result'
+                }
+                output.update(build_metadata)
+                yield output
+    
+    
     def check(self, logger, agentConfig):
         if self.high_watermarks is None:
             # On the first run of check(), prime the high_watermarks dict
             # so that we only send events that occured after the agent
-            # started. 
+            # started.
             # (Setting high_watermarks in the next statement prevents
             #  any kind of infinite loop (assuming nothing ever sets
             #  high_watermarks to None again!))
@@ -123,16 +119,21 @@ class Hudson(object):
         
         return build_events
 
-                
+
 
 
 if __name__ == '__main__':
     import logging
+    import sys
+    
+    hudson_home, apiKey = sys.argv[1:3]
+    
     logger = logging.getLogger('hudson')
     logger.setLevel(logging.INFO)
     logger.addHandler(logging.StreamHandler())
     hudson = Hudson()
     while True:
-        hudson.check(logger, {'hudson_home': '/Users/hudson/.hudson/'})
+        print hudson.check(logger, {'hudson_home': hudson_home,
+                              'apiKey':      apiKey})
         time.sleep(1)
         
