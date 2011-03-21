3c9b841179f70ef7b7b33db281a9db6bae06b96c
Fabrice Ollivier <fabrice@datadoghq.com>
2011-03-21 23:13:23 +0100

    * Nicer API * ActiveMQ metrics

--- checks/jmx.py
+++ checks/jmx.py
@@ -30,23 +30,35 @@ class JmxConnector:
         self._jmx.sendline("domain " + domain)
         self._wait_prompt() 
 
+    def set_bean(self,bean):
+        self._jmx.sendline("bean " + bean)
+        self._wait_prompt()
+
     def list_beans(self):
         self._jmx.sendline("beans")
         self._wait_prompt()
         return self._jmx.before.replace('\r','').split('\n')
 
-    def get_attribute(self,name,type,attribute,domain = None):
-        if name is not None and type is not None:
-            cmd = "name=%s,type=%s" % (name,type)
-        elif name is None:
-            cmd = "type=%s" % type
-        else:
-            cmd = "name=%s" % name
+    def get_attribute(self,attribute,**keywords):
+        cmd = None
+        domain = keywords.get("domain",None)
+        for key in keywords:
+            if key != "domain":
+                val = key + "=" + keywords[key]
+                if cmd is None:
+                    cmd = [val]
+                else:
+                    cmd.append(val)
+
+        if cmd is not None:
+            cmd = ",".join(cmd)
 
         if domain is not None:
             self._jmx.sendline("get -d %s -b %s %s" % (domain, cmd, attribute))
-        else:
+        elif cmd is not None:
             self._jmx.sendline("get -b %s %s" % (cmd, attribute))
+        else:
+            self._jmx.sendline("get %s" % attribute)
 
         self._jmx.expect("%s = (.*);" % attribute)
         values = self._jmx.match.group(1)
@@ -90,9 +102,10 @@ class JmxConnector:
                 ret[mname] = value
 
         self.set_domain("java.lang")
-        _append_to_metric('jvm.thread_count',self.get_attribute(None,"Threading","ThreadCount"))
-        _append_to_metric('jvm.heap_memory',self.get_attribute(None,"Memory","HeapMemoryUsage"))
-        _append_to_metric('jvm.non_heap_memory',self.get_attribute(None,"Memory","NonHeapMemoryUsage"))
+        _append_to_metric('jvm.thread_count',self.get_attribute("ThreadCount",type="Threading"))
+        _append_to_metric('jvm.heap_memory',self.get_attribute("HeapMemoryUsage",type="Memory"))
+        _append_to_metric('jvm.non_heap_memory',self.get_attribute("NonHeapMemoryUsage",
+            type="Memory"))
 
         return ret
 
@@ -104,7 +117,7 @@ class Jvm(Check):
         self.jmx = JmxConnector()
 
     def _store_metric(self,kind,mname,jvm_name,val):
-        name = jvm_name + "." + mname
+        name = jvm_name + "," + mname
         if kind == "gauge":
             if not self.is_gauge(name):
                 self.gauge(name)
@@ -139,26 +152,28 @@ class Tomcat(Jvm):
     def _get_service_stat(self,name):
 
         #Thread pool
+        self.jmx.set_bean("name=%s,type=ThreadPool" % name)
         self._store_metric("gauge","tomcat.threads.max",name,
-            self.jmx.get_attribute(name,"ThreadPool","maxThreads"))
+            self.jmx.get_attribute("maxThreads"))
         self._store_metric("gauge","tomcat.threads.count",name,
-            self.jmx.get_attribute(name,"ThreadPool","currentThreadCount"))
+            self.jmx.get_attribute("currentThreadCount"))
         self._store_metric("gauge","tomcat.threads.busy",name,
-            self.jmx.get_attribute(name,"ThreadPool","currentThreadsBusy"))
+            self.jmx.get_attribute("currentThreadsBusy"))
 
         # Global request processor
+        self.jmx.set_bean("name=%s,type=GlobalRequestProcessor" % name)
         self._store_metric("counter","tomcat.bytes_sent",name,
-            self.jmx.get_attribute(name,"GlobalRequestProcessor","bytesSent"))
+            self.jmx.get_attribute("bytesSent"))
         self._store_metric("counter","tomcat.bytes_rcvd",name,
-            self.jmx.get_attribute(name,"GlobalRequestProcessor","bytesReceived"))        
+            self.jmx.get_attribute("bytesReceived"))        
         self._store_metric("counter","tomcat.processing_time",name,
-            self.jmx.get_attribute(name,"GlobalRequestProcessor","processingTime"))
+            self.jmx.get_attribute("processingTime"))
         self._store_metric("counter","tomcat.error_count",name,
-             self.jmx.get_attribute(name,"GlobalRequestProcessor","errorCount"))
+             self.jmx.get_attribute("errorCount"))
         self._store_metric("counter","tomcat.request_count",name,
-            self.jmx.get_attribute(name,"GlobalRequestProcessor","requestCount"))        
+            self.jmx.get_attribute("requestCount"))        
         self._store_metric("gauge","tomcat.request_count",name,
-            self.jmx.get_attribute(name,"GlobalRequestProcessor","maxTime"))
+            self.jmx.get_attribute("maxTime"))
 
     def get_stats(self):
         self.jmx.set_domain("Catalina")
@@ -179,6 +194,62 @@ class Tomcat(Jvm):
         return self.get_samples()
         
 
+class ActiveMQ(Jvm):
+
+    broker_re = re.compile(r"org.apache.activemq:BrokerName=(.*),Destination=(.*),Type=Queue")
+
+    def _get_queue_stat(self, bean, broker, queue):
+        self.jmx.set_bean(bean)
+        name = "%s:%s" % (broker, queue)
+        #gauge
+        self._store_metric("gauge","activemq.avg_enqueue_time",name,
+            self.jmx.get_attribute("AverageEnqueueTime"))
+        self._store_metric("gauge","activemq.consumer_count",name,
+            self.jmx.get_attribute("ConsumerCount"))
+        self._store_metric("gauge","activemq.producer_count",name,
+            self.jmx.get_attribute("ProducerCount"))
+        self._store_metric("gauge","activemq.max_enqueue_time",name,
+            self.jmx.get_attribute("MaxEnqueueTime"))
+        self._store_metric("gauge","activemq.min_enqueue_time",name,
+            self.jmx.get_attribute("MinEnqueueTime"))
+        self._store_metric("gauge","activemq.memory_percent_usage",name,
+            self.jmx.get_attribute("MemoryPercentUsage"))
+        self._store_metric("gauge","activemq.queue_size",name,
+            self.jmx.get_attribute("QueueSize"))
+
+        #counter
+        self._store_metric("counter","activemq.dequeue_count",name,
+            self.jmx.get_attribute("DequeueCount"))
+        self._store_metric("counter","activemq.dispatch_count",name,
+            self.jmx.get_attribute("DispatchCount"))
+        self._store_metric("counter","activemq.enqueue_count",name,
+            self.jmx.get_attribute("EnqueueCount"))
+        self._store_metric("counter","activemq.expired_count",name,
+            self.jmx.get_attribute("ExpiredCount"))
+        self._store_metric("counter","activemq.in_flight_count",name,
+            self.jmx.get_attribute("InFlightCount"))
+
+    def get_stats(self):
+        self.jmx.set_domain("org.apache.activemq")
+        beans = self.jmx.match_beans("Type=Queue")
+        for bean in beans:
+            m = self.broker_re.match(bean)
+            if m is not None:
+                broker, queue = m.groups()
+                self._get_queue_stat(bean, broker, queue)
+
+    def check(self, agentConfig):
+
+        try:
+            self._check_jvm('activemq',agentConfig,'ActiveMQServer',
+                'ActiveMQUser','ActiveMQPassword')
+            self.get_stats()
+        except Exception, e:
+            self.logger.exception('Error while fetching ActiveMQ metrics: %s' % e)
+
+        return self.get_samples()
+
+
 if __name__ == "__main__":
     
     import logging
@@ -186,6 +257,10 @@ if __name__ == "__main__":
     #jvm = Jvm(logging)
     #print jvm.check({'JVMServer': "localhost:8090", 'JVMName': "tomcat"})
 
-    tomcat = Tomcat(logging)
-    print tomcat.check({'TomcatServer': 'localhost:8090'})
-    print tomcat.check({'TomcatServer': 'localhost:8090'})
+    #tomcat = Tomcat(logging)
+    #print tomcat.check({'TomcatServer': 'localhost:8090'})
+    #print tomcat.check({'TomcatServer': 'localhost:8090'})
+
+    a = ActiveMQ(logging)
+    print a.check({'ActiveMQServer': '5684'})
+    print a.check({'ActiveMQServer': '5684'})
