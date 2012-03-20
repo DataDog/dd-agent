cc310572d5b565156815539f47dd35b9358f5793
Matt Singleton <matt@datadoghq.com>
2012-03-20 16:10:52 -0400

    launchd config

--- packaging/datadog-agent/osx/setup_agent.sh
+++ packaging/datadog-agent/osx/setup_agent.sh
@@ -24,11 +24,22 @@ mkdir -p $dd_base/agent
 curl -L -o $dd_base/agent.tar.gz https://github.com/DataDog/dd-agent/tarball/master
 tar -xz -C $dd_base/agent --strip-components 1 -f $dd_base/agent.tar.gz
 sed "s/api_key:.*/api_key: $1/" $dd_base/agent/datadog.conf.example > $dd_base/agent/datadog.conf
+chmod +x $dd_base/agent
 
 # set up supervisor
 mkdir -p $dd_base/supervisord/logs
 pip install supervisor
 cp $dd_base/agent/packaging/datadog-agent/osx/supervisord.conf $dd_base/supervisord/supervisord.conf
+
+# prepare launchd
+mkdir -p $dd_base/launchd/logs
+touch $dd_base/launchd/logs/launchd.log
+sed "s|AGENT_BASE|$dd_base|; s|USER_NAME|$(whoami)|" $dd_base/agent/packaging/datadog-agent/osx/com.datadoghq.Agent.plist.example > $dd_base/launchd/com.datadoghq.Agent.plist
+
+# consolidate logging
+mkdir -p $dd_base/logs
+ln -s $dd_base/supervisord/logs $dd_base/logs/supervisord
+ln -s $dd_base/launchd/logs $dd_base/logs/launchd
 
 # clean up
 rm $dd_base/virtualenv.py
@@ -48,6 +59,12 @@ To start the agent up again after killing this script, run:
 
     cd $dd_base
     sh bin/agent
+
+To make it permanent, run:
+
+    mkdir -p ~/Library/LaunchAgents
+    cp $dd_base/launchd/com.datadoghq.Agent.plist ~/Library/LaunchAgents/.
+    launchctl load -w ~/Library/LaunchAgents/com.datadoghq.Agent.plist
 
 Here we go!
 
