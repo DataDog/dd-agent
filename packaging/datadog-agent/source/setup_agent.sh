cd45ad1811514c7821e9c093f45898b78085e9c5
Matt Singleton <matthew.j.singleton@gmail.com>
2012-03-27 14:37:20 -0400

    make source install script platform independent

--- packaging/datadog-agent/source/setup_agent.sh
+++ packaging/datadog-agent/source/setup_agent.sh
@@ -6,6 +6,7 @@ if [ $# -ne 1 ]; then
 fi
 
 apikey=$1
+unamestr=`uname`
 
 # create home base for the agent
 dd_base=$HOME/.datadog-agent
@@ -14,7 +15,7 @@ mkdir -p $dd_base
 # set up a virtual env
 curl -L -o $dd_base/virtualenv.py https://raw.github.com/pypa/virtualenv/master/virtualenv.py
 python $dd_base/virtualenv.py --python=python2.6 $dd_base/venv
-source $dd_base/venv/bin/activate
+. $dd_base/venv/bin/activate
 
 # install dependencies
 pip install tornado
@@ -25,23 +26,27 @@ curl -L -o $dd_base/agent.tar.gz https://github.com/DataDog/dd-agent/tarball/mas
 tar -xz -C $dd_base/agent --strip-components 1 -f $dd_base/agent.tar.gz
 sed "s/api_key:.*/api_key: $1/" $dd_base/agent/datadog.conf.example > $dd_base/agent/datadog.conf
 mkdir -p $dd_base/bin
-cp $dd_base/agent/packaging/datadog-agent/osx/agent $dd_base/bin/agent
+cp $dd_base/agent/packaging/datadog-agent/source/agent $dd_base/bin/agent
 chmod +x $dd_base/bin/agent
 
 # set up supervisor
 mkdir -p $dd_base/supervisord/logs
 pip install supervisor
-cp $dd_base/agent/packaging/datadog-agent/osx/supervisord.conf $dd_base/supervisord/supervisord.conf
+cp $dd_base/agent/packaging/datadog-agent/source/supervisord.conf $dd_base/supervisord/supervisord.conf
 
-# prepare launchd
-mkdir -p $dd_base/launchd/logs
-touch $dd_base/launchd/logs/launchd.log
-sed "s|AGENT_BASE|$dd_base|; s|USER_NAME|$(whoami)|" $dd_base/agent/packaging/datadog-agent/osx/com.datadoghq.Agent.plist.example > $dd_base/launchd/com.datadoghq.Agent.plist
+if [ "$unamestr" = "Darwin" ]; then
+    # prepare launchd
+    mkdir -p $dd_base/launchd/logs
+    touch $dd_base/launchd/logs/launchd.log
+    sed "s|AGENT_BASE|$dd_base|; s|USER_NAME|$(whoami)|" $dd_base/agent/packaging/datadog-agent/osx/com.datadoghq.Agent.plist.example > $dd_base/launchd/com.datadoghq.Agent.plist
+fi
 
 # consolidate logging
 mkdir -p $dd_base/logs
 ln -s $dd_base/supervisord/logs $dd_base/logs/supervisord
-ln -s $dd_base/launchd/logs $dd_base/logs/launchd
+if [ "$unamestr" = "Darwin" ]; then
+    ln -s $dd_base/launchd/logs $dd_base/logs/launchd
+fi
 
 # clean up
 rm $dd_base/virtualenv.py
@@ -49,6 +54,7 @@ rm $dd_base/virtualenv.pyc
 rm $dd_base/agent.tar.gz
 
 # print instructions
+if [ "$unamestr" = "Darwin" ]; then
 echo "
 
 We're about to start up the agent for the first time. Once it's running,
@@ -71,6 +77,26 @@ To make it permanent, run:
 Here we go!
 
 "
+fi
+
+if [ "$unamestr" = "Linux" ]; then
+echo "
+
+We're about to start up the agent for the first time. Once it's running,
+you can stop it with 'ctrl-c'. You should start seeing metrics within
+a few seconds at:
+
+    https://app.datadoghq.com/dash/host_name/`hostname`
+
+To start the agent up again after killing this script, run:
+
+    cd $dd_base
+    sh bin/agent
+
+Here we go!
+
+"
+fi
 
 # run agent
 cd $dd_base
