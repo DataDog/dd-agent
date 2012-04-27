e984274fc505c6b8f72b1d2151dd2c7e0c274c34
Alexis Lê-Quôc <alq@datadoghq.com>
2012-04-27 14:26:57 -0700

    Don't error out when we reply 202 to the agent's POST.

--- emitter.py
+++ emitter.py
@@ -10,17 +10,9 @@ from util import json, headers
 
 
 def format_body(message, logger):
-
-    logger.debug('http_emitter: json convert')
     payload = json.dumps(message)
-
-    logger.debug('http_emitter: json converted, hash')
-    #logger.debug('http_emitter:\n%s' % pp(message))
-
     payloadHash = md5(payload).hexdigest()
     postBackData = urllib.urlencode({'payload' : payload, 'hash' : payloadHash})
-
-    logger.debug('http_emitter: hashed')
     return postBackData
 
 def http_emitter(message, logger, agentConfig):
@@ -29,31 +21,21 @@ def http_emitter(message, logger, agentConfig):
 
         # Post back the data
         postBackData = format_body(message, logger)
-
-        logger.debug('http_emitter: attempting postback: ' + agentConfig['ddUrl'])
+        logger.debug('http_emitter: attempting postback to ' + agentConfig['ddUrl'])
         
         # Build the request handler
         request = urllib2.Request(agentConfig['ddUrl'] + '/intake/', postBackData, headers(agentConfig))
         
         # Do the request, log any errors
         response = urllib2.urlopen(request)
-        
         logger.debug('http_emitter: postback response: ' + str(response.read()))
             
     except urllib2.HTTPError, e:
-        logger.error('http_emitter: HTTPError = ' + str(e))
-        
+        if e.code != 202:
+            logger.exception('http_emitter: HTTPError = ' + str(e))
     except urllib2.URLError, e:
-        logger.error('http_emitter: URLError = ' + str(e))
-        
-    except httplib.HTTPException, e: # Added for case #26701
-        logger.error('http_emitter: HTTPException')
-            
-    except Exception, e:
-        import traceback
-        logger.error('http_emitter: Exception = ' + traceback.format_exc())
-
-    else:
-        logger.debug('http_emitter: completed')
-
-
+        logger.exception('http_emitter: URLError = ' + str(e))
+    except httplib.HTTPException, e:
+        logger.exception('http_emitter: HTTPException = ' + str(e))
+    except:
+        logger.exception('http_emitter')
