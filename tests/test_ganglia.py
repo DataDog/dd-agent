83f0db7d455e773ad4c1d0e0e9a0633c401847c9
Alexis Lê-Quôc <alq@datadoghq.com>
2012-05-24 11:55:49 -0400

    Simple cleanup.

--- tests/test_ganglia.py
+++ tests/test_ganglia.py
@@ -9,15 +9,15 @@ except ImportError:
 import pstats
 import tempfile
 from hashlib import md5
+from util import json
 
 from checks.ganglia import Ganglia
 
 TEST_FN = "tests/ganglia.txt"
 
 class TestGanglia(unittest.TestCase):
-    def testSpeed(self, size_in_bytes=10000):
-        """Pretend to be gmetad and serve a large piece of content
-        """
+    def testSpeed(self):
+        # Pretend to be gmetad and serve a large piece of content
         server = subprocess.Popen("nc -l 8651 < %s" % TEST_FN, shell=True)
 
         pfile = tempfile.NamedTemporaryFile()
