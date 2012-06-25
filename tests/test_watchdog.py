d18020d82d4fbfad3285492e9660ac08d3ced024
Alexis Lê-Quôc <alq@datadoghq.com>
2012-06-24 21:36:44 -0400

    New watchdog for collector & forwarder.
    Both are using signals for their watchdogs, instead of a separate thread.
    This makes the code simpler. test_watchdog.py has been improved to include
    tornado tests.

--- tests/test_watchdog.py
+++ tests/test_watchdog.py
@@ -1,22 +1,17 @@
 import unittest
 import subprocess
-from threading import Timer
 import os
 import sys
-from signal import SIGKILL
 from random import random
 import urllib as url
 import time
+from ddagent import Application
+
+from util import Watchdog
 
 class TestWatchdog(unittest.TestCase):
     """Test watchdog in various conditions
     """
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
     def runTest(self):
         """Verify that watchdog kills ourselves even when spinning
         Verify that watchdog kills ourselves when hanging
@@ -27,55 +22,99 @@ class TestWatchdog(unittest.TestCase):
             raise Exception("Should have died with an error")
         except subprocess.CalledProcessError:
             duration = int(time.time() - start)
-            print "SUCCESS, busy loop was killed in %s seconds" % int(time.time() - start)
             self.assertEquals(duration, 5)
 
         # Start pseudo web server
-        print "nc pid", subprocess.Popen(["nc", "-l", "31834"]).pid
+        subprocess.Popen(["nc", "-l", "31834"])
         start = time.time()
         try:
             subprocess.check_call(["python", "tests/test_watchdog.py", "net"])
             raise Exception("Should have died with an error")
         except subprocess.CalledProcessError:
             duration = int(time.time() - start)
-            print "SUCCESS, hanging net was killed in %s seconds" % int(time.time() - start)
             self.assertEquals(duration, 5)
 
-def watchdog(f):
-    def selfdestruct():
-        print "SUCCESS, self-destructing"
-        os.kill(os.getpid(), SIGKILL)
+        # Normal loop, should run 5 times
+        start = time.time()
+        try:
+            subprocess.check_call(["python", "tests/test_watchdog.py", "normal"])
+            duration = int(time.time() - start)
+            self.assertEquals(duration, 5)
+        except subprocess.CalledProcessError:
+            self.fail("Watchdog killed normal process after %s seconds" % int(time.time() - start))
 
-    def wrapped(self):
-        t = Timer(5, selfdestruct, [])
-        t.start()
-        f(self)
-        t.cancel()
-        print "FAILURE, did not self-destruct"
-    return wrapped
+        # Fast tornado, not killed
+        start = time.time()
+        p = subprocess.Popen(["python", "tests/test_watchdog.py", "fast"])
+        p.wait()
+        duration = int(time.time() - start)
+        # should die as soon as flush_trs has been called
+        self.assertEquals(duration, 10)
 
-    
+        # Slow tornado, killed by the Watchdog
+        start = time.time()
+        p = subprocess.Popen(["python", "tests/test_watchdog.py", "slow"])
+        p.wait()
+        duration = int(time.time() - start)
+        self.assertEquals(duration, 4)
+
+class MockTxManager(object):
+    def flush(self):
+        "Pretend to flush for a long time"
+        time.sleep(5)
+        sys.exit(0)
+
 class PseudoAgent(object):
     """Same logic as the agent, simplified"""
-    @watchdog
-    def busyRun(self):
+    def busy_run(self):
+        w = Watchdog(5)
+        w.reset()
         x = 0
         while True:
             x = random()
 
-    @watchdog
-    def hangingNet(self):
+    def hanging_net(self):
+        w = Watchdog(5)
+        w.reset()
         x = url.urlopen("http://localhost:31834")
         print "ERROR Net call returned", x
         return True
 
+    def normal_run(self):
+        w = Watchdog(2)
+        w.reset()
+        for i in range(5):
+            time.sleep(1)
+            w.reset()
+
+    def slow_tornado(self):
+        a = Application(12345, {})
+        a._watchdog = Watchdog(4)
+        a._tr_manager = MockTxManager()
+        a.run()
+
+    def fast_tornado(self):
+        a = Application(12345, {})
+        a._watchdog = Watchdog(6)
+        a._tr_manager = MockTxManager()
+        a.run()
+
 if __name__ == "__main__":
     if sys.argv[1] == "busy":
         a = PseudoAgent()
-        a.busyRun()
+        a.busy_run()
     elif sys.argv[1] == "net":
         a = PseudoAgent()
-        a.hangingNet()
+        a.hanging_net()
+    elif sys.argv[1] == "normal":
+        a = PseudoAgent()
+        a.normal_run()
+    elif sys.argv[1] == "slow":
+        a = PseudoAgent()
+        a.slow_tornado()
+    elif sys.argv[1] == "fast":
+        a = PseudoAgent()
+        a.fast_tornado()
     elif sys.argv[1] == "test":
         t = TestWatchdog()
         t.runTest()
