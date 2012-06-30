375b8cb97346f339de879a534176313520a2daba
Alexis Lê-Quôc <alq@datadoghq.com>
2012-06-30 18:09:42 -0400

    First commit.

---
+++ tests/test_haproxy.py
@@ -0,0 +1,12 @@
+import unittest
+from checks.haproxy import Haproxy
+
+class HaproxyTestCase(unittest.Testcase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_haproxy_stats(self):
+        pass
