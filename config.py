d2478830971736381d563e4a3c871cc26d8bc0d5
Alexis Lê-Quôc <alq@datadoghq.com>
2012-07-09 18:18:36 -0400

    Increasing version to 3.0.2

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ def get_parsed_args():
     return options, args
 
 def get_version():
-    return "3.0.1"
+    return "3.0.2"
 
 def skip_leading_wsp(f):
     "Works on a file, returns a file-like object"
