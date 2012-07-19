e399a2f0d5d5b816316e9492602fb9f0e8fd4535
Alexis Lê-Quôc <alq@datadoghq.com>
2012-07-19 19:34:45 -0400

    Added a lot more ES metrics.
    The weird bit is that the number of reported metrics seems to vary
    depending on the platform. Mac in particular is not the richest
    platform.

--- checks/db/elastic.py
+++ checks/db/elastic.py
@@ -1,5 +1,3 @@
-#! /usr/bin/python
-
 import urlparse
 import urllib2
 import socket
@@ -17,60 +15,117 @@ class ElasticSearch(Check):
     NODES_URL = "/_cluster/nodes?network=true"
 
     METRICS = {
-        "docs.count": "gauge",
-        "docs.deleted": "gauge",
-        "store.size": ("gauge", "store.size_in_bytes"),
-        "indexing.index.total": ("gauge","indexing.index_total"),
-        "indexing.index.time": ("gauge","indexing.index_time_in_millis"),
-        "indexing.index.current": ("gauge","indexing.index_current"),
-        "indexing.delete.total": ("gauge", "indexing.delete_total"),
-        "indexing.delete.time": ("gauge", "indexing.delete_time_in_millis"),
-        "indexing.delete.current": ("gauge","indexing.delete_current"),
-        "get.total": ("gauge","get.total"),
-        "get.time": ("gauge","get.time_in_millis"),
-        "get.current": ("gauge","get.current"),
-        "get.exists.total": ("gauge","get.exists_total"),
-        "get.exists.time": ("gauge","get.exists_time_in_millis"),
-        "get.missing.total": ("gauge","get.missing_total"),
-        "get.missing.time": ("gauge","get.missing_time_in_millis"),
-        "search.query.total": ("gauge","search.query_total"),
-        "search.query.time": ("gauge","search.query_time_in_millis"),
-        "search.query.current": ("gauge","search.query_current"),
-        "search.fetch.total": ("gauge","search.fetch_total"),
-        "search.fetch.time": ("gauge","search.fetch_time_in_millis"),
-        "search.fetch.current": ("gauge","search.fetch_current"),
-        "cache.field.evictions": ("gauge","cache.field_evictions"),
-        "cache.field.size": ("gauge","cache.field_size_in_bytes"),
-        "cache.filter.count": ("gauge","cache.filter_count"),
-        "cache.filter.evictions": ("gauge","cache.filter_evictions"),
-        "cache.filter.size": ("gauge","cache.filter_size_in_bytes"),
-        "merges.current": ("gauge","merges.current"),
-        "merges.current.docs": ("gauge","merges.current_docs"),
-        "merges.current.size": ("gauge","merges.current_size_in_bytes"),
-        "merges.total": ("gauge","merges.total"),
-        "merges.total.time": ("gauge","merges.total_time_in_millis"),
-        "merges.total.docs": ("gauge","merges.total_docs"),
-        "merges.total.size": ("gauge","merges.total_size_in_bytes"),
-        "refresh.total": ("gauge","refresh.total"),
-        "refresh.total.time": ("gauge","refresh.total_time_in_millis"),
-        "flush.total": ("gauge","flush.total"),
-        "flush.total.time": ("gauge","flush.total_time_in_millis"),
+        "elasticsearch.docs.count": ("gauge", "indices.docs.count"),
+        "elasticsearch.docs.deleted": ("gauge", "indices.docs.deleted"),
+        "elasticsearch.store.size": ("gauge", "indices.store.size_in_bytes"),
+        "elasticsearch.indexing.index.total": ("gauge", "indices.indexing.index_total"),
+        "elasticsearch.indexing.index.time": ("gauge", "indices.indexing.index_time_in_millis", lambda v: float(v)/1000),
+        "elasticsearch.indexing.index.current": ("gauge", "indices.indexing.index_current"),
+        "elasticsearch.indexing.delete.total": ("gauge", "indices.indexing.delete_total"),
+        "elasticsearch.indexing.delete.time": ("gauge", "indices.indexing.delete_time_in_millis", lambda v: float(v)/1000),
+        "elasticsearch.indexing.delete.current": ("gauge", "indices.indexing.delete_current"),
+        "elasticsearch.get.total": ("gauge", "indices.get.total"),
+        "elasticsearch.get.time": ("gauge", "indices.get.time_in_millis", lambda v: float(v)/1000),
+        "elasticsearch.get.current": ("gauge", "indices.get.current"),
+        "elasticsearch.get.exists.total": ("gauge", "indices.get.exists_total"),
+        "elasticsearch.get.exists.time": ("gauge", "indices.get.exists_time_in_millis", lambda v: float(v)/1000),
+        "elasticsearch.get.missing.total": ("gauge", "indices.get.missing_total"),
+        "elasticsearch.get.missing.time": ("gauge", "indices.get.missing_time_in_millis", lambda v: float(v)/1000),
+        "elasticsearch.search.query.total": ("gauge", "indices.search.query_total"),
+        "elasticsearch.search.query.time": ("gauge", "indices.search.query_time_in_millis", lambda v: float(v)/1000),
+        "elasticsearch.search.query.current": ("gauge", "indices.search.query_current"),
+        "elasticsearch.search.fetch.total": ("gauge", "indices.search.fetch_total"),
+        "elasticsearch.search.fetch.time": ("gauge", "indices.search.fetch_time_in_millis", lambda v: float(v)/1000),
+        "elasticsearch.search.fetch.current": ("gauge", "indices.search.fetch_current"),
+        "elasticsearch.cache.field.evictions": ("gauge", "indices.cache.field_evictions"),
+        "elasticsearch.cache.field.size": ("gauge", "indices.cache.field_size_in_bytes"),
+        "elasticsearch.cache.filter.count": ("gauge", "indices.cache.filter_count"),
+        "elasticsearch.cache.filter.evictions": ("gauge", "indices.cache.filter_evictions"),
+        "elasticsearch.cache.filter.size": ("gauge", "indices.cache.filter_size_in_bytes"),
+        "elasticsearch.merges.current": ("gauge", "indices.merges.current"),
+        "elasticsearch.merges.current.docs": ("gauge", "indices.merges.current_docs"),
+        "elasticsearch.merges.current.size": ("gauge", "indices.merges.current_size_in_bytes"),
+        "elasticsearch.merges.total": ("gauge", "indices.merges.total"),
+        "elasticsearch.merges.total.time": ("gauge", "indices.merges.total_time_in_millis", lambda v: float(v)/1000),
+        "elasticsearch.merges.total.docs": ("gauge", "indices.merges.total_docs"),
+        "elasticsearch.merges.total.size": ("gauge", "indices.merges.total_size_in_bytes"),
+        "elasticsearch.refresh.total": ("gauge", "indices.refresh.total"),
+        "elasticsearch.refresh.total.time": ("gauge", "indices.refresh.total_time_in_millis", lambda v: float(v)/1000),
+        "elasticsearch.flush.total": ("gauge", "indices.flush.total"),
+        "elasticsearch.flush.total.time": ("gauge", "indices.flush.total_time_in_millis", lambda v: float(v)/1000),
+        "elasticsearch.process.open_fd": ("gauge", "process.open_file_descriptors"),
+        "elasticsearch.transport.rx_count": ("gauge", "transport.rx_count"),
+        "elasticsearch.transport.tx_count": ("gauge", "transport.tx_count"),
+        "elasticsearch.transport.rx_size": ("gauge", "transport.rx_size_in_bytes"),
+        "elasticsearch.transport.tx_size": ("gauge", "transport.tx_size_in_bytes"),
+        "elasticsearch.transport.server_open": ("gauge", "transport.server_open"),
+        "elasticsearch.thread_pool.bulk.active": ("gauge", "thread_pool.bulk.active"),
+        "elasticsearch.thread_pool.bulk.threads": ("gauge", "thread_pool.bulk.threads"),
+        "elasticsearch.thread_pool.bulk.queue": ("gauge", "thread_pool.bulk.queue"),
+        "elasticsearch.thread_pool.cache.active": ("gauge", "thread_pool.cache.active"),
+        "elasticsearch.thread_pool.cache.threads": ("gauge", "thread_pool.cache.threads"),
+        "elasticsearch.thread_pool.cache.queue": ("gauge", "thread_pool.cache.queue"),
+        "elasticsearch.thread_pool.flush.active": ("gauge", "thread_pool.flush.active"),
+        "elasticsearch.thread_pool.flush.threads": ("gauge", "thread_pool.flush.threads"),
+        "elasticsearch.thread_pool.flush.queue": ("gauge", "thread_pool.flush.queue"),
+        "elasticsearch.thread_pool.generic.active": ("gauge", "thread_pool.generic.active"),
+        "elasticsearch.thread_pool.generic.threads": ("gauge", "thread_pool.generic.threads"),
+        "elasticsearch.thread_pool.generic.queue": ("gauge", "thread_pool.generic.queue"),
+        "elasticsearch.thread_pool.get.active": ("gauge", "thread_pool.get.active"),
+        "elasticsearch.thread_pool.get.threads": ("gauge", "thread_pool.get.threads"),
+        "elasticsearch.thread_pool.get.queue": ("gauge", "thread_pool.get.queue"),
+        "elasticsearch.thread_pool.index.active": ("gauge", "thread_pool.index.active"),
+        "elasticsearch.thread_pool.index.threads": ("gauge", "thread_pool.index.threads"),
+        "elasticsearch.thread_pool.index.queue": ("gauge", "thread_pool.index.queue"),
+        "elasticsearch.thread_pool.management.active": ("gauge", "thread_pool.management.active"),
+        "elasticsearch.thread_pool.management.threads": ("gauge", "thread_pool.management.threads"),
+        "elasticsearch.thread_pool.management.queue": ("gauge", "thread_pool.management.queue"),
+        "elasticsearch.thread_pool.merge.active": ("gauge", "thread_pool.merge.active"),
+        "elasticsearch.thread_pool.merge.threads": ("gauge", "thread_pool.merge.threads"),
+        "elasticsearch.thread_pool.merge.queue": ("gauge", "thread_pool.merge.queue"),
+        "elasticsearch.thread_pool.percolate.active": ("gauge", "thread_pool.percolate.active"),
+        "elasticsearch.thread_pool.percolate.threads": ("gauge", "thread_pool.percolate.threads"),
+        "elasticsearch.thread_pool.percolate.queue": ("gauge", "thread_pool.percolate.queue"),
+        "elasticsearch.thread_pool.refresh.active": ("gauge", "thread_pool.refresh.active"),
+        "elasticsearch.thread_pool.refresh.threads": ("gauge", "thread_pool.refresh.threads"),
+        "elasticsearch.thread_pool.refresh.queue": ("gauge", "thread_pool.refresh.queue"),
+        "elasticsearch.thread_pool.search.active": ("gauge", "thread_pool.search.active"),
+        "elasticsearch.thread_pool.search.threads": ("gauge", "thread_pool.search.threads"),
+        "elasticsearch.thread_pool.search.queue": ("gauge", "thread_pool.search.queue"),
+        "elasticsearch.thread_pool.snapshot.active": ("gauge", "thread_pool.snapshot.active"),
+        "elasticsearch.thread_pool.snapshot.threads": ("gauge", "thread_pool.snapshot.threads"),
+        "elasticsearch.thread_pool.snapshot.queue": ("gauge", "thread_pool.snapshot.queue"),
+        "elasticsearch.http.current_open": ("gauge", "http.current_open"),
+        "elasticsearch.http.total_opened": ("gauge", "http.total_opened"),
+        "jvm.gc.collection_count": ("gauge", "jvm.gc.collection_count"),
+        "jvm.gc.collection_time": ("gauge", "jvm.gc.collection_time_in_millis", lambda v: float(v)/1000),
+        "jvm.gc.concurrent_mark_sweep.count": ("gauge", "jvm.gc.collectors.ConcurrentMarkSweep.collection_count"),
+        "jvm.gc.concurrent_mark_sweep.collection_time": ("gauge", "jvm.gc.collectors.ConcurrentMarkSweep.collection_time_in_millis", lambda v: float(v)/1000),
+        "jvm.gc.par_new.count": ("gauge", "jvm.gc.collectors.ParNew.collection_count"),
+        "jvm.gc.par_new.collection_time": ("gauge", "jvm.gc.collectors.ParNew.collection_time_in_millis", lambda v: float(v)/1000),
+        "jvm.mem.heap_committed": ("gauge", "jvm.mem.heap_committed_in_bytes"),
+        "jvm.mem.heap_used": ("gauge", "jvm.mem.heap_used_in_bytes"),
+        "jvm.mem.non_heap_committed": ("gauge", "jvm.mem.non_heap_committed_in_bytes"),
+        "jvm.mem.non_heap_used": ("gauge", "jvm.mem.non_heap_used_in_bytes"),
+        "jvm.threads.count": ("gauge", "jvm.threads.count"),
+        "jvm.threads.peak_count": ("gauge", "jvm.threads.peak_count"),
     }
 
     @classmethod
-    def _map_metric(cls,func):
+    def _map_metric(cls, func):
+        """Apply a function to all known metrics.
+        Used to create and sample metrics.
+        """
         for metric in cls.METRICS:
+            # metric description
             desc = cls.METRICS[metric]
-            if type(desc) == tuple:
-                func("elasticsearch." + metric,*desc)
-            else:
-                func("elasticsearch." + metric,desc,metric)
+            func(metric, *desc)
 
     def __init__(self, logger):
         Check.__init__(self, logger)
 
-        def generate_metric(name, type, path):
-            if type == "counter":
+        def generate_metric(name, xtype, *args):
+            if xtype == "counter":
                 self.counter(name)
             else:
                 self.gauge(name)
@@ -79,7 +134,6 @@ class ElasticSearch(Check):
 
     def _get_data(self, agentConfig, url):
         "Hit a given URL and return the parsed json"
-
         req = urllib2.Request(url, None, headers(agentConfig))
         request = urllib2.urlopen(req)
         response = request.read()
@@ -88,28 +142,33 @@ class ElasticSearch(Check):
     def _metric_not_found(self, metric, path):
         self.logger.warning("Metric not found: %s -> %s", path, metric)
 
-    def _process_metric(self, data, metric, path):
-
-        value = data.get("indices",None)
+    def _process_metric(self, data, metric, path, xform=None):
+        """data: dictionary containing all the stats
+        metric: datadog metric
+        path: corresponding path in data, flattened, e.g. thread_pool.bulk.queue
+        xfom: a lambda to apply to the numerical value
+        """
+        value = data
+        # Traverse the nested dictionaries
         for key in path.split('.'):
             if value is not None:
-                value = value.get(key,None)
+                value = value.get(key, None)
             else:
-                value = None
                 break
 
         if value is not None:
-            self.save_sample(metric,long(value))
+            if xform: value = xform(value)
+            self.save_sample(metric, value)
         else:
             self._metric_not_found(metric, path)
 
     def _process_data(self, agentConfig, data):
-
         for node in data['nodes']:
             node_data = data['nodes'][node]
 
-            def process_metric(metric, xtype, path):
-                self._process_metric(node_data, metric, path)
+            def process_metric(metric, xtype, path, xform=None):
+                # closure over node_data
+                self._process_metric(node_data, metric, path, xform)
 
             if 'hostname' in node_data:
                 # For ES >= 0.19
@@ -145,7 +204,9 @@ class ElasticSearch(Check):
 
         if node_name in data['nodes']:
             node = data['nodes'][node_name]
-            if 'network' in node:
+            if 'network' in node\
+            and 'primary_interface' in node['network']\
+            and 'address' in node['network']['primary_interface']:
                 return node['network']['primary_interface']['address']
 
         raise NodeNotFound()
@@ -210,3 +271,12 @@ http://www.elasticsearch.org/guide/reference/api/admin-cluster-nodes-stats.html
             self.logger.exception('Unable to get elasticsearch statistics')
             return False
 
+
+if __name__ == "__main__":
+    import pprint
+    import logging
+    from config import get_version
+    logging.basicConfig()
+    logger = logging.getLogger()
+    c = ElasticSearch(logger)
+    pprint.pprint(c.check({"elasticsearch": "http://localhost:9200", "version": get_version()}))
