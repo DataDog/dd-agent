896fdac3c982229413c0d37d5c4bdcd7f248b3c5
Matt Perpick <matthewperpick@gmail.com>
2012-08-16 10:42:56 -0400

    Enabling pup in the agent.

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ def get_parsed_args():
     return options, args
 
 def get_version():
-    return "3.0.5"
+    return "3.1.0"
 
 def skip_leading_wsp(f):
     "Works on a file, returns a file-like object"
