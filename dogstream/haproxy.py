500fd170abc3ebe3feeb4d9f97d11f55645315af
Alexis Lê-Quôc <alq@datadoghq.com>
2012-09-09 22:21:41 -0400

    * dogstream/haproxy.py: Moved to class-based dogstream.

--- dogstream/haproxy.py
+++ dogstream/haproxy.py
@@ -30,95 +30,103 @@ import re
 
 HAPROXY_RE = re.compile("^.*haproxy\[(?P<pid>\d+)\]: (?P<client_ip>[\d.]+):(?P<client_port>\d+) \[(?P<accept_date>[^]]+)\] (?P<frontend_name>\w+) (?P<backend_name>\w+)/(?P<server_name>\S+) (?P<tq>-?\d+)/(?P<tw>-?\d+)/(?P<tc>-?\d+)/(?P<tr>-?\d+)/(?P<tt>-?\d+) (?P<status_code>\d{3}) (?P<bytes_read>\d+) . . .... (?P<actconn>\d+)/(?P<feconn>\d+)/(?P<beconn>\d+)/(?P<srvconn>\d+)/(?P<retries>\d+) (?P<srv_queue>\d+)/(?P<backend_queue>\d+).*\"(?P<cmd>\w+) (?P<url>\S+) HTTP/.\..\"$")
 
-def parse_timestamp(timestamp):
-    return time.mktime(datetime.strptime(timestamp, '%d/%b/%Y:%H:%M:%S.%f').timetuple())
+class HAProxyLogParser(object):
+    def __init__(self, config):
+        self._logger = logging.getLogger('haproxy-logparser')
+        self._state = {}
 
-def stem_url(url):
-    """Remove unneeded variability in URLs"""
-    try:
-        # First ditch parameters after ?
-        # Then remove all digits
-        return url.split("?")[0].translate(None, "0123456789")
-    except:
-        return None
+    @staticmethod
+    def parse_timestamp(timestamp):
+        return time.mktime(datetime.strptime(timestamp, '%d/%b/%Y:%H:%M:%S.%f').timetuple())
 
-def parse_status_code(counters, code, lbound, ubound, metric):
-    assert lbound < ubound
-    if int(code) >= lbound and int(code) <= ubound:
-        counters[metric] += 1
-    return counters
-
-def parse_haproxy(logger, line, state):
-    points = []
-    # Simple init of status codes counters
-    if len(state) == 0:
-        state['codes'] = {'2xx': 0, '3xx': 0, '4xx': 0, '5xx': 0}
-        state['aborts'] = 0
-
-    m = HAPROXY_RE.match(line)
-    # No match? skip
-    if m is None:
-        return None
-
-    timestamp = parse_timestamp(m.group('accept_date'))
-    # Skip STATS call
-    if m.group('server_name') == '<STATS>':
-        return None
-
-    attributes = {'tags': ['service:%s' % m.group('backend_name')]}
-    cmd = m.group('cmd')
-    # url_tag = stem_url(m.group('url'))
-    # if url_tag:
-    #     attributes['tags'].append('url:%s' % url_tag)
-
-    # status codes
-    parse_status_code(state['codes'], m.group('status_code'), 200, 299, '2xx')
-    parse_status_code(state['codes'], m.group('status_code'), 300, 399, '3xx')
-    parse_status_code(state['codes'], m.group('status_code'), 400, 499, '4xx')
-    parse_status_code(state['codes'], m.group('status_code'), 500, 599, '5xx')
-
-    metrics = [
-        ('haproxy.http.tq', 'gauge', lambda d: int(d.group('tq'))),
-        ('haproxy.http.tw', 'gauge', lambda d: int(d.group('tw'))),
-        ('haproxy.http.tc', 'gauge', lambda d: int(d.group('tc'))),
-        ('haproxy.http.tr', 'gauge', lambda d: int(d.group('tr'))),
-        ('haproxy.http.tt', 'gauge', lambda d: int(d.group('tt'))),
-        ('haproxy.http.bytes_read', 'gauge', lambda d: d.group('bytes_read')),
-        ('haproxy.http.2xx', 'counter', lambda m, s=state: s['codes']['2xx']),
-        ('haproxy.http.3xx', 'counter', lambda m, s=state: s['codes']['3xx']),
-        ('haproxy.http.4xx', 'counter', lambda m, s=state: s['codes']['4xx']),
-        ('haproxy.http.5xx', 'counter', lambda m, s=state: s['codes']['5xx']),
-    ]
-
-    is_abort = False
-    for name, typ, accessor in metrics:
-        value = accessor(m)
+    @staticmethod
+    def stem_url(url):
+        """Remove unneeded variability in URLs"""
         try:
-            value = int(value)
+            # First ditch parameters after ?
+            # Then remove all digits
+            return url.split("?")[0].translate(None, "0123456789")
         except:
-            pass
-        else:
-            # Treat -1 in timing as an abort and skip the metric
-            if value == -1 and name[-2:] in ('tq', 'tw', 'tc', 'tr', 'tt'):
-                if not is_abort:
-                    state['aborts'] += 1
-                    points.append(('haproxy.http.abort', timestamp, state['aborts'], {'metric_type': 'counter'}))
-                    is_abort = True
+            return None
+
+    @staticmethod
+    def parse_status_code(counters, code, lbound, ubound, metric):
+        assert lbound < ubound
+        if int(code) >= lbound and int(code) <= ubound:
+            counters[metric] += 1
+        return counters
+
+    def parse_line(self, line):
+        points = []
+        # Simple init of status codes counters
+        if len(self._state) == 0:
+            self._state['codes'] = {'2xx': 0, '3xx': 0, '4xx': 0, '5xx': 0}
+            self._state['aborts'] = 0
+
+        m = HAPROXY_RE.match(line)
+        # No match? skip
+        if m is None:
+            return None
+
+        timestamp = HAProxyLogParser.parse_timestamp(m.group('accept_date'))
+        # Skip STATS call
+        if m.group('server_name') == '<STATS>':
+            return None
+
+        attributes = {'tags': ['service:%s' % m.group('backend_name')]}
+        cmd = m.group('cmd')
+        # url_tag = stem_url(m.group('url'))
+        # if url_tag:
+        #     attributes['tags'].append('url:%s' % url_tag)
+
+        # status codes
+        HAProxyLogParser.parse_status_code(self._state['codes'], m.group('status_code'), 200, 299, '2xx')
+        HAProxyLogParser.parse_status_code(self._state['codes'], m.group('status_code'), 300, 399, '3xx')
+        HAProxyLogParser.parse_status_code(self._state['codes'], m.group('status_code'), 400, 499, '4xx')
+        HAProxyLogParser.parse_status_code(self._state['codes'], m.group('status_code'), 500, 599, '5xx')
+
+        metrics = [
+            ('haproxy.http.tq', 'gauge', lambda d: int(d.group('tq'))),
+            ('haproxy.http.tw', 'gauge', lambda d: int(d.group('tw'))),
+            ('haproxy.http.tc', 'gauge', lambda d: int(d.group('tc'))),
+            ('haproxy.http.tr', 'gauge', lambda d: int(d.group('tr'))),
+            ('haproxy.http.tt', 'gauge', lambda d: int(d.group('tt'))),
+            ('haproxy.http.bytes_read', 'gauge', lambda d: d.group('bytes_read')),
+            ('haproxy.http.2xx', 'counter', lambda m, s=self._state: s['codes']['2xx']),
+            ('haproxy.http.3xx', 'counter', lambda m, s=self._state: s['codes']['3xx']),
+            ('haproxy.http.4xx', 'counter', lambda m, s=self._state: s['codes']['4xx']),
+            ('haproxy.http.5xx', 'counter', lambda m, s=self._state: s['codes']['5xx']),
+        ]
+
+        is_abort = False
+        for name, typ, accessor in metrics:
+            value = accessor(m)
+            try:
+                value = int(value)
+            except:
+                pass
             else:
-                points.append((name, timestamp, value, attributes))
-    return points
+                # Treat -1 in timing as an abort and skip the metric
+                if value == -1 and name[-2:] in ('tq', 'tw', 'tc', 'tr', 'tt'):
+                    if not is_abort:
+                        self._state['aborts'] += 1
+                        points.append(('haproxy.http.abort', timestamp, self._state['aborts'], {'metric_type': 'counter'}))
+                        is_abort = True
+                else:
+                    points.append((name, timestamp, value, attributes))
+        return points
 
 if __name__ == '__main__':
     # Parse stdin and extract metrics
     import sys
     import pprint
-    state = {}
     logging.basicConfig(format="%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s")
+    parser = HAProxyLogParser({})
     while True:
         line = sys.stdin.readline()
         if line is None or len(line) == 0:
             break
         else:
-            r = parse_haproxy(logging.getLogger(), line, state)
+            r = parser.parse_line(line)
             if r is not None:
                 pprint.pprint(r)
