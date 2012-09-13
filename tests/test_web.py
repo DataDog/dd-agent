c3bb713c314efe066d0a343cf117bd7ee5f4d917
Remi Hakim <remi@datadoghq.com>
2012-09-13 10:38:21 -0400

    Fix the nginx tests

--- tests/test_web.py
+++ tests/test_web.py
@@ -4,26 +4,21 @@ logger = logging.getLogger(__file__)
 
 from checks.web import *
 
-NGINX_CONF = os.path.realpath(os.path.join(os.path.dirname(__file__), "nginx.conf"))
 
 class TestWeb(unittest.TestCase):
 
     def setUp(self):
         self.apache = Apache(logger)
         self.nginx = Nginx(logger)
-        try:
-            self.conf = tempfile.NamedTemporaryFile()
-            self.conf.write(open(NGINX_CONF).read())
-            self.flush()
 
     def testApache(self):
         pass
 
     def testNginx(self):
-        config = { "nginx_status_url": "http://localhost/nginx_status/", 
-                    "nginx_status_url_1": "http://localhost/nginx_status/:first_one",
-                    "nginx_status_url_2": "http://localhost/nginx_status/:dummy",
-                    "nginx_status_url_3": "http://localhost/nginx_status/:second",
+        config = { "nginx_status_url": "http://localhost:44441/nginx_status/", 
+                    "nginx_status_url_1": "http://dummyurl:first_one",
+                    "nginx_status_url_2": "http://localhost.fr:44441/nginx_status/:dummy",
+                    "nginx_status_url_3": "http://localhost.fr:44441/nginx_status/:second",
                 'version': '0.1',
                 'api_key': 'toto'
         }
