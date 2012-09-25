26a651a00166eacb022a057f74bece6c0dcc7223
Conor Branagan <conor.branagan@gmail.com>
2012-09-25 14:39:09 -0400

    fix test check module loader to search all classes

--- tests/common.py
+++ tests/common.py
@@ -1,5 +1,7 @@
+from checks import AgentCheck
 from config import get_checksd_path
 import sys
+import inspect
 
 def load_check(name, config, agentConfig):
     checksd_path = get_checksd_path()
@@ -7,8 +9,16 @@ def load_check(name, config, agentConfig):
         sys.path.append(checksd_path)
 
     check_module = __import__(name)
-    check_cls = getattr(check_module, check_module.CHECK)
+    check_class = None
+    classes = inspect.getmembers(check_module, inspect.isclass)
+    for name, clsmember in classes:
+        if AgentCheck in clsmember.__bases__:
+            check_class = clsmember
+            break
+    if check_class is None:
+        raise Exception("Unable to import check %s. Missing a class that inherits AgentCheck" % name)
+
     init_config = config.get('init_config', None)
 
     # init the check class
-    return check_cls(name, init_config=init_config, agentConfig=agentConfig)
+    return check_class(name, init_config=init_config, agentConfig=agentConfig)
