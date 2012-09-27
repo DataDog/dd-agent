b5dfbe03030131657d1567e2712c947af7fab4db
Conor Branagan <conor.branagan@gmail.com>
2012-09-27 14:16:50 -0400

    Update WMI check to tag by name if there are multiple "devices"

--- checks/wmi_check.py
+++ checks/wmi_check.py
@@ -30,16 +30,39 @@ class WMICheck(Check):
             
             # Get the metric from WMI
             self.gauge(metric_name)
+
             try:
-                res = getattr(w, wmi_name)()[0]
-                val = getattr(res, wmi_val)
+                wmi_cls = getattr(w, wmi_name)()
+            except AttributeError:
+                self.logger.error("Unable to find the '%s' WMI class. Skipping." \
+                    % wmi_name)
+                continue
+
+            if not len(wmi_cls):
+                self.logger.error("No values in the '%s' WMI class" % wmi_name)
+                continue
+
+            for device in wmi_cls:
+                # There's only one value under this class, just use that one
+                try:
+                    val = getattr(device, wmi_val)
+                except AttributeError:
+                    self.logger.error("'%s' WMI class does not have value for %s" \
+                        % (wmi_name, wmi_val))
+                    break
+
+                # If len > 1, we want to tag by name
+                if len(wmi_cls) > 1:
+                    tags = ['name:%s' % self.normalize_device_name(device.name)]
+                else:
+                    tags = None
+
                 try:
                     val = float(val)
                 except:
                     self.logger.error('Unable to parse %s/%s as a metric. The value %s can not be parsed as a float' % (wmi_name, wmi_val, val))
                     continue
-                self.save_gauge(metric_name, val)
-            except:
-                self.logger.exception('Unable to get metric %s/%s' % (wmi_name, wmi_val))
+
+                self.save_gauge(metric_name, val, tags=tags)
 
         return self.get_metrics()
