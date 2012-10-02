ceeb523b794ea3d961c9e67fce360f730ee0cfb1
Conor Branagan <conor.branagan@gmail.com>
2012-10-02 14:58:08 -0400

    abstract the subprocess terminate so it works on 2.4/2.5

--- tests/common.py
+++ tests/common.py
@@ -3,6 +3,7 @@ from config import get_checksd_path
 from util import getOS
 import sys
 import inspect
+import os
 
 def load_check(name, config, agentConfig):
     checksd_path = get_checksd_path(getOS())
@@ -23,3 +24,18 @@ def load_check(name, config, agentConfig):
 
     # init the check class
     return check_class(name, init_config=init_config, agentConfig=agentConfig)
+
+def kill_subprocess(process_obj):
+    try:
+        process_obj.terminate()
+    except AttributeError:
+        # py < 2.6 doesn't support process.terminate()
+        if getOS() == 'windows':
+            import ctypes
+            PROCESS_TERMINATE = 1
+            handle = ctypes.windll.kernel32.OpenProcess(PROCESS_TERMINATE, False,
+                process_obj.pid)
+            ctypes.windll.kernel32.TerminateProcess(handle, -1)
+            ctypes.windll.kernel32.CloseHandle(handle)
+        else:
+            os.kill(process_obj.pid, signal.SIGKILL)
