d836bddf947dbd2ef72160d41a180527f5ba64d0
Conor Branagan <conor.branagan@gmail.com>
2012-10-02 15:00:40 -0400

    just use os.pid to get the current pid
    multiprocessing module isn't in 2.4/2.5

--- tests/test_mcache.py
+++ tests/test_mcache.py
@@ -1,7 +1,7 @@
 import unittest
 import logging
+import os
 from subprocess import Popen, PIPE
-import multiprocessing
 from checks.db.mcache import *
 
 class TestMemCache(unittest.TestCase):
@@ -9,7 +9,7 @@ class TestMemCache(unittest.TestCase):
         self.c = Memcache(logging.getLogger(__file__))
 
     def _countConnections(self, port):
-        pid = multiprocessing.current_process().pid
+        pid = os.getpid()
         p1 = Popen(['lsof', '-a', '-p{0}'.format(pid), '-i4'], stdout=PIPE)
         p2 = Popen(["grep", ":{0}".format(port)], stdin=p1.stdout, stdout=PIPE)
         p3 = Popen(["wc", "-l"], stdin=p2.stdout, stdout=PIPE)
