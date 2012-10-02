8e51d6528b94a2dce7c7ad0f136257ef38128b9e
Conor Branagan <conor.branagan@gmail.com>
2012-10-02 15:36:23 -0400

    remote string.format() from memcache tests

--- tests/test_mcache.py
+++ tests/test_mcache.py
@@ -10,8 +10,9 @@ class TestMemCache(unittest.TestCase):
 
     def _countConnections(self, port):
         pid = os.getpid()
-        p1 = Popen(['lsof', '-a', '-p{0}'.format(pid), '-i4'], stdout=PIPE)
-        p2 = Popen(["grep", ":{0}".format(port)], stdin=p1.stdout, stdout=PIPE)
+        p1 = Popen(['lsof', '-a', '-p%s' %
+            pid, '-i4'], stdout=PIPE)
+        p2 = Popen(["grep", ":%s" % port], stdin=p1.stdout, stdout=PIPE)
         p3 = Popen(["wc", "-l"], stdin=p2.stdout, stdout=PIPE)
         output = p3.communicate()[0]
         return int(output.strip())
