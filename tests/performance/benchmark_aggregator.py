8ddc0491b63839addc0c0ca819506a9282b1b81b
Matt Perpick <matthewperpick@gmail.com>
2012-10-29 14:10:56 -0400

    Test tags + sampling in the dogstatsd benchmark.

--- tests/performance/benchmark_aggregator.py
+++ tests/performance/benchmark_aggregator.py
@@ -11,7 +11,7 @@ from aggregator import MetricsAggregator
 class TestAggregatorPerf(object):
 
     FLUSH_COUNT = 10
-    LOOPS_PER_FLUSH = 5000
+    LOOPS_PER_FLUSH = 2000
     METRIC_COUNT = 5
 
     def test_dogstatsd_aggregation_perf(self):
@@ -19,12 +19,26 @@ class TestAggregatorPerf(object):
 
         for _ in xrange(self.FLUSH_COUNT):
             for i in xrange(self.LOOPS_PER_FLUSH):
-                # Counters
                 for j in xrange(self.METRIC_COUNT):
+
+                    # metrics
                     ma.submit_packets('counter.%s:%s|c' % (j, i))
                     ma.submit_packets('gauge.%s:%s|g' % (j, i))
                     ma.submit_packets('histogram.%s:%s|h' % (j, i))
                     ma.submit_packets('set.%s:%s|s' % (j, 1.0))
+
+                    # tagged metrics
+                    ma.submit_packets('counter.%s:%s|c|#tag1,tag2' % (j, i))
+                    ma.submit_packets('gauge.%s:%s|g|#tag1,tag2' % (j, i))
+                    ma.submit_packets('histogram.%s:%s|h|#tag1,tag2' % (j, i))
+                    ma.submit_packets('set.%s:%s|s|#tag1,tag2' % (j, i))
+
+                    # sampled metrics
+                    ma.submit_packets('counter.%s:%s|c|@0.5' % (j, i))
+                    ma.submit_packets('gauge.%s:%s|g|@0.5' % (j, i))
+                    ma.submit_packets('histogram.%s:%s|h|@0.5' % (j, i))
+                    ma.submit_packets('set.%s:%s|s|@0.5' % (j, i))
+
             ma.flush()
 
     def test_checksd_aggregation_perf(self):
@@ -45,4 +59,4 @@ class TestAggregatorPerf(object):
 if __name__ == '__main__':
     t = TestAggregatorPerf()
     t.test_dogstatsd_aggregation_perf()
-    t.test_checksd_aggregation_perf()
+    #t.test_checksd_aggregation_perf()
