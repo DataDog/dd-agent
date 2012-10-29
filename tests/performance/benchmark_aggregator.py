6b3fb5ed1635297654c4df39828ef3c946859b8e
Matt Perpick <matthewperpick@gmail.com>
2012-10-29 13:49:40 -0400

    Share test run data between types.

--- tests/performance/benchmark_aggregator.py
+++ tests/performance/benchmark_aggregator.py
@@ -10,25 +10,39 @@ from aggregator import MetricsAggregator
 
 class TestAggregatorPerf(object):
 
-    def test_aggregation_performance(self):
+    FLUSH_COUNT = 10
+    LOOPS_PER_FLUSH = 5000
+    METRIC_COUNT = 5
+
+    def test_dogstatsd_aggregation_perf(self):
         ma = MetricsAggregator('my.host')
 
-        flush_count = 10
-        loops_per_flush = 5000
-        metric_count = 5
-
-        for _ in xrange(flush_count):
-            for i in xrange(loops_per_flush):
+        for _ in xrange(self.FLUSH_COUNT):
+            for i in xrange(self.LOOPS_PER_FLUSH):
                 # Counters
-                for j in xrange(metric_count):
+                for j in xrange(self.METRIC_COUNT):
                     ma.submit_packets('counter.%s:%s|c' % (j, i))
                     ma.submit_packets('gauge.%s:%s|g' % (j, i))
                     ma.submit_packets('histogram.%s:%s|h' % (j, i))
                     ma.submit_packets('set.%s:%s|s' % (j, 1.0))
-
             ma.flush()
+
+    def test_checksd_aggregation_perf(self):
+        ma = MetricsAggregator('my.host')
+
+        for _ in xrange(self.FLUSH_COUNT):
+            for i in xrange(self.LOOPS_PER_FLUSH):
+                # Counters
+                for j in xrange(self.METRIC_COUNT):
+                    ma.increment('counter.%s' % j, i)
+                    ma.gauge('gauge.%s' % j, i)
+                    ma.histogram('histogram.%s' % j, i)
+                    ma.set('set.%s' % j, float(i))
+            ma.flush()
+
 
 
 if __name__ == '__main__':
     t = TestAggregatorPerf()
-    t.test_aggregation_performance()
+    t.test_dogstatsd_aggregation_perf()
+    t.test_checksd_aggregation_perf()
