c4c2ffe8c7652b1477f8c5b948329a4544bfa79a
Conor Branagan <conor.branagan@gmail.com>
2012-11-14 23:19:13 -0500

    add test for old-style varnish config

--- tests/test_varnish.py
+++ tests/test_varnish.py
@@ -1881,5 +1881,14 @@ instances:
         except:
             pass
 
+    def testOldConfig(self):
+        v, instances = get_check('varnish', self.config)
+        config = {
+            'varnishstat': '/usr/bin/varnishstat'
+        }
+
+        instances = v.parse_agent_config(config)['instances']
+        assert instances[0]['varnishstat'] == config['varnishstat']
+
 if __name__ == '__main__':
     unittest.main()
