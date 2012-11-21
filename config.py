7362c01f922486419f311c882d8c42d21e8b5c0c
Conor Branagan <conor.branagan@gmail.com>
2012-11-21 11:31:40 -0500

    Bump to version 3.4.0

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ def get_parsed_args():
     return options, args
 
 def get_version():
-    return "3.3.0"
+    return "3.4.0"
 
 def skip_leading_wsp(f):
     "Works on a file, returns a file-like object"
