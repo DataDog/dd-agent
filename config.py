7c83592c6bc0cedd28040044792f10caa65f4572
Matt Perpick <matthewperpick@gmail.com>
2012-12-03 18:02:12 -0500

    Bumping version to 3.4.1.

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ def get_parsed_args():
     return options, args
 
 def get_version():
-    return "3.4.0"
+    return "3.4.1"
 
 def skip_leading_wsp(f):
     "Works on a file, returns a file-like object"
