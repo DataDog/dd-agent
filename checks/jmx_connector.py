f68830adec3b64d90775a0fc420b058b5320061e
Matt Perpick <matthewperpick@gmail.com>
2012-12-11 15:35:09 -0500

    Import json from util in JMX.

--- checks/jmx_connector.py
+++ checks/jmx_connector.py
@@ -1,27 +1,10 @@
-# Import json for the agent. Try simplejson first, then the stdlib version and
-# if all else fails, use minjson which we bundle with the agent.
-try:
-    import simplejson as json
-except ImportError:
-    try:
-        import json
-    except ImportError:
-        import minjson
-        class json(object):
-            @staticmethod
-            def dumps(data):
-                return minjson.write(data)
-
-            @staticmethod
-            def loads(data):
-                return minjson.safeRead(data)
-
 
 import os
 import re
 import sys
 
 from checks import AgentCheck
+from util import json
 
 import logging
 log = logging.getLogger('jmx')
