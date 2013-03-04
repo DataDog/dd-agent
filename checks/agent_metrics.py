d9e2b7998f3678c39ab31052af34f7d9720fdc86
Alexis Lê-Quôc <alq@datadoghq.com>
2013-03-03 19:12:02 -0500

    Cleaner agent metric messages (units, precision).
    Also pep8 for the most part (except line length).

--- checks/agent_metrics.py
+++ checks/agent_metrics.py
@@ -6,6 +6,8 @@ MAX_THREADS_COUNT = 50
 MAX_COLLECTION_TIME = 30
 MAX_EMIT_TIME = 5
 MAX_CPU_PCT = 10
+
+
 class CollectorMetrics(Check):
     def __init__(self, logger):
         Check.__init__(self, logger)
@@ -15,32 +17,31 @@ class CollectorMetrics(Check):
         self.gauge('datadog.agent.collector.threads.count')
         self.gauge('datadog.agent.collector.cpu.used')
 
-    def check(self, payload, agentConfig, collection_time, emit_time, cpu_time=None):
-        
+    def check(self, payload, agent_config, collection_time, emit_time, cpu_time=None):
+
         if threading.activeCount() > MAX_THREADS_COUNT:
             self.save_sample('datadog.agent.collector.threads.count', threading.activeCount())
-            self.logger.warning("Thread count is high: %s %s" % (threading.activeCount()))
+            self.logger.warning("Thread count is high: %d" % threading.activeCount())
 
-        
         if collection_time > MAX_COLLECTION_TIME:
             self.save_sample('datadog.agent.collector.collection.time', collection_time)
-            self.logger.warning("Collection time is high: %s, metrics count: %s, events count: %s" 
-                    % (collection_time, len(payload['metrics']), len(payload['events'])))
-        
+            self.logger.warning("Collection time (s) is high: %.1f, metrics count: %d, events count: %d"
+                                % (collection_time, len(payload['metrics']), len(payload['events'])))
+
         if emit_time is not None and emit_time > MAX_EMIT_TIME:
             self.save_sample('datadog.agent.emitter.emit.time', emit_time)
-            self.logger.warning("Emit time is high: %s, metrics count: %s, events count: %s" 
-                % (emit_time, len(payload['metrics']), len(payload['events'])))
-        
+            self.logger.warning("Emit time (s) is high: %.1f, metrics count: %d, events count: %d"
+                                % (emit_time, len(payload['metrics']), len(payload['events'])))
+
         if cpu_time is not None:
             try:
                 cpu_used_pct = 100.0 * float(cpu_time)/float(collection_time)
                 if cpu_used_pct > MAX_CPU_PCT:
                     self.save_sample('datadog.agent.collector.cpu.used', cpu_used_pct)
-                    self.logger.warning("CPU consumed is high: %s, metrics count: %s, events count: %s" 
-                    % (cpu_used_pct, len(payload['metrics']), len(payload['events'])))
+                    self.logger.warning("CPU consumed (%%) is high: %.1f, metrics count: %d, events count: %d"
+                                        % (cpu_used_pct, len(payload['metrics']), len(payload['events'])))
             except Exception, e:
                 self.logger.debug("Couldn't compute cpu used by collector with values %s %s %s"
-                    % (cpu_time, collection_time, str(e)))
-        
+                                  % (cpu_time, collection_time, str(e)))
+
         return self.get_metrics()
