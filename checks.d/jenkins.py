413a08b63fc5cccecc5dc81474c8cc39ddec8f05
Remi Hakim <remi@datadoghq.com>
2013-03-18 20:44:03 -0400

    Move jenkins checks to checks.d

--- checks.d/jenkins.py
+++ checks.d/jenkins.py
@@ -1,8 +1,5 @@
 import os
-import re
-import socket
 import time
-import traceback
 
 try:
     from collections import defaultdict
@@ -18,16 +15,14 @@ except ImportError:
     from elementtree import ElementTree
 
 from util import get_hostname
+from checks import AgentCheck
 
-class Continue(Exception):
-    pass
-
-class Hudson(object):
-    key = 'Hudson'
+class Jenkins(AgentCheck):
     datetime_format = '%Y-%m-%d_%H-%M-%S'
 
     def __init__(self):
-        self.high_watermarks = None
+        AgentCheck.__init__(self, name, init_config, agentConfig)
+        self.high_watermarks = {}
 
     def _extract_timestamp(self, dir_name):
         try:
@@ -39,11 +34,12 @@ class Hudson(object):
             raise Exception("Error with build directory name, not a parsable date: %s" % (dir_name))
 
     def _get_build_metadata(self, dir_name):
-        # Read the build.xml metadata file that Hudson generates
+        # Read the build.xml metadata file that Jenkins generates
         build_metadata = os.path.join(dir_name, 'build.xml')
 
         if not os.access(build_metadata, os.R_OK):
-            raise Continue("Can't read build file at %s" % (build_metadata))
+            self.log.error("Can't read build file at %s" % (build_metadata))
+            raise Exception("Can't read build file at %s" % (build_metadata))
         else:
             tree = ElementTree()
             tree.parse(build_metadata)
@@ -56,7 +52,7 @@ class Hudson(object):
                         if v is not None])
             return d
 
-    def _get_build_results(self, logger, job_dir):
+    def _get_build_results(self, instance_key, job_dir):
         job_name = os.path.basename(job_dir)
 
         try:
@@ -68,11 +64,11 @@ class Hudson(object):
                     dir_name = dirs[index]
                     timestamp = self._extract_timestamp(dir_name)
                     # Check if it's a new build
-                    if timestamp > self.high_watermarks[job_name]:
+                    if timestamp > self.high_watermarks[instance_key][job_name]:
                         # If we can't get build metadata, we try the previous one
                         try:
                             build_metadata = self._get_build_metadata(dir_name)
-                        except:
+                        except Exception:
                             continue
 
                         output = {
@@ -81,54 +77,51 @@ class Hudson(object):
                                 'event_type':   'build result'
                             }
                         output.update(build_metadata)
-                        self.high_watermarks[job_name] = timestamp
+                        self.high_watermarks[instance_key][job_name] = timestamp
                         yield output
                     # If it not a new build, stop here
                     else:
                         break
         except Exception, e:
-            logger.error("Error while working on job %s, exception: %s" % (job_name, e))
+            self.log.error("Error while working on job %s, exception: %s" % (job_name, e))
 
-    def check(self, logger, agentConfig):
-        if self.high_watermarks is None:
+    def check(self, instance, create_event=True):
+        if self.high_watermarks.get(instance.get('name'), None) is None:
             # On the first run of check(), prime the high_watermarks dict
             # so that we only send events that occured after the agent
             # started.
             # (Setting high_watermarks in the next statement prevents
             #  any kind of infinite loop (assuming nothing ever sets
             #  high_watermarks to None again!))
-            self.high_watermarks = defaultdict(lambda: 0)
-            self.check(logger, agentConfig)
+            self.high_watermarks[instance.get('name')] = defaultdict(lambda: 0)
+            self.check(instance, create_event=False)
 
-        hudson_home = agentConfig.get('hudson_home', None)
+        jenkins_home = instance.get('jenkins_home', None)
 
-        if not hudson_home:
-            return False
+        if not jenkins_home:
+            raise Exception("No jenkins_home directory set in the config file")
 
-        job_dirs = glob(os.path.join(hudson_home, 'jobs', '*'))
+        job_dirs = glob(os.path.join(jenkins_home, 'jobs', '*'))
 
         build_events = []
 
         for job_dir in job_dirs:
-            for output in self._get_build_results(logger, job_dir):
-                output['api_key'] = agentConfig['api_key']
+            for output in self._get_build_results(instance.get('name'), job_dir):
+                output['api_key'] = self.agentConfig['api_key']
                 output['host'] = get_hostname(agentConfig)
-                build_events.append(output)
+                if create_event:
+                    log.debug("Creating event for job: %s" % output['job_name'])
+                    self.event(output)
 
-        return build_events
+    @staticmethod
+    def parse_agent_config(agentConfig):
+        if not agentConfig.get('hudson_home'):
+            return False
 
-if __name__ == '__main__':
-    import logging
-    import sys
+        return {
+            'instances': [{
+                'name': 'default',
+                'jenkins_home': agentConfig.get('hudson_home'),
+            }]
+        }
 
-    hudson_home, apiKey = sys.argv[1:3]
-
-    logger = logging.getLogger('ddagent.checks.hudson')
-    logger.setLevel(logging.INFO)
-    logger.addHandler(logging.StreamHandler())
-    hudson = Hudson()
-    while True:
-        print hudson.check(logger,
-                           {'hudson_home': hudson_home,
-                            'api_key': apiKey})
-        time.sleep(5)
