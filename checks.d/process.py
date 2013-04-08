983ced33b968d58668db7f609d9a4209f4168d27
Luca Cipriani <luca@c9.io>
2013-04-08 19:39:23 +0000

    Added real memory info and exceptions handling

--- checks.d/process.py
+++ checks.d/process.py
@@ -5,7 +5,7 @@ class ProcessCheck(AgentCheck):
     
     def find_pids(self, processes, search_string, exact_match=True):
         """
-        Create a set of pids of selected processe.
+        Create a set of pids of selected processes.
         Search for search_string 
         """
         found_process_list = []
@@ -13,42 +13,66 @@ class ProcessCheck(AgentCheck):
             found = False
             for string in search_string:
                 if exact_match:
-                     if proc.name == string:
-                        found = True
+                    try:
+                        if proc.name == string:
+                            found = True
+                    except psutil.NoSuchProcess:
+                        self.log.warning('Process disappeared while scanning')
+                        pass
+                    except psutil.AccessDenied, e:
+                        self.log.error('Access denied to %s process' % string)
+                        self.log.error('Error: %s' % e)
+                        raise
                 else:
                     if not found:
-                        if string in ' '.join(proc.cmdline):
-                            found = True
+                        try:
+                            if string in ' '.join(proc.cmdline):
+                                found = True
+                        except psutil.NoSuchProcess:
+                            self.log.warning('Process disappeared while scanning')
+                            pass
+                        except psutil.AccessDenied, e:
+                            self.log.error('Access denied to %s process' % string)
+                            self.log.error('Error: %s' % e)
+                            raise
+ 
                 if found or string == 'All':
                     found_process_list.append(proc.pid)
             
         return set(found_process_list)
         
     def get_process_memory_size(self, pids):
-        rss = 0
-        vms = 0
+        rss = vms = real = 0
         for pid in set(pids):
             try:
-                memory_info = psutil.Process(pid).get_ext_memory_info()
-                rss += memory_info[0]
-                vms += memory_info[1]
+                mem = psutil.Process(pid).get_ext_memory_info()
+                rss += mem.rss
+                vms += mem.vms
+                real += mem.rss - mem.shared
             # Skip processes dead in the meantime
             except psutil.NoSuchProcess:
+                self.log.warning('Process %s disappeared while scanning' % pid)
                 pass
         #Return value in Byte
-        return (rss, vms)
+        return (rss, vms, real)
         
     def check(self, instance):
-        name = instance.get('name', '')
+        name = instance.get('name', False)
+        if not name:
+            raise KeyError('The "name" of process groups is mandatory')
         exact_match = instance.get('exact_match', True)
-        search_string = instance.get('search_string')
+        search_string = instance.get('search_string', False)
+        if not search_string:
+            raise KeyError('The "search_string" is mandatory')
+        
         processes = psutil.process_iter()
         pids = self.find_pids(processes, search_string, exact_match)
         self.log.debug('ProcessCheck: process %s analysed' % name)
         self.gauge('system.processes.number', len(pids), tags=[name])
-        rss, vms = self.get_process_memory_size(pids)
+        rss, vms, real = self.get_process_memory_size(pids)
         self.gauge('system.processes.mem.rss', rss, tags=[name])
         self.gauge('system.processes.mem.vms', vms, tags=[name])
+        self.gauge('system.processes.mem.real', real, tags=[name])
 
 
 if __name__ == '__main__':
