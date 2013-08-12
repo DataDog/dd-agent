d0358bcbc592fb553fc74614a5e8a852c2a8085f
Remi Hakim <remi@datadoghq.com>
2013-08-12 20:13:58 +0200

    Revert "Include GUI into build"
    This reverts commit e233e1433fd369450543005bd0b4209f876c63c3.

--- packaging/datadog-agent/win32/build.ps1
+++ packaging/datadog-agent/win32/build.ps1
@@ -7,8 +7,10 @@ rm build/*.msi
 
 # Build the agent.exe service
 python setup.py py2exe
-mkdir packaging\datadog-agent\win32\install_files\files
-cp -r dist\* packaging\datadog-agent\win32\install_files\files
+cp dist\agent.exe packaging\datadog-agent\win32\install_files\agent.exe
+cp dist\shell.exe packaging\datadog-agent\win32\install_files\shell.exe
+mkdir packaging\datadog-agent\win32\install_files\Microsoft.VC90.CRT
+cp dist\Microsoft.VC90.CRT\* packaging\datadog-agent\win32\install_files\Microsoft.VC90.CRT\
 
 # Change to the packaging directory
 cd packaging\datadog-agent\win32
@@ -28,21 +30,23 @@ cp -R ..\..\..\dist\pup install_files\pup
 ## Generate the CLI installer with WiX
 
     # Generate fragments for the files in checks.d, conf.d and pup
-    heat dir install_files\files -gg -dr INSTALLDIR -var var.InstallFilesBins -cg files -o wix\files.wxs
     heat dir install_files\checks.d -gg -dr INSTALLDIR -var var.InstallFilesChecksD -cg checks.d -o wix\checksd.wxs
     heat dir install_files\pup -gg -dr INSTALLDIR -var var.InstallFilesPup -cg pup -o wix\pup.wxs
     heat dir install_files\conf.d -gg -dr APPLIDATIONDATADIRECTORY -t wix\confd.xslt -var var.InstallFilesConfD -cg conf.d -o wix\confd.wxs
 
     # Create .wixobj files from agent.wxs, confd.wxs, checksd.wxs
     $opts = '-dInstallFiles=install_files', '-dWixRoot=wix', '-dInstallFilesChecksD=install_files\checks.d', '-dInstallFilesConfD=install_files\conf.d', '-dInstallFilesPup=install_files\pup', "-dAgentVersion=$version"
-    candle $opts wix\agent.wxs wix\checksd.wxs wix\confd.wxs wix\pup.wxs wix\files.wxs
+    candle $opts wix\agent.wxs wix\checksd.wxs wix\confd.wxs wix\pup.wxs
 
     # Light to create the msi
-    light agent.wixobj checksd.wixobj confd.wixobj pup.wixobj files.wixobj -o ..\..\..\build\ddagent.msi
+    light agent.wixobj checksd.wixobj confd.wixobj pup.wixobj -o ..\..\..\build\ddagent.msi
 
 # Clean up
 rm *wixobj*
-rm -r install_files\*
+rm -r install_files\conf.d
+rm -r install_files\checks.d
+rm -r install_files\pup
+rm -r install_files\Microsoft.VC90.CRT
 
 # Move back to the root workspace
 cd ..\..\..\
