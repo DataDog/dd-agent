c04ea3b5fb2de75b61f25631eca75a76c438661a
Remi Hakim <remi@datadoghq.com>
2013-08-16 16:17:48 +0200

    Remove logger name from windows log format as there is only one logger for windows

--- config.py
+++ config.py
@@ -727,7 +727,9 @@ def load_check_directory(agentConfig):
 
 
 def get_log_format(logger_name):
-    return '%%(asctime)s | %%(levelname)s | dd.%s | %%(name)s(%%(filename)s:%%(lineno)s) | %%(message)s' % logger_name
+    if get_os() != 'windows':
+        return '%%(asctime)s | %%(levelname)s | dd.%s | %%(name)s(%%(filename)s:%%(lineno)s) | %%(message)s' % logger_name
+    return '%%(asctime)s | %%(levelname)s | %%(name)s(%%(filename)s:%%(lineno)s) | %%(message)s'
 
 
 def get_syslog_format(logger_name):
