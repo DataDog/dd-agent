defa2adff1924ffb8227d35b98d2d09db080fc21
Elijah Andrews <elijah@datadoghq.com>
2013-08-23 14:09:48 -0400

    Update emitter's get_opener for new proxy_settings Now is proxy_settings['field'] is not configured, proxy_settings will not have key 'field'. Before, proxy_settings['field'] was None in this case.

--- emitter.py
+++ emitter.py
@@ -74,14 +74,14 @@ def get_opener(logger, proxy_settings, use_forwarder, urllib2):
         # We are using the forwarder, so it's local trafic. We don't use the proxy
         return None
 
-    if proxy_settings['system_settings'] or proxy_settings['host'] is None:
+    if proxy_settings.get('system_settings') or proxy_settings.get('host') is None:
         # urllib2 will figure out how to connect automatically        
         return None
 
     proxy_url = '%s:%s' % (proxy_settings['host'], proxy_settings['port'])
-    if proxy_settings['user'] is not None:
+    if proxy_settings.get('user') is not None:
         proxy_auth = proxy_settings['user']
-        if proxy_settings['password'] is not None:
+        if proxy_settings.get('password') is not None:
             proxy_auth = '%s:%s' % (proxy_auth, proxy_settings['password'])
         proxy_url = '%s@%s' % (proxy_auth, proxy_url)
         
