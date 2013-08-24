e2ce57ecf90f7fdeadecab681fc8746c1b6a8a29
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2013-08-24 17:51:44 +0200

    allow basic authorization for nginx status url (similar to apache)

--- checks.d/nginx.py
+++ checks.d/nginx.py
@@ -1,6 +1,7 @@
 import re
 import time
 import urllib2
+import base64
 
 from util import headers
 from checks import AgentCheck
@@ -20,69 +21,67 @@ class Nginx(AgentCheck):
     Reading: 0 Writing: 2 Waiting: 6
 
     """
+
+    def __init__(self, name, init_config, agentConfig, instances=None):
+        AgentCheck.__init__(self, name, init_config, agentConfig, instances)
+        self.assumed_url = {}
+
     def check(self, instance):
         if 'nginx_status_url' not in instance:
-            raise Exception('NginX instance missing "nginx_status_url" value.')
+            raise Exception('nginx instance missing "nginx_status_url" value.')
+
+        url = self.assumed_url.get(instance['nginx_status_url'], instance['nginx_status_url'])
+
         tags = instance.get('tags', [])
 
-        self._get_metrics(instance['nginx_status_url'], tags)
-
-    def _get_metrics(self, url, tags):
-        req = urllib2.Request(url, None, headers(self.agentConfig))
+        req = urllib2.Request(url, None,
+            headers(self.agentConfig))
+        if 'nginx_status_user' in instance and 'nginx_status_password' in instance:
+            auth_str = '%s:%s' % (instance['nginx_status_user'], instance['nginx_status_password'])
+            encoded_auth_str = base64.encodestring(auth_str)
+            req.add_header("Authorization", "Basic %s" % encoded_auth_str)
         request = urllib2.urlopen(req)
         response = request.read()
+
+        metric_count = 0
 
         # Thanks to http://hostingfu.com/files/nginx/nginxstats.py for this code
         # Connections
         parsed = re.search(r'Active connections:\s+(\d+)', response)
         if parsed:
+            metric_count += 1
             connections = int(parsed.group(1))
             self.gauge("nginx.net.connections", connections, tags=tags)
-
+        
         # Requests per second
         parsed = re.search(r'\s*(\d+)\s+(\d+)\s+(\d+)', response)
         if parsed:
+            metric_count += 1
             requests = int(parsed.group(3))
             self.rate("nginx.net.request_per_s", requests, tags=tags)
-
+        
         # Connection states, reading, writing or waiting for clients
         parsed = re.search(r'Reading: (\d+)\s+Writing: (\d+)\s+Waiting: (\d+)', response)
         if parsed:
+            metric_count += 1
             reading, writing, waiting = map(int, parsed.groups())
             self.gauge("nginx.net.reading", reading, tags=tags)
             self.gauge("nginx.net.writing", writing, tags=tags)
             self.gauge("nginx.net.waiting", waiting, tags=tags)
 
+        if metric_count == 0:
+            if self.assumed_url.get(instance['nginx_status_url'], None) is None and url[-5:] != '?auto':
+                self.assumed_url[instance['nginx_status_url']]= '%s?auto' % url
+                self.warning("Assuming url was not correct. Trying to add ?auto suffix to the url")
+                self.check(instance)
+            else:
+                raise Exception("No metrics were fetched for this instance. Make sure that %s is the proper url." % instance['nginx_status_url'])
+
     @staticmethod
     def parse_agent_config(agentConfig):
-        instances = []
-
-        # Try loading from the very old format
-        nginx_url = agentConfig.get("nginx_status_url", None)
-        if nginx_url is not None:
-            instances.append({
-                'nginx_status_url': nginx_url
-            })
-
-        # Try the older multi-instance style
-        # nginx_status_url_1: http://www.example.com/nginx_status:first_tag
-        # nginx_status_url_2: http://www.example2.com/nginx_status:8080:second_tag
-        # nginx_status_url_2: http://www.example3.com/nginx_status:third_tag
-        def load_conf(index=1):
-            instance = agentConfig.get("nginx_status_url_%s" % index, None)
-            if instance is not None:
-                instance = instance.split(":")
-                instances.append({
-                    'nginx_status_url': ":".join(instance[:-1]),
-                    'tags': ['instance:%s' % instance[-1]]
-                })
-                load_conf(index+1)
-
-        load_conf()
-
-        if not instances:
+        if not agentConfig.get('nginx_status_url'):
             return False
 
         return {
-            'instances': instances
+            'instances': [{'nginx_status_url': agentConfig.get('nginx_status_url')}]
         }
