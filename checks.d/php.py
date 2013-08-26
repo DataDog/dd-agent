e734f8e59057c45f3c4b8608201584ae8644204d
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2013-08-26 18:18:56 +0200

    remove unnecessary code (aka refactoring)

--- checks.d/php.py
+++ checks.d/php.py
@@ -33,10 +33,6 @@ class Php(AgentCheck):
         'max children reached': 'php.max_children_reached'
     }
 
-    RATES = {
-        
-    }
-
     COUNTERS = {
         'accepted conn': 'php.accepted_conn',
         'slow requests': 'php.slow_requests'
@@ -63,7 +59,6 @@ class Php(AgentCheck):
         request = urllib2.urlopen(req)
         response = request.read()
 
-        metric_count = 0
         # Loop through and extract the numerical values
         for line in response.split('\n'):
             values = line.split(': ')
@@ -74,37 +69,12 @@ class Php(AgentCheck):
                 except ValueError:
                     continue
 
-                # Send metric as a gauge, if applicable
-                if metric in self.GAUGES:
-                    metric_count += 1
-                    metric_name = self.GAUGES[metric]
-                    self.gauge(metric_name, value, tags=tags)
-
                 # Send metric as a rate, if applicable
                 if metric in self.RATES:
-                    metric_count += 1
                     metric_name = self.RATES[metric]
                     self.rate(metric_name, value, tags=tags)
 
                 # Send metric as a increment, if applicable
                 if metric in self.COUNTERS:
-                    metric_count += 1
                     metric_name = self.COUNTERS[metric]
                     self.increment(metric_name, value, tags=tags)
-
-        if metric_count == 0:
-            if self.assumed_url.get(instance['php_status_url'], None) is None and url[-5:] != '?auto':
-                self.assumed_url[instance['php_status_url']]= '%s?auto' % url
-                self.warning("Assuming url was not correct. Trying to add ?auto suffix to the url")
-                self.check(instance)
-            else:
-                raise Exception("No metrics were fetched for this instance. Make sure that %s is the proper url." % instance['php_status_url'])
-
-    @staticmethod
-    def parse_agent_config(agentConfig):
-        if not agentConfig.get('php_status_url'):
-            return False
-
-        return {
-            'instances': [{'php_status_url': agentConfig.get('php_status_url')}]
-        }
