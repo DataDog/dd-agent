ebe964b20b813b3bec4c594dc9d9dd6a108d1a34
Remi Hakim <remi@datadoghq.com>
2013-12-06 15:48:34 -0500

    Add the possibility to add tags to postfix metrics

--- checks.d/postfix.py
+++ checks.d/postfix.py
@@ -20,23 +20,26 @@ class PostfixCheck(AgentCheck):
 
         directory = config['directory']
         queues = config['queues']
+        tags = config['tags']
 
-        self._get_queue_count(directory, queues)
+        self._get_queue_count(directory, queues, tags)
 
     def _get_config(self, instance):
         directory = instance.get('directory', None)
         queues = instance.get('queues', None)
+        tags = instance.get('tags', [])
         if not queues or not directory:
             raise Exception('missing required yaml config entry')
 
         instance_config = {
             'directory': directory,
-            'queues': queues
+            'queues': queues,
+            'tags': tags,
         }
 
         return instance_config
 
-    def _get_queue_count(self, directory, queues):
+    def _get_queue_count(self, directory, queues, tags):
         for queue in queues:
             queue_path = os.path.join(directory, queue)
             if not os.path.exists(queue_path):
@@ -57,7 +60,7 @@ class PostfixCheck(AgentCheck):
                     self.log.warning('the dd-agent user does not have sudo access')
 
             # emit an individually tagged metric
-            self.gauge('postfix.queue.size', count, tags=['queue:%s' % queue, 'instance:%s' %  os.path.basename(directory)])
+            self.gauge('postfix.queue.size', count, tags=tags + ['queue:%s' % queue, 'instance:%s' %  os.path.basename(directory)])
 
             # these can be retrieved in a single graph statement
             # for example:
