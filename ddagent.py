f6c372d761dbfc96c6957f879a8641d5b9b099fa
Remi Hakim <remi@datadoghq.com>
2014-03-10 12:34:45 -0400

    Always use tornado curl http client
    Fix #856

--- ddagent.py
+++ ddagent.py
@@ -212,12 +212,11 @@ class MetricTransaction(Transaction):
                 tornado_client_params['ca_certs'] = ssl_certificate
 
                 req = tornado.httpclient.HTTPRequest(**tornado_client_params)
-                tornado.httpclient.AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
 
             else:
                 req = tornado.httpclient.HTTPRequest(**tornado_client_params)
-                log.debug("Using Tornado simple HTTP Client")
                 
+            tornado.httpclient.AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
             http = tornado.httpclient.AsyncHTTPClient()
             
 
