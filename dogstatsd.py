15b02fba14afbff289455ffd8c8b70ea253f376d
Carlo Cabanilla <carlo@datadoghq.com>
2014-04-02 22:06:47 +0000

    only compress dogstatsd payloads that are larger than 1k

--- dogstatsd.py
+++ dogstatsd.py
@@ -42,9 +42,17 @@ FLUSH_LOGGING_PERIOD = 70
 FLUSH_LOGGING_INITIAL = 10
 FLUSH_LOGGING_COUNT = 5
 EVENT_CHUNK_SIZE = 50
+COMPRESS_THRESHOLD = 1024
 
 def serialize_metrics(metrics):
-    return zlib.compress(json.dumps({"series" : metrics}))
+    serialized = json.dumps({"series" : metrics})
+    if len(serialized) > COMPRESS_THRESHOLD:
+        headers = {'Content-Type': 'application/json',
+                   'Content-Encoding': 'deflate'}
+        serialized = zlib.compress(serialized)
+    else:
+        headers = {'Content-Type': 'application/json'}
+    return serialized, headers
 
 def serialize_event(event):
     return json.dumps(event)
@@ -147,9 +155,7 @@ class Reporter(threading.Thread):
     def submit(self, metrics):
         # Copy and pasted from dogapi, because it's a bit of a pain to distribute python
         # dependencies with the agent.
-        body = serialize_metrics(metrics)
-        headers = {'Content-Type': 'application/json',
-                   'Content-Encoding': 'deflate'}
+        body, headers = serialize_metrics(metrics)
         method = 'POST'
 
         params = {}
