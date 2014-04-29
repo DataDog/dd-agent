3a251aac1995c0e01f22ee026090ecf284ce47f6
Rudy <rudy@datadoghq.com>
2014-04-29 14:25:01 -0400

    SNMP| Add interface monitoring

--- checks.d/snmp.py
+++ checks.d/snmp.py
@@ -6,18 +6,63 @@ import pysnmp.proto.rfc1902 as snmp_type
 snmp_counters = [snmp_type.Counter32, snmp_type.Counter64]
 snmp_gauges = [snmp_type.Gauge32]
 
-target_oids = [
-                (('SNMPv2-MIB','snmpInPkts'),'0'),
-                (('UDP-MIB','udpInDatagrams'),'0')
-                ]
+interface_oids = [
+        ("IF-MIB", "ifInOctets"),
+        ("IF-MIB", "ifInErrors"),
+        ("IF-MIB", "ifInDiscards"),
+        ("IF-MIB", "ifOutOctets"),
+        ("IF-MIB", "ifOutErrors"),
+        ("IF-MIB", "ifOutDiscards")
+        ]
+
+device_oids = [
+        (("UDP-MIB", "udpInDatagrams"),0),
+        (("TCP-MIB", "tcpCurrEstab"),0),
+        (("TCP-MIB", "tcpActiveOpens"),0),
+        (("TCP-MIB", "tcpPassiveOpens"),0)
+        ]
+
+
+
 
 class SnmpCheck(AgentCheck):
-    
+
     def __init__(self, name, init_config, agentConfig, instances=None):
         AgentCheck.__init__(self, name, init_config, agentConfig, instances)
         self.counter_state = {}
         self.interface_list = {}
-    
+        for instance in instances:
+            if 'ip_address' in instance:
+                ip_address = instance["ip_address"]
+                self.counter_state[ip_address] = {}
+                self.interface_list[ip_address] = self.get_interfaces(instance)
+
+    def get_interfaces(self, instance):
+
+        interface_list = {}
+
+        def get_interfaces_nb():
+            result = SnmpCheck.snmp_get(instance, [(("IF-MIB","ifNumber"),0)])[0]
+            return int(result[1])
+
+        interface_nb = get_interfaces_nb()
+        interfaces_descr_oids = []
+        for interface in range(interface_nb):
+            interface_index = interface + 1 #SNMP indexes start from 1
+            interfaces_descr_oids.append((("IF-MIB","ifDescr"),interface_index))
+            interfaces_descr_oids.append((("IF-MIB","ifType"),interface_index))
+
+        interfaces_description = SnmpCheck.snmp_get(instance, interfaces_descr_oids)
+        self.log.info(interfaces_description)
+        for i in range(interface_nb):
+            # order is guaranteed
+            descr = str(interfaces_description.pop(0)[1])
+            type = int(interfaces_description.pop(0)[1])
+            if type != 24:
+                interface_list[i+1] = descr
+
+        return interface_list
+
     @staticmethod
     def get_auth_data(instance):
         if "community_string" in instance:
@@ -34,16 +79,33 @@ class SnmpCheck(AgentCheck):
         return cmdgen.UdpTransportTarget((ip_address,port))
 
     def check(self, instance):
-        
+        results = SnmpCheck.snmp_get(instance, device_oids)
+        for oid, value in results:
+            self.report_as_statsd(instance, oid, value, tags=None)
+
+        for interface, descr in self.interface_list[instance['ip_address']].items():
+            oids = [(oid, interface) for oid in interface_oids]
+            interface_results = SnmpCheck.snmp_get(instance, oids)
+            for oid, value in interface_results:
+                self.report_as_statsd(instance, oid, value, tags = ["interface:"+descr])
+
+    @staticmethod
+    def snmp_get(instance, oids):
+        """
+        Perform a snmp get command to the device that instance
+        describe and return a list of tuble (name, values)
+        corresponding to the elements in oids.
+        """
         transport_target = SnmpCheck.get_transport_target(instance)
         auth_data = SnmpCheck.get_auth_data(instance)
 
         cmd_generator = cmdgen.CommandGenerator()
 
-        errorIndication, errorStatus, errorIndex, varBinds = cmd_generator.getCmd(
+        snmp_command = cmd_generator.getCmd
+        errorIndication, errorStatus, errorIndex, varBinds = snmp_command(
                 auth_data,
                 transport_target,
-                *target_oids,
+                *oids,
                 lookupNames=True,
                 lookupValues=True
                 )
@@ -54,26 +116,26 @@ class SnmpCheck(AgentCheck):
             if errorStatus:
                 self.log.warning(errorStatus.prettyPrint())
             else:
-                for oid, value in varBinds:
-                    self.report_as_statsd(oid.prettyPrint(), value)
+                return varBinds
 
-
-    def report_as_statsd(self, name, snmp_value):
+    def report_as_statsd(self, instance, oid, snmp_value, tags=[]):
+        name = "snmp." + oid.getMibSymbol()[1]
         snmp_class = getattr(snmp_value, '__class__')
         value = int(snmp_value)
         if snmp_class in snmp_counters:
-            self.counter(name, value, snmp_class)
+            self.counter(instance, name, value, snmp_class, tags)
         elif snmp_class in snmp_gauges:
-            self.gauge(name, value)
+            self.gauge(name, value, tags)
 
 
-    def counter(self, name, value, snmp_class):
-        if name in self.counter_state:
-            diff = value - self.counter_state[name]
+    def counter(self, instance, name, value, snmp_class, tags = []):
+        current_state = self.counter_state[instance['ip_address']]
+        if name in current_state:
+            diff = value - current_state[name]
             if diff < 0:
                 # Counters monotonically increase so it means the counter wrapped
                 diff += pow(2, 32 if snmp_class==snmp_type.Counter32 else 64)
-            self.increment(name, diff)
+            self.increment(name, diff,tags=tags)
         else:
             self.log.info("Setting up initial value for Counter {0}".format(name))
-        self.counter_state[name] = value
+        current_state[name] = value
