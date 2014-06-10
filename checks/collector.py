878cd52e5f1ed2646a670aa4dd247742329acf32
tristan <tristan@datadoghq.com>
2014-06-10 12:05:38 -0400

    Review fix of gohai
    use dict instead of getattr and stator

--- checks/collector.py
+++ checks/collector.py
@@ -45,10 +45,16 @@ class Collector(object):
         self.os = get_os()
         self.plugins = None
         self.emitters = emitters
-        self.metadata_interval = int(agentConfig.get('metadata_interval', 4 * 60 * 60))
-        self.metadata_start = time.time()
-        self.agent_checks_start = time.time()
-        self.agent_checks_interval = int(agentConfig.get('agent_checks_interval', 10 * 60))
+        self.push_times = {
+            'metadata': {
+                'start': time.time(),
+                'interval': int(agentConfig.get('metadata_interval', 4 * 60 * 60))
+            },
+            'agent_checks': {
+                'start': time.time(),
+                'interval': int(agentConfig.get('agent_checks_interval', 10 * 60))
+            }
+        }
         socket.setdefaulttimeout(15)
         self.run_count = 0
         self.continue_running = True
@@ -470,15 +476,11 @@ class Collector(object):
     def _should_send_additional_data(self, data_name):
         if self._is_first_run():
             return True
-        DATA_TIME_TUPLES = {
-            'metadata': ('metadata_start', 'metadata_interval'),
-            'agent_checks': ('agent_checks_start', 'agent_checks_interval')
-        }
         # If the interval has passed, send the metadata again
         now = time.time()
-        if now - getattr(self, DATA_TIME_TUPLES[data_name][0]) >= getattr(self, DATA_TIME_TUPLES[data_name][1]):
-            log.debug('Metadata interval has passed. Sending metadata.')
-            setattr(self, DATA_TIME_TUPLES[data_name][0], now)
+        if now - self.push_times[data_name]['start'] >= self.push_times[data_name]['interval']:
+            log.debug('%s interval has passed. Sending it.' % data_name)
+            self.push_times[data_name]['start'] = now
             return True
 
         return False
