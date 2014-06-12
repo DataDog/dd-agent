2480c2b967682f5c401c28c6ad9b363456304073
Remi Hakim <remi@datadoghq.com>
2014-06-12 12:25:03 -0400

    Let the possibility to disable instance metadata collection
    Fix #975

--- tests/test_ec2.py
+++ tests/test_ec2.py
@@ -12,7 +12,7 @@ class TestEC2(unittest.TestCase):
         if os.environ.get('TRAVIS', False): return
         # Test gathering metadata from ec2
         start = time.time()
-        d = EC2.get_metadata()
+        d = EC2.get_metadata({'collect_instance_metadata': True})
         end = time.time()
         assert type(d) == types.DictType
         # Either we're on ec2 or we're not (at least 7 attributes expected)
