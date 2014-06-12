8010f6174fadbd2a015f872fc2bbdca5609ddb65
Remi Hakim <remi@datadoghq.com>
2014-06-12 14:17:46 -0400

    Fix test

--- tests/test_postgresql.py
+++ tests/test_postgresql.py
@@ -39,7 +39,7 @@ class TestPostgres(unittest.TestCase):
         metrics = self.check.get_metrics()
 
         self.assertTrue(len(metrics) == 20, metrics)
-        self.assertTrue(len([m for m in metrics if 'db:datadog_test' in str(m[3]['tags']) ]) == 18, metrics)
+        self.assertTrue(len([m for m in metrics if 'db:datadog_test' in str(m[3]['tags']) ]) == 20, metrics)
         self.assertTrue(len([m for m in metrics if 'table:persons' in str(m[3]['tags']) ]) == 8, metrics)
 
 if __name__ == '__main__':
