1c09b09ae2a5a2405890dea52d30d4963c855cde
Rudy <rudy@datadoghq.com>
2014-06-13 18:47:40 -0400

    SNMP Checks| Add documentation

--- checks.d/snmp.py
+++ checks.d/snmp.py
@@ -10,15 +10,17 @@ from pysnmp.smi.exval import noSuchInstance, noSuchObject
 from pysnmp.smi import builder
 import pysnmp.proto.rfc1902 as snmp_type
 
+# Additional types that are not part of the SNMP protocol. cf RFC 2856
 (CounterBasedGauge64, ZeroBasedCounter64) = builder.MibBuilder().importSymbols("HCNUM-TC","CounterBasedGauge64", "ZeroBasedCounter64")
 
+# Metric type that we support
 SNMP_COUNTERS = [snmp_type.Counter32.__name__, snmp_type.Counter64.__name__, ZeroBasedCounter64.__name__]
 SNMP_GAUGES = [snmp_type.Gauge32.__name__, CounterBasedGauge64.__name__]
 
 # IF-MIB magic values
+IF_TABLE_OID = '.1.3.6.1.2.1.2.2.1.'
 IF_TABLE_TYPE_POS = 9
 IF_TABLE_INDEX_POS = 10
-IF_INDEX = 1
 IF_DESCR = 2
 IF_TYPE = 3
 LOCALHOST_INTERFACE = 24
@@ -33,12 +35,16 @@ class SnmpCheck(AgentCheck):
 
     def __init__(self, name, init_config, agentConfig, instances=None):
         AgentCheck.__init__(self, name, init_config, agentConfig, instances)
-        self.counter_state = defaultdict(dict)
+
         self.interface_list = {}
+
+        # Load Custom MIB directory
         mibs_path = None
         if init_config is not None:
             mibs_path = init_config.get("mibs_folder")
         SnmpCheck.create_command_generator(mibs_path)
+
+        # Detect the interfaces of the instance and retain their indexes
         if instances is not None:
             for instance in instances:
                 if 'ip_address' in instance:
@@ -47,6 +53,11 @@ class SnmpCheck(AgentCheck):
 
     @classmethod
     def create_command_generator(cls, mibs_path=None):
+        '''
+        Create a command generator to perform all the snmp query
+        If mibs_path is not None, load the mibs present in the custom mibs
+        folder (Need to be in pysnmp format)
+        '''
         cls.cmd_generator = cmdgen.CommandGenerator()
         if mibs_path is not None:
             mibBuilder = cls.cmd_generator.snmpEngine.msgAndPduDsp.\
@@ -57,6 +68,17 @@ class SnmpCheck(AgentCheck):
             mibBuilder.setMibSources(*mibSources)
 
     def get_interfaces(self, instance):
+        '''
+        Query the IF-MIB table to get the list of interfaces and their description
+        Ignore the interfaces that are loopback (localhost)
+        The nextCmd will return all the value in a format like this:
+
+            [(1.3.6.1.2.1.2.2.1.{what type of information}.{index of the interface}, information)]
+
+        For now we only care about the index, the description(eg. interface name eth0) and the type
+        to ignore the loopback interface
+        See http://www.alvestrand.no/objectid/1.3.6.1.2.1.2.2.1.html for more info about the table
+        '''
         interface_list = {}
         transport_target = self.get_transport_target(instance)
         auth_data = self.get_auth_data(instance)
@@ -65,7 +87,7 @@ class SnmpCheck(AgentCheck):
         errorIndication, errorStatus, errorIndex, varBinds = snmp_command(
             auth_data,
             transport_target,
-            '.1.3.6.1.2.1.2.2.1.',
+            IF_TABLE_OID,
             lookupValues = True
             )
 
@@ -93,6 +115,10 @@ class SnmpCheck(AgentCheck):
 
     @classmethod
     def get_auth_data(cls, instance):
+        '''
+        Generate a Security Parameters object based on the configuration of the instance
+        See http://pysnmp.sourceforge.net/docs/current/security-configuration.html
+        '''
         if "community_string" in instance:
             # SNMP v1 - SNMP v2
             return cmdgen.CommunityData(instance['community_string'])
@@ -120,6 +146,9 @@ class SnmpCheck(AgentCheck):
 
     @classmethod
     def get_transport_target(cls, instance):
+        '''
+        Generate a Transport target object based on the configuration of the instance
+        '''
         if "ip_address" not in instance:
             raise Exception("An IP address needs to be specified")
         ip_address = instance["ip_address"]
@@ -130,13 +159,15 @@ class SnmpCheck(AgentCheck):
         tags = instance.get("tags",[])
         ip_address = instance["ip_address"]
         device_oids = []
-        interface_oids = []
         oid_names ={}
+
+        # Check the metrics completely defined
         for metric in instance.get('metrics',[]):
             if 'MIB' in metric:
                 device_oids.append(((metric["MIB"],metric["symbol"]),metric["index"]))
             elif 'OID' in metric:
                 device_oids.append(metric['OID'])
+                # Associate the name to the OID so that we can perform the matching
                 oid_names[metric['OID']]=metric['name']
             else:
                 raise Exception('Unsupported metrics format in config file')
@@ -146,6 +177,9 @@ class SnmpCheck(AgentCheck):
             self.submit_metric(instance, oid, value, tags=tags + ["snmp_device:" + ip_address],
                                                      oid_names = oid_names)
 
+        # Check the metrics defined per interface by appending the index
+        # of the table to create a fully defined metric
+        interface_oids = []
         for metric in instance.get('interface_metrics',[]):
             interface_oids.append((metric["MIB"],metric["symbol"]))
         for interface, descr in self.interface_list[instance['ip_address']].items():
@@ -185,9 +219,17 @@ class SnmpCheck(AgentCheck):
                 return varBinds
 
     def submit_metric(self, instance, oid, snmp_value, tags=[], oid_names={}):
+        '''
+        Convert the values reported as pysnmp-Managed Objects to values and
+        report them to the aggregator
+        '''
         if reply_invalid(snmp_value):
+            # Metrics not present in the queried object
             self.log.warning("No such Mib available: %s" %oid.getMibSymbol()[1])
             return
+
+        # Get the name for the OID either from the name that we can decode
+        # or from the name that was specified for it
         if str(oid.getOid()) in oid_names:
             name = "snmp."+ oid_names[str(oid.getOid())]
         else:
@@ -197,6 +239,9 @@ class SnmpCheck(AgentCheck):
                 self.log.warning("Couldn't find a name for oid {0}".format(oid))
                 return
 
+        # Ugly hack but couldn't find a cleaner way
+        # Proper way would be to use the ASN1 method isSameTypeWith but this
+        # returns True in the case of CounterBasedGauge64 and Counter64 for example
         snmp_class = snmp_value.__class__.__name__
         for counter_class in SNMP_COUNTERS:
             if snmp_class==counter_class:
