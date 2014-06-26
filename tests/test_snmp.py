7b043500899b6d134826bafb42119024c090471d
Rudy <rudy@datadoghq.com>
2014-06-26 17:49:23 -0400

    SNMP Checks| Add test case for querying table

--- tests/test_snmp.py
+++ tests/test_snmp.py
@@ -11,10 +11,26 @@ class TestSNMP(unittest.TestCase):
             'api_key': 'toto'
         }
 
+    def testInit(self):
         self.config = {
                 "init_config": {
                     'mibs_folder':'/etc/mibs'
-                    },
+                    }
+                }
+        # Initialize the check from checks.d
+        self.check = load_check('snmp', self.config, self.agentConfig)
+
+        mib_folders = self.check.cmd_generator.snmpEngine.msgAndPduDsp\
+                .mibInstrumController.mibBuilder.getMibSources()
+        custom_folder_represented = False
+        for folder in mib_folders:
+            if '/etc/mibs' == folder.fullPath():
+                custom_folder_represented = True
+                break
+        self.assertTrue(custom_folder_represented)
+
+    def test_scalar_SNMPCheck(self):
+        self.config = {
                 "instances": [{
                     "ip_address": "localhost",
                     "port":161,
@@ -31,21 +47,6 @@ class TestSNMP(unittest.TestCase):
                         }]
                     }]
                 }
-
-    def testInit(self):
-        # Initialize the check from checks.d
-        self.check = load_check('snmp', self.config, self.agentConfig)
-
-        mib_folders = self.check.cmd_generator.snmpEngine.msgAndPduDsp\
-                .mibInstrumController.mibBuilder.getMibSources()
-        custom_folder_represented = False
-        for folder in mib_folders:
-            if '/etc/mibs' == folder.fullPath():
-                custom_folder_represented = True
-                break
-        self.assertTrue(custom_folder_represented)
-
-    def testSNMPCheck(self):
 
         self.check = load_check('snmp', self.config, self.agentConfig)
 
@@ -71,6 +72,56 @@ class TestSNMP(unittest.TestCase):
                     break
             else:
                 self.fail("Missing metric: %s" % metric)
+
+    def test_table_SNMPCheck(self):
+        self.config = {
+                "instances": [{
+                    "ip_address": "localhost",
+                    "port":161,
+                    "community_string": "public",
+                    "metrics": [{
+                        "MIB": "IF-MIB",
+                        "table": "ifTable",
+                        "symbols": ["ifInOctets", "ifOutOctets"],
+                        "metric_tags": [{
+                            "tag":"interface",
+                            "column":"ifDescr"
+                            }, {
+                            "tag":"dumbindex",
+                            "index":1
+                            }]
+                        }]
+                    }]
+                }
+
+        self.check = load_check('snmp', self.config, self.agentConfig)
+
+        self.check.check(self.config['instances'][0])
+        # Sleep for 1 second so the rate interval >=1
+        time.sleep(1)
+        # Run the check again so that we get the rates
+        self.check.check(self.config['instances'][0])
+        metrics = self.check.get_metrics()
+        # nb of metrics depends on the nb of interfaces on the test machine
+        # so it's not possible to specify an excat number
+        self.assertGreater(len(metrics), 0)
+        for metric in metrics:
+            self.assertTrue(metric[0] in ['snmp.ifInOctets', 'snmp.ifOutOctets'],
+                            metric[0])
+            tags = metric[3]['tags']
+            # Assert that all the wanted tags are here
+            self.assertEquals(len(tags), 3, tags)
+            tag_group_expected = ["snmp_device", "dumbindex", "interface"]
+            for tag in tags:
+                tag_group = tag.split(":")[0]
+                self.assertTrue(tag_group in tag_group_expected, tag_group)
+                if tag_group == "interface":
+                    # Assert that the column tags are properly represented and
+                    # not left to their numerical equivalent form.
+                    # If this assertion fails, it may be that the list is non exhaustive
+                    interface_type = tag.split(":")[1]
+                    self.assertTrue(interface_type in ["lo", "eth0", "eth1"],
+                                    interface_type)
 
 if __name__ == "__main__":
     unittest.main()
