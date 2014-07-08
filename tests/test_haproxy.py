8d0000fbd0055e4b7253849adc76dc28cfbea01d
tristan <tristan@datadoghq.com>
2014-07-08 14:59:15 -0400

    Add some more test to count_per_status

--- tests/test_haproxy.py
+++ tests/test_haproxy.py
@@ -135,6 +135,10 @@ b,BACKEND,0,0,1,2,0,421,1,0,0,0,,0,0,0,0,UP,6,6,0,,0,1,0,,1,3,0,,421,,1,0,,1,,,,
         expected_hosts_statuses[('a', 'UP')] = 1
         self.assertEquals(self.check.hosts_statuses, expected_hosts_statuses)
 
+        # with collect_aggregates_only set to True
+        self.check._process_data(data, True, True, collect_status_metrics=True, collect_status_metrics_by_host=False)
+        self.assertEquals(self.check.hosts_statuses, expected_hosts_statuses)
+
         # per host
         self.check._process_data(data, True, False, collect_status_metrics=True, collect_status_metrics_by_host=True)
         expected_hosts_statuses = defaultdict(int)
@@ -143,7 +147,9 @@ b,BACKEND,0,0,1,2,0,421,1,0,0,0,,0,0,0,0,UP,6,6,0,,0,1,0,,1,3,0,,421,,1,0,,1,,,,
         expected_hosts_statuses[('b', 'i-1', 'UP')] = 1
         expected_hosts_statuses[('b', 'i-2', 'UP')] = 1
         expected_hosts_statuses[('b', 'i-3', 'UP')] = 1
+        self.assertEquals(self.check.hosts_statuses, expected_hosts_statuses)
 
+        self.check._process_data(data, True, True, collect_status_metrics=True, collect_status_metrics_by_host=True)
         self.assertEquals(self.check.hosts_statuses, expected_hosts_statuses)
 
     def testWrongConfig(self):
