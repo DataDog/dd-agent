a08f8237361711992b4692ecc2cd001ad4d5033e
Rudy <rudy@datadoghq.com>
2014-07-22 16:06:16 -0400

    2008SP2 IIS| Add workaround for typo in metric name in windows

--- checks.d/iis.py
+++ checks.d/iis.py
@@ -83,9 +83,12 @@ class IIS(AgentCheck):
 
             for metric, mtype, wmi_val in self.METRICS:
                 if not hasattr(iis_site, wmi_val):
-                    self.warning('Unable to fetch metric %s. Missing %s in Win32_PerfFormattedData_W3SVC_WebService' \
-                        % (metric, wmi_val))
-                    continue
+                    if wmi_val == 'TotalBytesTransferred' and hasattr(iis_site, 'TotalBytesTransfered'): #fix for win2008sp2 bug
+                        wmi_val = 'TotalBytesTransfered'
+                    else:
+                        self.warning('Unable to fetch metric %s. Missing %s in Win32_PerfFormattedData_W3SVC_WebService' \
+                            % (metric, wmi_val))
+                        continue
 
                 # Submit the metric value with the correct type
                 value = float(getattr(iis_site, wmi_val))
