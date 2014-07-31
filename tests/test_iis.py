41edca4f20dfc4c4fc6d928dd56275797527329b
Rudy <rudy@datadoghq.com>
2014-07-31 19:07:23 -0400

    IIS SC| Fix failing tests and add test for the service check

--- tests/test_iis.py
+++ tests/test_iis.py
@@ -22,6 +22,12 @@ class IISTestCase(unittest.TestCase):
         check, instances = get_check('iis', CONFIG)
         check.check(instances[0])
         metrics = check.get_metrics()
+        service_checks = check.get_service_checks()
+
+        # Second run to get the rates
+        check.check(instances[0])
+        metrics = check.get_metrics()
+        service_checks = check.get_service_checks()
 
         base_metrics = [m[0] for m in check.METRICS]
         ret_metrics = [m[0] for m in metrics]
@@ -29,11 +35,17 @@ class IISTestCase(unittest.TestCase):
 
         # Make sure each metric was captured
         for metric in base_metrics:
-            assert metric in ret_metrics
+            self.assertTrue(metric in ret_metrics, "not reporting %s" % metric)
 
         # Make sure everything is tagged correctly
         for tags in ret_tags:
-            assert tags == ['mytag1', 'mytag2']
+            self.assertEquals(assert tags == ['mytag1', 'mytag2'], tags)
+
+        # Make sure that we get a service check
+        self.assertEquals(len(service_checks),1)
+        self.assertEquals(check.SERVICE_CHECK, service_checks[0]['check'])
+        self.assertEquals(['site:Default Web Site'], service_checks[0]['tags'])
+
 
 if __name__ == "__main__":
     unittest.main()
