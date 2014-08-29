ddd97edd58ad71955c99c4d743271ffeeeb69e21
Jon Glick <jglick@basho.com>
2014-08-28 19:36:18 -0500

    Updates to address issues in comments

--- checks.d/riakcs.py
+++ checks.d/riakcs.py
@@ -10,111 +10,88 @@ from checks import AgentCheck
 import simplejson as json
 from boto.s3.connection import S3Connection
 
-
 class RiakCs(AgentCheck):
 
-    keys = [
-        "object_put_acl",
-        "bucket_create",
-        "bucket_list_keys",
-        "service_get_buckets",
-        "block_delete",
-        "block_put",
-        "block_get_retry",
-        "block_get",
-        "bucket_delete",
-        "bucket_get_acl",
-        "bucket_put_acl",
-        "object_get",
-        "object_put",
-        "object_head",
-        "object_delete",
-        "object_get_acl"
-    ]
-    pool_keys = [
-        "bucket_list_pool",
-        "request_pool"
-    ]
+    STATS_BUCKET = 'riak-cs'
+    STATS_KEY = 'stats'
 
-    def __init__(self, name, init_config, agentConfig, instances=None):
-        AgentCheck.__init__(self, name, init_config, agentConfig, instances)
+    STAT_KEYS = [
+        # Object Keys
+        "object_get", "object_put", "object_delete", "object_head",
+        "object_get_acl", "object_put_acl",
+        # Bucket Keys
+        "bucket_create", "service_get_buckets", "bucket_delete",
+        "bucket_list_keys", "bucket_get_acl", "bucket_put_acl",
+        # Block Keys
+        "block_get", "block_put", "block_delete", "block_get_retry" ]
+    STAT_GAUGES = [
+        "rate", "latency_mean", "latency_median",
+        "latency_95", "latency_99" ]
+    POOL_KEYS = [
+        "bucket_list_pool", "request_pool" ]
+    POOL_GAUGES = [
+        "workers", "overflow", "size" ]
 
     def check(self, instance):
 
       s3_settings=dict(
         aws_access_key_id=instance.get('access_id', None),
         aws_secret_access_key=instance.get('access_secret', None),
-        port=instance.get('s3_port', None),
-        proxy=instance.get('proxy_host', None),
-        proxy_port=instance.get('proxy_port', None),
-        proxy_user=instance.get('proxy_user', None),
-        proxy_pass=instance.get('proxy_pass', None),
+        proxy=instance.get('host','localhost'),
+        proxy_port=instance.get('port', 8080),
         is_secure=instance.get('is_secure', None))
 
-      if instance.get('s3_host'):
-        s3_settings['host'] = instance['s3_host']
+      if instance.get('s3_root'):
+        s3_settings['host'] = instance['s3_root']
 
-      aggregation_key = md5(json.dumps(s3_settings)).hexdigest()
+      aggregation_key = s3_settings['proxy'] + ":" + str(s3_settings['proxy_port'])
 
-      default_timeout = self.init_config.get('default_timeout', 5)
-      timeout         = float(instance.get('timeout', default_timeout))
+      s3 = self._connect(s3_settings, aggregation_key)
+
+      stats = self._get_stats(s3, aggregation_key)
+
+      if stats:
+
+        for key in self.STAT_KEYS:
+          if key not in stats:
+            continue
+          vals = stats[key]
+          self.count('riakcs.' + key, vals.pop(0))
+          for gauge in self.STAT_GAUGES:
+            self.gauge('riakcs.' + key + "_" + gauge, vals.pop(0))
+
+        for key in self.POOL_KEYS:
+          if key not in stats:
+            continue
+          vals = stats[key]
+          for gauge in self.POOL_GAUGES:
+            self.gauge('riakcs.' + key + "_" + gauge, vals.pop(0))
+
+    def _connect(self, s3_settings, aggregation_key):
 
       try:
+        s3 = S3Connection(**s3_settings)
 
-        self.s3 = S3Connection(**s3_settings)
-        stats_key = self.s3.get_bucket('riak-cs', validate=False).get_key('stats')
-        stats_str = stats_key.get_contents_as_string()
-        stats = json.loads(stats_str)
-
-      except socket.timeout, e:
-          self.timeout_event(timeout, aggregation_key)
+      except Exception, e:
+          self._error("Error connecting to " + aggregation_key, e)
           return
 
-      except socket.error, e:
-          self.timeout_event(timeout, aggregation_key)
+      return s3
+
+    def _get_stats(self, s3, aggregation_key):
+
+      try:
+          bucket = s3.get_bucket(self.STATS_BUCKET, validate=False)
+          key = bucket.get_key(self.STATS_KEY)
+          stats_str = key.get_contents_as_string()
+          stats = json.loads(stats_str)
+
+      except Exception, e:
+          self._error("Error retrieving stats from " + aggregation_key, e)
           return
 
-      except HttpLib2Error, e:
-          self.timeout_event(timeout, aggregation_key)
-          return
+      return stats
 
-      except boto.exception, e:
-          self.timeout_event(timeout, aggregation_key)
-          return
-
-      for key in self.keys:
-        if key not in stats:
-          continue
-        vals = stats[key]
-        self.count('riakcs.' + key, vals.pop(0))
-        for gauge in [
-          "rate", "latency_mean", "latency_median",
-          "latency_95", "latency_99" ]:
-          self.gauge('riakcs.' + key + "_" + gauge, vals.pop(0))
-
-      self.gauge('riakcs.test', 1)
-
-      for key in self.pool_keys:
-        if key not in stats:
-          continue
-        vals = stats[key]
-        for gauge in ["workers", "overflow", "size"]:
-          self.gauge('riakcs.' + key + "_" + gauge, vals.pop(0))
-
-    def timeout_event(self, timeout, aggregation_key):
-        self.event({
-            'timestamp': int(time.time()),
-            'event_type': 'riak_check',
-            'msg_title': 'riak check timeout',
-            'msg_text': 'stats out after %s seconds.' % (timeout),
-            'aggregation_key': aggregation_key
-        })
-
-    def status_code_event(self, r, aggregation_key):
-        self.event({
-            'timestamp': int(time.time()),
-            'event_type': 'riak_check',
-            'msg_title': 'Invalid reponse code for riak check',
-            'msg_text': 'stats returned a status of %s' % (r.status_code),
-            'aggregation_key': aggregation_key
-        })
+    def _error(self, message, error):
+        self.warning(message + ": " + str(error))
+        self.log.critical(message + ": " + str(error))
