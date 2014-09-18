519be84e46f30ada6db5cbb65dee1f8eb820728c
Remi Hakim <remi@datadoghq.com>
2014-09-18 17:29:54 -0400

    Rewrite the check

--- checks.d/btrfs.py
+++ checks.d/btrfs.py
@@ -1,8 +1,3 @@
-#####
-# This is adapted from btrfs-gui: http://carfax.org.uk/btrfs-gui (License included)
-# It mimics the behavior of the btrfs fi df command
-#
-
 # stdlib
 import os
 import struct
@@ -16,94 +11,64 @@ from checks import AgentCheck
 # 3rd party
 import psutil
 
-BLOCK_GROUP_DATA = 1 << 0
-BLOCK_GROUP_SYSTEM = 1 << 1
-BLOCK_GROUP_METADATA = 1 << 2
-BLOCK_GROUP_RAID0 = 1 << 3
-BLOCK_GROUP_RAID1 = 1 << 4
-BLOCK_GROUP_DUP = 1 << 5
-BLOCK_GROUP_RAID10 = 1 << 6 
+MIXED = "mixed"
+DATA = "data"
+METADATA = "metadata"
+SYSTEM = "system"
+SINGLE = "single"
+RAID0 = "raid0"
+RAID1 = "raid1"
+RAID10 = "raid10"
+DUP = "dup"
 
-IOCTL_SPACE_ARGS = struct.Struct("=2Q")
-IOC_SPACE_INFO = 0xc0109414
-IOCTL_SPACE_INFO = struct.Struct("=3Q")
+FLAGS_MAPPER = {
+    1: (SINGLE, DATA),
+    2: (SINGLE, SYSTEM),
+    4: (SINGLE, METADATA),
+    5: (SINGLE, MIXED),
+    9: (RAID0, DATA),
+    10: (RAID0, SYSTEM),
+    12: (RAID0, METADATA),
+    13: (RAID0, MIXED),
+    17: (RAID1, DATA),
+    18: (RAID1, SYSTEM),
+    20: (RAID1, METADATA),
+    21: (RAID1, MIXED),
+    33: (DUP, DATA),
+    34: (DUP, SYSTEM),
+    36: (DUP, METADATA),
+    37: (DUP, MIXED),
+    65: (RAID10, DATA),
+    66: (RAID10, SYSTEM),
+    68: (RAID10, METADATA),
+    69: (RAID10, MIXED),
 
-def sized_array(count=4096):
+}
+
+BTRFS_IOC_SPACE_INFO = 0xc0109414
+
+TWO_LONGS_STRUCT = struct.Struct("=2Q") # 2 Longs
+THREE_LONGS_STRUCT= struct.Struct("=3Q") # 3 Longs
+
+def sized_array(count):
     return array.array("B", itertools.repeat(0, count))
 
-def get_replication_type(bgid):
-    if bgid & BLOCK_GROUP_RAID0:
-        return "RAID0"
-    elif bgid & BLOCK_GROUP_RAID1:
-        return "RAID1"
-    elif bgid & BLOCK_GROUP_RAID10:
-        return "RAID10"
-    elif bgid & BLOCK_GROUP_DUP:
-        return "DUP"
-    return "single"
-
-def get_usage_type(bgid):
-    if (bgid & BLOCK_GROUP_DATA) and (bgid & BLOCK_GROUP_METADATA):
-        return "mixed"
-    elif bgid & BLOCK_GROUP_DATA:
-        return "data"
-    elif bgid & BLOCK_GROUP_METADATA:
-        return "metadata"
-    elif bgid & BLOCK_GROUP_SYSTEM:
-        return "system"
-    return ""
-
-class Filesystem(object):
-
-    class _DirFD(object):
-        def __init__(self, fd):
-            self.fd = fd
-
-        def fileno(self):
-            return self.fd
-
-        def open(self, dir):
-            return self.fd
+class FileDescriptor(object):
 
     def __init__(self, mountpoint):
-        self.mountpoint = mountpoint
+        self.fd = os.open(mountpoint, os.O_DIRECTORY)
 
     def __enter__(self):
-        self.fd = os.open(self.mountpoint, os.O_DIRECTORY)
-        return self._DirFD(self.fd)
+        return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         os.close(self.fd)
 
-def df(mountpoint):
-    """Collect information on the usage of the filesystem. Replicate
-    the operation of btrfs fi df.
-    """
+    def fileno(self):
+        return self.fd
 
-    with Filesystem(mountpoint) as fd:
-        # Get the number of spaces we need to allocate for the result
-        ret = sized_array(IOCTL_SPACE_ARGS.size)
-        rv = fcntl.ioctl(fd, IOC_SPACE_INFO, ret)
-        space_slots, total_spaces = IOCTL_SPACE_ARGS.unpack(ret)
-
-        # Now allocate it, and get the data
-        buf_size = (IOCTL_SPACE_ARGS.size
-                    + total_spaces * IOCTL_SPACE_INFO.size)
-        ret = sized_array(buf_size)
-        IOCTL_SPACE_ARGS.pack_into(ret, 0, total_spaces, 0)
-        rv = fcntl.ioctl(fd, IOC_SPACE_INFO, ret)
-
-    # Parse the result
-    space_slots, total_spaces = IOCTL_SPACE_ARGS.unpack_from(ret, 0)
-
-    res = []
-    for offset in xrange(IOCTL_SPACE_ARGS.size,
-                         buf_size,
-                         IOCTL_SPACE_INFO.size):
-        flags, total, used = IOCTL_SPACE_INFO.unpack_from(ret, offset)
-        res.append({"flags": flags, "size": total, "used": used})
-
-    return res
+    def open(self, dir):
+        return self.fd
 
 class BTRFS(AgentCheck):
 
@@ -111,6 +76,37 @@ class BTRFS(AgentCheck):
         AgentCheck.__init__(self, name, init_config, agentConfig, instances=instances)
         if instances is not None and len(instances) > 1:
             raise Exception("BTRFS check only supports one configured instance.")
+
+    def get_usage(self, mountpoint):
+        results = []
+
+        with FileDescriptor(mountpoint) as fd:
+
+            # Get the struct size needed
+            # https://github.com/spotify/linux/blob/master/fs/btrfs/ioctl.h#L46-L50
+            ret = sized_array(TWO_LONGS_STRUCT.size)
+            fcntl.ioctl(fd, BTRFS_IOC_SPACE_INFO, ret)
+            _, total_spaces = TWO_LONGS_STRUCT.unpack(ret)
+
+            # Allocate it
+            buffer_size = (TWO_LONGS_STRUCT.size
+                        + total_spaces * THREE_LONGS_STRUCT.size)
+            
+            data = sized_array(buffer_size)
+            TWO_LONGS_STRUCT.pack_into(data, 0, total_spaces, 0)
+            fcntl.ioctl(fd, BTRFS_IOC_SPACE_INFO, data)
+
+
+        _, total_spaces = TWO_LONGS_STRUCT.unpack_from(ret, 0)
+        for offset in xrange(TWO_LONGS_STRUCT.size,
+                             buffer_size,
+                             THREE_LONGS_STRUCT.size):
+
+            # https://github.com/spotify/linux/blob/master/fs/btrfs/ioctl.h#L40-L44
+            flags, total_bytes, used_bytes = THREE_LONGS_STRUCT.unpack_from(data, offset)
+            results.append((flags, total_bytes, used_bytes))
+
+        return results
 
     def check(self, instance):
         btrfs_devices = {}
@@ -124,18 +120,17 @@ class BTRFS(AgentCheck):
             raise Exception("No btrfs device found")
 
         for device, mountpoint in btrfs_devices.iteritems():
-            v = df(mountpoint)
-            for data in v:
+            for flags, total_bytes, used_bytes in self.get_usage(mountpoint):
+                replication_type, usage_type  = FLAGS_MAPPER[flags]
                 tags = [
-                    'usage_type:' + get_usage_type(data['flags']),
-                    'replication_type:' + get_replication_type(data['flags']),
+                    'usage_type:{0}'.format(usage_type),
+                    'replication_type:{0}'.format(replication_type),
                 ]
-                total = data['size']
-                used = data['used']
-                free = total - used
-                usage = free / total
 
-                self.gauge('system.disk.btrfs.total', total, tags=tags, device_name=device)
-                self.gauge('system.disk.btrfs.used', used, tags=tags, device_name=device)
+                free = total_bytes - used_bytes
+                usage = float(free) / float(total_bytes)
+
+                self.gauge('system.disk.btrfs.total', total_bytes, tags=tags, device_name=device)
+                self.gauge('system.disk.btrfs.used', used_bytes, tags=tags, device_name=device)
                 self.gauge('system.disk.btrfs.free', free, tags=tags, device_name=device)
                 self.gauge('system.disk.btrfs.usage', usage, tags=tags, device_name=device)
