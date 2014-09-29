fce5152e8d902821c9b521402667ac87f9e9a17b
Remi Hakim <remi@datadoghq.com>
2014-09-29 18:56:05 -0400

    Send the core count as a metric

--- checks.d/system_core.py
+++ checks.d/system_core.py
@@ -5,6 +5,7 @@ class SystemCore(AgentCheck):
 
     def check(self, instance):
         cpu_times = psutil.cpu_times(percpu=True)
+        self.gauge("system.core.count", len(cpu_times))
 
         for i, cpu in enumerate(cpu_times):
             for key, value in cpu._asdict().iteritems():
