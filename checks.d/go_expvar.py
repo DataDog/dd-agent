5e85bfd255e658d80d55102ae769e8fa3a2e8849
Remi Hakim <remi@datadoghq.com>
2014-10-07 17:31:04 -0400

    Improvements to the go expvar check
    * Use requests instead of urllib2
    * Add memstats metrics by default
    * Add tags at the metric_level
    * Add path as a tag when an alias is set

--- checks.d/go_expvar.py
+++ checks.d/go_expvar.py
@@ -1,85 +1,153 @@
 # stdlib
-import urllib2
 import re
+from collections import defaultdict
 
 # project
 from checks import AgentCheck
 
 # 3rd party
-import simplejson as json
+import requests
 
-DEFAULT_MAX_METRICS = 200
+DEFAULT_MAX_METRICS = 350
+PATH = "path"
+ALIAS = "alias"
+TYPE = "type"
+TAGS = "tags"
+
+DEFAULT_TYPE = 'gauge'
+GAUGE = "gauge"
+RATE = "rate"
+
+SUPPORTED_TYPES = {
+    GAUGE: AgentCheck.gauge,
+    RATE: AgentCheck.rate,
+}
+
+METRIC_NAMESPACE = "go_expvar"
+
+
+# See http://golang.org/pkg/runtime/#MemStats
+DEFAULT_GAUGE_MEMSTAT_METRICS = [
+    # General statistics
+    "Alloc", "TotalAlloc", 
+
+    # Main allocation heap statistics
+    "HeapAlloc", "HeapSys", "HeapIdle", "HeapInuse",
+    "HeapReleased", "HeapObjects", 
+
+]
+
+DEFAULT_RATE_MEMSTAT_METRICS = [
+    # General statistics
+    "Lookups", "Mallocs", "Frees", 
+
+    # Garbage collector statistics
+    "PauseTotalNs", "NumGC",
+]
+
+DEFAULT_METRICS = [{PATH: "memstats/%s" % path, TYPE: GAUGE} for path in DEFAULT_GAUGE_MEMSTAT_METRICS] +\
+    [{PATH: "memstats/%s" % path, TYPE: RATE} for path in DEFAULT_RATE_MEMSTAT_METRICS]
+
 
 class GoExpvar(AgentCheck):
 
-    def __init__(self, name, init_config, agentConfig, instances=None):
-        AgentCheck.__init__(self, name, init_config, agentConfig, instances)
-        self.func= {
-                "gauge" : self.gauge,
-                "rate"  : self.rate
-                }
+    def __init__(self, name, init_config, agentConfig):
+        AgentCheck.__init__(self, name, init_config, agentConfig)
+        self._last_gc_count = defaultdict(int)
+
+    def _get_data(self, url):
+        r = requests.get(url)
+        r.raise_for_status()
+        return r.json()
+
+    def _load(self, instance):
+        url = instance.get('expvar_url')
+        if not url:
+            raise Exception('GoExpvar instance missing "expvar_url" value.')
+        
+        tags = instance.get('tags', [])
+        tags.append("expvar_url:%s" % url)
+        data = self._get_data(url)
+        metrics = DEFAULT_METRICS + instance.get("metrics", [])
+        max_metrics = instance.get("max_returned_metrics", DEFAULT_MAX_METRICS)
+        return data, tags, metrics, max_metrics, url
+
+    def get_gc_collection_histogram(self, data, tags, url):
+        num_gc = data.get("memstats", {}).get("NumGC")
+        pause_hist = data.get("memstats", {}).get("PauseNs")
+        last_gc_count = self._last_gc_count[url]
+        start = (last_gc_count + 256) % 255 -1
+        end = (num_gc + 255) % 255
+
+        self._last_gc_count[url] = num_gc
+
+        for value in pause_hist[start:end]:
+            self.histogram(
+                self.normalize("memstats.PauseNs", METRIC_NAMESPACE, fix_case=True),
+                value, tags=tags)
 
 
     def check(self, instance):
-        if 'expvar_url' not in instance:
-            raise Exception('GoExpvar instance missing "expvar_url" value.')
-        tags = instance.get('tags', [])
-        content = self._get_data(instance)
-        data = json.loads(content)
-        self.parse_expvar_data(instance, data)
+        data, tags, metrics, max_metrics, url = self._load(instance)
+        self.get_gc_collection_histogram(data, tags, url)
+        self.parse_expvar_data(data, tags, metrics, max_metrics)
 
-    def _get_data(self, instance):
-        '''
-        Query the expvar http server for the json
-        document containing all metrics
-        '''
-        url = instance.get('expvar_url')
-        req = urllib2.Request(url)
-        response = urllib2.urlopen(req)
-        body = response.read()
-        return body
-
-    def parse_expvar_data(self, instance, content):
+    def parse_expvar_data(self, data, tags, metrics, max_metrics):
         '''
         Report all the metrics based on the configuration in instance
         If a metric is not well configured or is not present in the payload,
         continue processing metrics but log the information to the info page
         '''
-        tags = instance.get("tags", [])
         count = 0
-        for metric in instance.get("metrics", []):
-            if "path" not in metric:
+        for metric in metrics:
+            path = metric.get(PATH)
+            metric_type = metric.get(TYPE, DEFAULT_TYPE)
+            metric_tags = list(metric.get(TAGS, []))
+            metric_tags += tags
+            alias = metric.get(ALIAS)
+            metric_name = None
+            tag_by_path = False
+
+            if not path:
                 self.warning("Metric %s has no path" % metric)
                 continue
-            metric_type = metric.get("type", "gauge")
-            if metric_type not in self.func:
+
+            if metric_type not in SUPPORTED_TYPES:
                 self.warning("Metric type %s not supported for this check" % metric_type)
                 continue
 
-            path = metric.get("path")
             keys = path.split("/")
-            values = self.deep_get(content, keys, [])
+            values = self.deep_get(data, keys)
 
-            if len(values)==0:
-                self.log.warning("No results matching path %s" % path)
-            elif len(values)==1:
-                # Defining an alias doesn't make sense if there is several metrics
-                metric_name = metric.get("alias")
+            if len(values) == 0:
+                self.warning("No results matching path %s" % path)
+                continue
+
+            if alias is not None:
+                metric_name = alias
+                tag_by_path = True
 
             for traversed_path, value in values:
-                metric_name = metric_name or self.normalize(".".join(traversed_path), "go_expvar", fix_case=True)
+                actual_path = ".".join(traversed_path)
+                if tag_by_path:
+                    metric_tags.append("path:%s" % actual_path)
+
+                metric_name = metric_name or self.normalize(actual_path, METRIC_NAMESPACE, fix_case=True)
+
                 try:
                     float(value)
                 except ValueError:
                     self.log.warning("Unreportable value for path %s: %s" % (path,value))
                     continue
-                if count >= instance.get("max_returned_metrics", DEFAULT_MAX_METRICS):
-                    self.warning("Reporting more metrics than the allowed maximum")
+
+                if count >= max_metrics:
+                    self.warning("Reporting more metrics than the allowed maximum. Please contact support@datadoghq.com for more information.")
                     return
-                self.func[metric_type](metric_name, value, tags)
+
+                SUPPORTED_TYPES[metric_type](self, metric_name, value, metric_tags)
                 count += 1
 
-    def deep_get(self, content, keys, traversed_path):
+    def deep_get(self, content, keys, traversed_path=None):
         '''
         Allow to retrieve content nested inside a several layers deep dict/list
 
@@ -101,6 +169,10 @@ class GoExpvar(AgentCheck):
                   -keys: ["key1", "key2", "1", "*"] would return [(["key1", "key2", "1", "value"], 72), (["key1", "key2", "1", "name"], "object2")]
                   -keys: ["key1", "key2", "*", "value"] would return [(["key1", "key2", "1", "value"], 72), (["key1", "key2", "0", "value"], 42)]
         '''
+
+        if traversed_path is None:
+            traversed_path = []
+
         if keys == []:
             return [(traversed_path, content)]
 
@@ -117,7 +189,7 @@ class GoExpvar(AgentCheck):
             for new_key, new_content in enumerate(object):
                 yield str(new_key), new_content
         elif isinstance(object, dict):
-            for new_key, new_content in object.items():
+            for new_key, new_content in object.iteritems():
                 yield str(new_key), new_content
         else:
             self.log.warning("Could not parse this object, check the json"
--- tests/test_go_expvar.py
+++ tests/test_go_expvar.py
@@ -2,6 +2,7 @@ import unittest
 import time
 import os
 from tests.common import load_check
+import simplejson as json
 
 
 class TestGoExpVar(unittest.TestCase):
@@ -30,7 +31,8 @@ class TestGoExpVar(unittest.TestCase):
                         {
                             "path":"random_walk",
                             "alias":"go_expvar.gauge1",
-                            "type":"gauge"
+                            "type":"gauge",
+                            "tags": ["metric_tag1:metric_value1", "metric_tag2:metric_value2"]
                         }
                         ]
                     }]
@@ -38,11 +40,14 @@ class TestGoExpVar(unittest.TestCase):
 
         self.check = load_check('go_expvar', self.config, self.agentConfig)
 
-        def _get_data_mock(instance):
-            with open(instance.get('expvar_url'), 'r') as go_output:
-                return go_output.read()
+        def _get_data_mock(url):
+            with open(url, 'r') as go_output:
+                return json.loads(go_output.read())
 
         self.check._get_data = _get_data_mock
+
+    def _assert_metric_number(self, metrics, metric_name, count):
+        self.assertEqual(len([m for m in metrics if m[0] == metric_name]), count, metrics)
 
     def testGoExpVar(self):
 
@@ -50,10 +55,9 @@ class TestGoExpVar(unittest.TestCase):
         metrics = self.check.get_metrics()
 
         # The rate is not here so only 2
-        self.assertEqual(len(metrics), 2)
-        metrics.sort(key=lambda x:x[0])
-        self.assertEqual(metrics[0][0], 'go_expvar.gauge1')
-        self.assertEqual(metrics[1][0], 'go_expvar.memstats.by_size.1.mallocs') # Verify the correct default value for metric name
+        self.assertEqual(len(metrics), 15, metrics)
+        self._assert_metric_number(metrics, 'go_expvar.gauge1', 1)
+        self._assert_metric_number(metrics, 'go_expvar.memstats.by_size.1.mallocs', 1)
 
         # Sleep for 1 second so the rate interval >=1
         time.sleep(1)
@@ -61,17 +65,42 @@ class TestGoExpVar(unittest.TestCase):
         self.check.check(self.config['instances'][0])
         metrics = self.check.get_metrics()
 
-        self.assertEqual(len(metrics) ,3)
-        metrics.sort(key=lambda x:x[0])
-        self.check.log.info(metrics)
-        self.assertEqual(metrics[0][0], 'go_expvar.gauge1')
-        self.assertEqual(metrics[1][0], 'go_expvar.gc.pause')
-        self.assertEqual(metrics[2][0], 'go_expvar.memstats.by_size.1.mallocs') # Verify the correct default value for metric name
+        self.assertEqual(len(metrics) ,16, metrics)
+        self._assert_metric_number(metrics, 'go_expvar.gauge1', 1)
+        self._assert_metric_number(metrics, 'go_expvar.gc.pause', 1)
+        self._assert_metric_number(metrics, 'go_expvar.memstats.by_size.1.mallocs', 1)
+
+        tags_count = {
+            "go_expvar.memstats.total_alloc" : 3,
+            "go_expvar.memstats.heap_inuse" : 3,
+            "go_expvar.memstats.alloc" : 3,
+            "go_expvar.memstats.mallocs" : 3,
+            "go_expvar.memstats.heap_objects" : 3,
+            "go_expvar.memstats.by_size.1.mallocs" : 3,
+            "go_expvar.memstats.heap_released" : 3,
+            "go_expvar.memstats.pause_total_ns" : 3,
+            "go_expvar.memstats.heap_alloc" : 3,
+            "go_expvar.gc.pause" : 4,
+            "go_expvar.memstats.heap_sys" : 3,
+            "go_expvar.memstats.lookups" : 3,
+            "go_expvar.memstats.num_gc" : 3,
+            "go_expvar.memstats.frees" : 3,
+            "go_expvar.memstats.heap_idle" : 3,
+            "go_expvar.gauge1" : 6}
+
         for metric in metrics:
             tags = metric[3]['tags']
-            self.assertEqual(len(tags), 2)
+            self.assertEqual(len(tags), tags_count[metric[0]], metric)
             self.assertTrue("optionaltag1" in tags)
             self.assertTrue("optionaltag2" in tags)
+            self.assertTrue("expvar_url:%s" % self.config['instances'][0]['expvar_url'] in tags)
+
+            if metric[0] == "go_expvar.gauge1":
+                self.assertTrue("metric_tag1:metric_value1" in tags, metric)
+                self.assertTrue("metric_tag2:metric_value2" in tags, metric)
+
+
+
 
         # Verify that the max number of metrics is respected
         self.config['instances'][0]['max_returned_metrics'] = 1
