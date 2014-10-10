54f6d96f20f1041fe02c52f1b1451e298eccb408
nambrosch <nik@habitforming.net>
2014-10-10 02:20:53 -0400

    updated with some of remh's sugesstions

--- checks.d/ping.py
+++ checks.d/ping.py
@@ -5,50 +5,43 @@ import time
 
 class PingCheck(AgentCheck):
     def check(self, instance):
+
+        # let's make sure our yaml is proper
         if 'name' not in instance:
             raise Exception('Skipping instance, name not defined.')
-
         if 'ip' not in instance:
             raise Exception('Skipping instance, ip not defined.')
 
+        # read yaml values
         name = instance['name']
         ip = instance['ip']
-
         tags = instance.get('tags', [])
         tags = tags + ['destination:' + name, 'ip:' + ip]
 
+        # run ping command, remember if it works or not
         try:
             var = subprocess.check_output('ping -q -c 5 -t 20 ' + ip, shell=True).splitlines(True)
-
-            for line in var:
-                if "round-trip" in line or "rtt" in line:
-                    split = line.replace('/',' ').split()
-                    (min, avg, max, jitter) = (split[6], split[7], split[8], split[9])
-
-                elif " packets received, " in line:
-                    split = line.replace('%','').split()
-                    loss = split[6]
-
-                elif " received, " in line:
-                    split = line.replace('%','').split()
-                    loss = split[5]
-
+            pingworks = 1
         except:
-            (min, avg, max, jitter,loss) = ('0', '0', '0', '0', '100')
+            (pingworks, var, min, avg, max, jitter, loss) = (0, '', '', '', '', '', '')
 
+        # parse ping output line-by-line
+        for line in var:
+            if "round-trip" in line or "rtt" in line:
+                split = line.replace('/',' ').split()
+                (min, avg, max, jitter) = (split[6], split[7], split[8], split[9])
+
+            elif " packets received, " in line:
+                split = line.replace('%','').split()
+                loss = split[6]
+
+            elif " received, " in line:
+                split = line.replace('%','').split()
+                loss = split[5]
+
+        # send metrics to home base
         self.gauge('ping.min', min, tags)
         self.gauge('ping.avg', avg, tags)
         self.gauge('ping.max', max, tags)
         self.gauge('ping.jitter', jitter, tags)
         self.gauge('ping.loss', loss, tags)
-
-        if float(loss) > 0:
-            self.event({
-                'timestamp': int(time.time()),
-                'event_type': 'ping_check',
-                'api_key': self.agentConfig.get('api_key', ''),
-                'msg_title': loss + '% packet loss on ' + name,
-                'msg_text': loss + '% packet loss on ' + name,
-                'aggregation_key': ip + name + loss,
-                'alert_type': 'warning'
-            })
