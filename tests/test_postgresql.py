45d1bbf094383d9ba069d2fec4363ba1e9da39bf
Remi Hakim <remi@datadoghq.com>
2014-10-15 16:14:44 -0400

    Fix tests

--- tests/test_postgresql.py
+++ tests/test_postgresql.py
@@ -1,10 +1,7 @@
 import unittest
 from tests.common import load_check
-
 from nose.plugins.attrib import attr
-
 import time
-from pprint import pprint
 
 @attr(requires='postgres')
 class TestPostgres(unittest.TestCase):
@@ -32,26 +29,12 @@ class TestPostgres(unittest.TestCase):
 
         self.check.run()
         metrics = self.check.get_metrics()
-        self.assertTrue(len([m for m in metrics if m[0] == u'postgresql.connections'])               >= 1, pprint(metrics))
-        self.assertTrue(len([m for m in metrics if m[0] == u'postgresql.dead_rows'])                 >= 1, pprint(metrics))
-        self.assertTrue(len([m for m in metrics if m[0] == u'postgresql.live_rows'])                 >= 1, pprint(metrics))
-        self.assertTrue(len([m for m in metrics if m[0] == u'postgresql.table_size'])                >= 1, pprint(metrics))
-        self.assertTrue(len([m for m in metrics if m[0] == u'postgresql.index_size'])                >= 1, pprint(metrics))
-        self.assertTrue(len([m for m in metrics if m[0] == u'postgresql.total_size'])                >= 1, pprint(metrics))
-        self.assertTrue(len([m for m in metrics if m[0] == u'postgresql.max_connections'])           >= 1, pprint(metrics))
-        self.assertTrue(len([m for m in metrics if m[0] == u'postgresql.percent_usage_connections']) >= 1, pprint(metrics))
-        # Don't test for locks
-        # self.assertTrue(len([m for m in metrics if m[0] == u'postgresql.locks'])              >= 1, pprint(metrics))
-        # Brittle tests
-        # self.assertTrue(4 <= len(metrics) <= 6, metrics)
-        # self.assertTrue(4 <= len([m for m in metrics if 'db:datadog_test' in str(m[3]['tags']) ]) <= 5, pprint(metrics))
-        # self.assertTrue(len([m for m in metrics if 'table:persons' in str(m[3]['tags'])]) == 2, pprint(metrics))
-
-        # Rate metrics, need 2 collection rounds
-        time.sleep(1)
-        self.check.run()
-        metrics = self.check.get_metrics()
-        self.assertTrue(len([m for m in metrics if m[0] == u'postgresql.bgwriter.sync_time']) >= 1, pprint(metrics))
+        self.assertTrue(len([m for m in metrics if m[0] == 'postgresql.connections']) == 1, metrics)
+        self.assertTrue(len([m for m in metrics if m[0] == 'postgresql.dead_rows']) == 1, metrics)
+        self.assertTrue(len([m for m in metrics if m[0] == 'postgresql.live_rows']) == 1, metrics)
+        self.assertTrue(4 <= len(metrics) <= 6, metrics)
+        self.assertTrue(4 <= len([m for m in metrics if 'db:datadog_test' in str(m[3]['tags']) ]) <= 5, metrics)
+        self.assertTrue(len([m for m in metrics if 'table:persons' in str(m[3]['tags'])]) == 2, metrics)
 
         # Service checks
         service_checks = self.check.get_service_checks()
@@ -68,9 +51,9 @@ class TestPostgres(unittest.TestCase):
         self.check.run()
         metrics = self.check.get_metrics()
 
-        self.assertTrue(len(metrics) == 36, metrics)
-        self.assertTrue(len([m for m in metrics if 'db:datadog_test' in str(m[3].get('tags', []))]) == 24, metrics)
-        self.assertTrue(len([m for m in metrics if 'table:persons' in str(m[3].get('tags', [])) ]) == 11, metrics)
+        self.assertTrue(len(metrics) == 20, metrics)
+        self.assertTrue(len([m for m in metrics if 'db:datadog_test' in str(m[3]['tags']) ]) == 20, metrics)
+        self.assertTrue(len([m for m in metrics if 'table:persons' in str(m[3]['tags']) ]) == 8, metrics)
 
 if __name__ == '__main__':
     unittest.main()
