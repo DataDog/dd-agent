a2d968004c129e787ce3d368ea88a92c7de28818
Arthur Yan <arthur.yan@datadoghq.com>
2014-10-29 21:49:03 +0000

    Create expired mock case

--- tests/test_http_check_ssl.py
+++ tests/test_http_check_ssl.py
@@ -2,8 +2,12 @@ import unittest
 from tests.common import load_check
 from checks import AgentCheck
 import time
+import mock
 
 class HttpSslTestCase(unittest.TestCase):
+
+    def tearDown(self):
+        self.check.stop()
 
     def test_http_ssl(self):
 
@@ -55,3 +59,30 @@ class HttpSslTestCase(unittest.TestCase):
         self.check._process_results()
         service = self.check.get_service_checks()
         self.assertEqual(service[1].get('status'), AgentCheck.WARNING)
+
+
+
+    fake_cert = {'notAfter': 'Apr 12 12:00:00 2006 GMT'}
+    @mock.patch('ssl.SSLSocket.getpeercert', return_value=fake_cert)
+    def test_mock_case(self, getpeercert_func):
+
+        # assert getpeercert_func.called
+        config = {
+            'instances': [{
+            'name' : 'Expired Cert',
+            'url' : 'https://github.com',
+            'timeout' : 1,
+            'check_certificate_expiration': True,
+            'days_warning': 14
+            },
+            ]
+        }
+        agentConfig = {}
+        self.check = load_check('http_check', config, agentConfig)
+
+        #Failed Status
+        self.check.check(config['instances'][0])
+        time.sleep(1)
+        self.check._process_results()
+        service = self.check.get_service_checks()
+        self.assertEqual(service[1].get('status'), AgentCheck.CRITICAL)
