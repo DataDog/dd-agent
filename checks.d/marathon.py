a69cd62e8047a2b007e41b0ee1081f12a44ae8a4
Aman Kapur <aman@datadoghq.com>
2014-11-17 23:40:14 +0000

    added checks for mesos, and cleanup

--- checks.d/marathon.py
+++ checks.d/marathon.py
@@ -10,8 +10,13 @@ from util import headers
 # 3rd party
 import simplejson as json
 import requests
-import logging
-log = logging.getLogger('collector')
+
+def get_request_json(req):
+    # Condition for request v1.x backward compatibility
+    if hasattr(req.json, '__call__'):
+        return req.json()
+    else:
+        return req.json
 
 class Marathon(AgentCheck):
 
@@ -33,7 +38,8 @@ class Marathon(AgentCheck):
             for app in response['apps']:
                 tags = ['app_id:' + app['id'], 'version:' + app['version']] + instance_tags
                 for attr in ['taskRateLimit','instances','cpus','mem','tasksStaged','tasksRunning']:
-                    self.gauge('marathon.' + attr, app[attr], tags=tags)
+                    if hasattr(app, attr):
+                        self.gauge('marathon.' + attr, app[attr], tags=tags)
                 versions_reply = self.get_v2_app_versions(url, app, timeout)
                 if versions_reply is not None:
                     self.gauge('marathon.versions', len(versions_reply['versions']), tags=tags)
@@ -42,65 +48,55 @@ class Marathon(AgentCheck):
         # Use a hash of the URL as an aggregation key
         aggregation_key = md5(url).hexdigest()
         tags = ['url:%s' % url]
-
+        msg = None
+        status = None
         try:
             r = requests.get(url + "/v2/apps", timeout=timeout)
-            status = AgentCheck.OK
             if r.status_code != 200:
                 self.status_code_event(url, r, aggregation_key)
                 status = AgentCheck.CRITICAL
                 msg = "Got %s when hitting %s" % (r.status_code, url)
-                self.warning(msg)
         except requests.exceptions.Timeout as e:
             # If there's a timeout
             self.timeout_event(url, timeout, aggregation_key)
             msg = "Timeout when hitting %s" % url
             status = AgentCheck.CRITICAL
-            self.warning(msg)
         except Exception as e:
             msg = e.message
             status = AgentCheck.CRITICAL
         finally:
-            self.service_check(self.check_name, status, tags=tags, message=msg)
             if status is AgentCheck.CRITICAL:
+                self.service_check(self.check_name, status, tags=tags, message=msg)
+                self.warning(msg)
                 raise Exception(msg)
 
+        req_json = get_request_json(r)
+        app_count = len(req_json['apps'])
 
-        # Condition for request v1.x backward compatibility
-        if hasattr(r.json, '__call__'):
-            return r.json()
-        else:
-            return r.json
+        status = AgentCheck.CRITICAL if app_count is 0 else AgentCheck.OK
+        for app in req_json['apps']:
+            tags.append(['app_id:%s' % app['id'], 'version:%s' % app['version']])
+
+        msg = "%s marathon apps detected at %s" % (app_count, url)
+        self.service_check(self.check_name, status, tags=tags, message=msg)
+
+        return req_json
 
     def get_v2_app_versions(self, url, app, timeout):
         # Use a hash of the URL as an aggregation key
         aggregation_key = md5(url).hexdigest()
 
-        tags = ['url:%s' % url, 'app_id:%s' % app['id'], 'version:%s' % app['version']]
-
         try:
-            r = requests.get(url + "/v2/apps/" + app_id + "/versions", timeout=timeout)
-            status = AgentCheck.OK
-            if r.status_code != 200:
-                self.status_code_event(url, r, aggregation_key)
-                status = AgentCheck.CRITICAL
-                msg = "Got %s when hitting %s" % (r.status_code, url)
-                self.warning(msg)
+            r = requests.get(url + "/v2/apps/" + app['id'] + "/versions", timeout=timeout)
         except requests.exceptions.Timeout as e:
             # If there's a timeout
-            msg = "Timeout when hitting %s" % url
             self.timeout_event(url, timeout, aggregation_key)
-            status = AgentCheck.CRITICAL
-            self.warning(msg)
-        except Exception as e:
-            msg = e.message
-            status = AgentCheck.CRITICAL
-        finally:
-            self.service_check(self.check_name, status, tags=tags, message=msg)
-            if status is AgentCheck.CRITICAL:
-                return None
+            self.warning("Timeout when hitting %s" % url)
+            return None
 
-
+        if r.status_code != 200:
+            self.status_code_event(url, r, aggregation_key)
+            self.warning("Got %s when hitting %s" % (r.status_code, url))
 
         return r.json()
 
