5d7e0e34bbc378b6e5dbb2a3f190b3491014654d
Remi Hakim <remi@datadoghq.com>
2014-11-24 10:20:14 -0500

    Do not exit 1 when parameters are missing
    It can be expected when installing the MSI

--- packaging/datadog-agent/win32/wix/FindReplace/FindReplace.go
+++ packaging/datadog-agent/win32/wix/FindReplace/FindReplace.go
@@ -21,7 +21,7 @@ func main() {
 
 	if len(args) != 4 {
 		fmt.Println("Usage: FindReplace.exe path_to_file search_text replace_text\n")
-		os.Exit(1)
+		os.Exit(0)
 	}
 
 	filePath := args[1]
@@ -30,13 +30,13 @@ func main() {
 
 	if strings.Trim(replaceText, " ") == "" {
 		fmt.Println("Replace text can't be empty")
-		os.Exit(1)
+		os.Exit(0)
 	}
 
 	parts := strings.Split(replaceText, ":")
 	if len(parts) == 2 && strings.Trim(parts[1], " ") == "" {
 		fmt.Println("You can't specify an empty key.")
-		os.Exit(1)
+		os.Exit(0)
 	}
 
 	contents, err := ioutil.ReadFile(filePath)
@@ -46,5 +46,6 @@ func main() {
 	newContentsByte := []byte(newContents)
 	err = ioutil.WriteFile(filePath, newContentsByte, 0644)
 	check(err)
+	os.Exit(0)
 
 }
