1ecb73145c915c28f8d2a434a16d8635f8df1e2e
Cory G Watson <gphat@keen.io>
2014-12-26 16:26:33 -0600

    Clean up and modernize marathon plugin

--- checks.d/marathon.py
+++ checks.d/marathon.py
@@ -1,17 +1,25 @@
-# stdlib
-import time
-from hashlib import md5
-
 # project
 from checks import AgentCheck
 
 # 3rd party
-import simplejson as json
 import requests
 
 class Marathon(AgentCheck):
 
-    SERVICE_CHECK_NAME = "marathon.can_connect"
+    DEFAULT_TIMEOUT = 5
+    SERVICE_CHECK_NAME = 'marathon.can_connect'
+
+    APP_METRICS = [
+        'backoffFactor',
+        'backoffSeconds',
+        'cpus',
+        'dist',
+        'instances',
+        'mem',
+        'taskRateLimit',
+        'tasksRunning',
+        'tasksStaged'
+    ]
 
     def check(self, instance):
         if 'url' not in instance:
@@ -20,98 +28,37 @@ class Marathon(AgentCheck):
         # Load values from the instance config
         url = instance['url']
         instance_tags = instance.get('tags', [])
-        default_timeout = self.init_config.get('default_timeout', 5)
+        default_timeout = self.init_config.get('default_timeout', self.DEFAULT_TIMEOUT)
         timeout = float(instance.get('timeout', default_timeout))
 
-        response = self.get_v2_apps(url, timeout)
+        response = self.get_json(url + "/v2/apps", timeout)
         if response is not None:
             self.gauge('marathon.apps', len(response['apps']), tags=instance_tags)
             for app in response['apps']:
                 tags = ['app_id:' + app['id'], 'version:' + app['version']] + instance_tags
-                for attr in ['taskRateLimit', 'instances', 'cpus', 'mem', 'tasksStaged', 'tasksRunning', 'backoffSeconds', 'backoffFactor']:
-                    if hasattr(app, attr):
+                for attr in self.APP_METRICS:
+                    if attr in app:
                         self.gauge('marathon.' + attr, app[attr], tags=tags)
-                    else:
-                        self.log.debug('Marathon application (id: %s) has no attribute %s' % (app['id'], attr))
-                versions_reply = self.get_v2_app_versions(url, app['id'], timeout)
+                versions_reply = self.get_json(url + "/v2/apps/" + app['id'] + "/versions", timeout)
                 if versions_reply is not None:
                     self.gauge('marathon.versions', len(versions_reply['versions']), tags=tags)
 
-    def get_v2_apps(self, url, timeout):
-        # Use a hash of the URL as an aggregation key
-        aggregation_key = md5(url).hexdigest()
-        tags = ['url:%s' % url]
-        msg = None
-        status = None
+    def get_json(self, url, timeout):
         try:
-            r = requests.get(url + "/v2/apps", timeout=timeout)
-            if r.status_code != 200:
-                self.status_code_event(url, r, aggregation_key)
-                status = AgentCheck.CRITICAL
-                msg = "Got %s when hitting %s" % (r.status_code, url)
+            r = requests.get(url, timeout=timeout)
         except requests.exceptions.Timeout:
             # If there's a timeout
-            self.timeout_event(url, timeout, aggregation_key)
-            msg = "%s seconds timeout when hitting %s" % (timeout, url)
-            status = AgentCheck.CRITICAL
-        except Exception as e:
-            msg = e.message
-            status = AgentCheck.CRITICAL
-        finally:
-            if status is AgentCheck.CRITICAL:
-                self.service_check(self.SERVICE_CHECK_NAME, status, tags=tags, message=msg)
-                raise Exception(msg)
-
-        req_json = r.json()
-        app_count = len(req_json['apps'])
-
-        if app_count is 0:
-            status = AgentCheck.WARN
-            msg = "No marathon applications detected at %s" % url
-        else:
-            instance_count = 0
-            for app in req_json['apps']:
-                instance_count += app['instances']
-            status = AgentCheck.CRITICAL if instance_count is 0 else AgentCheck.OK
-            msg = "%s Marathon app(s) detected with %s instances running at %s" % (app_count, instance_count, url)
-
-
-        self.service_check(self.SERVICE_CHECK_NAME, status, tags=tags, message=msg)
-
-        return req_json
-
-    def get_v2_app_versions(self, url, app_id, timeout):
-        # Use a hash of the URL as an aggregation key
-        aggregation_key = md5(url).hexdigest()
-
-        try:
-            r = requests.get(url + "/v2/apps/" + app_id + "/versions", timeout=timeout)
-        except requests.exceptions.Timeout:
-            # If there's a timeout
-            self.timeout_event(url, timeout, aggregation_key)
-            self.warning("Timeout when hitting %s" % url)
-            return None
+            self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.CRITICAL,
+                message='%s timed out after %s seconds.' % (url, timeout),
+                tags = ["url:{}".format(url)])
+            raise Exception("Timeout when hitting %s" % url)
 
         if r.status_code != 200:
-            self.status_code_event(url, r, aggregation_key)
-            self.warning("Got %s when hitting %s" % (r.status_code, url))
+            self.status_code_event(url, r)
+            raise Exception("Got %s when hitting %s" % (r.status_code, url))
+        else:
+            self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.OK,
+                tags = ["url:{}".format(url)]
+            )
 
         return r.json()
-
-    def timeout_event(self, url, timeout, aggregation_key):
-        self.event({
-            'timestamp': int(time.time()),
-            'event_type': 'http_check',
-            'msg_title': 'URL timeout',
-            'msg_text': '%s timed out after %s seconds.' % (url, timeout),
-            'aggregation_key': aggregation_key
-        })
-
-    def status_code_event(self, url, r, aggregation_key):
-        self.event({
-            'timestamp': int(time.time()),
-            'event_type': 'http_check',
-            'msg_title': 'Invalid reponse code for %s' % url,
-            'msg_text': '%s returned a status of %s' % (url, r.status_code),
-            'aggregation_key': aggregation_key
-        })
