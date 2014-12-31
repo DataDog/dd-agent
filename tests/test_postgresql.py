57fa1716a2266a9f0e9db41d79e6a0c70bb305a9
Conor Branagan <conor.branagan@gmail.com>
2014-12-31 14:23:28 +0000

    There should be 2 service checks from PG now, since we run twice.

--- tests/test_postgresql.py
+++ tests/test_postgresql.py
@@ -59,7 +59,7 @@ class TestPostgres(unittest.TestCase):
         service_checks_count = len(service_checks)
         self.assertTrue(type(service_checks) == type([]))
         self.assertTrue(service_checks_count > 0)
-        self.assertEquals(len([sc for sc in service_checks if sc['check'] == "postgres.can_connect"]), 1, service_checks)
+        self.assertEquals(len([sc for sc in service_checks if sc['check'] == "postgres.can_connect"]), 2, service_checks)
         # Assert that all service checks have the proper tags: host, port and db
         self.assertEquals(len([sc for sc in service_checks if "host:localhost" in sc['tags']]), service_checks_count, service_checks)
         self.assertEquals(len([sc for sc in service_checks if "port:%s" % config['instances'][0]['port'] in sc['tags']]), service_checks_count, service_checks)
