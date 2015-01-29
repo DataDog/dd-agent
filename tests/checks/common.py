8e36e58ed13bdb735a6c75e2c7b21f8998457c6a
Christopher Snell <chris.snell@gmail.com>
2015-01-29 15:31:34 -0800

    [snmp] Add support for forced SNMP data types to help w/ buggy devices
    [snmp] adding tests, minor fix to PR.
    
    [snmp] with new SNMP network check, we run test twice to collect results.
    
    [snmp] log network check exceptions, fix test.
    
    [snmpd-test] add a sleep parameter to ensure interval >= 1

--- tests/checks/common.py
+++ tests/checks/common.py
@@ -168,13 +168,13 @@ class AgentCheckTest(unittest.TestCase):
         self.run_check(config, agent_config, mocks)
 
     def run_check_n(self, config, agent_config=None, mocks=None,
-                    force_reload=False, repeat=1):
+                    force_reload=False, repeat=1, sleep=1):
         for i in xrange(repeat):
             if not i:
                 self.run_check(config, agent_config, mocks, force_reload)
             else:
                 self.run_check(config, agent_config, mocks)
-            time.sleep(1)
+            time.sleep(sleep)
 
     def run_check(self, config, agent_config=None, mocks=None, force_reload=False):
         # If not loaded already, do it!
@@ -325,7 +325,7 @@ WARNINGS
             raise
 
     def assertMetric(self, metric_name, value=None, tags=None, count=None,
-                     at_least=1, hostname=None, device_name=None):
+                     at_least=1, hostname=None, device_name=None, metric_type=None):
         candidates = []
         for m_name, ts, val, mdata in self.metrics:
             if m_name == metric_name:
@@ -336,6 +336,8 @@ WARNINGS
                 if hostname is not None and mdata['hostname'] != hostname:
                     continue
                 if device_name is not None and mdata['device_name'] != device_name:
+                    continue
+                if metric_type is not None and mdata['type'] != metric_type:
                     continue
 
                 candidates.append((m_name, ts, val, mdata))
