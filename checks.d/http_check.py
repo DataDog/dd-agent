a38fb039f5c26e5e88c62751eef2de6633a883f2
Christopher Snell <chris.snell@gmail.com>
2015-01-28 23:29:35 -0800

    [http_check] enable regex testing for content_match
    [yann@datadoghq.com] Rebased on 5.2.1 agent release

--- checks.d/http_check.py
+++ checks.d/http_check.py
@@ -4,6 +4,7 @@ import os.path
 import socket
 import ssl
 import time
+import re
 from urlparse import urlparse
 
 # 3rd party
@@ -152,7 +153,7 @@ class HTTPCheck(NetworkCheck):
                 # Host is UP
                 # Check content matching is set
                 if content_match:
-                    if content_match in content:
+                    if re.search(content_match, content):
                         self.log.debug("%s is found in return content" % content_match)
                         service_checks.append((
                             self.SC_STATUS, Status.UP, "UP"
