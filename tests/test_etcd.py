ad405a246d2a42e309faab4d86aebd71529571c2
Quentin Madec <quentin.madec@datadoghq.com>
2015-02-19 12:31:35 -0500

    [tests] add assertServiceCheck in common check
    Add assertServiceCheck (same principle than assertMetrics) in
    AgentCheckTest. If service_checks does not exist, retrieve latest data
    from AgentCheckTest.check.

--- tests/test_etcd.py
+++ tests/test_etcd.py
@@ -27,17 +27,15 @@ class EtcdTest(AgentCheckTest):
             self.run_check(self.config)
 
             self.assertEqual(len(self.service_checks), 1, self.service_checks)
-            sc = self.service_checks[0]
-            self.assertEquals(sc["check"], self.check.SERVICE_CHECK_NAME, sc["check"])
-            self.assertEquals(sc["status"], AgentCheck.OK, sc["status"])
-            self.assertEquals(sc["tags"], ['url:http://localhost:4001', 'etcd_state:leader'], sc["tags"])
+            self.assertServiceCheck(self.check.SERVICE_CHECK_NAME,
+                                    status=AgentCheck.OK,
+                                    tags=['url:http://localhost:4001', 'etcd_state:leader'])
 
         def test_bad_config(self):
             self.assertRaises(Exception,
                 lambda: self.run_check({"instances": [{"url": "http://localhost:4001/test"}]}))
-            service_checks = self.check.get_service_checks()
-            self.assertEqual(len(service_checks), 1, service_checks)
-            sc = service_checks[0]
-            self.assertEquals(sc["check"], self.check.SERVICE_CHECK_NAME, sc["check"])
-            self.assertEquals(sc["status"], AgentCheck.CRITICAL, sc["status"])
-            self.assertEquals(sc["tags"], ['url:http://localhost:4001/test/v2/stats/self'], sc["tags"])
+
+            self.assertEqual(len(self.service_checks), 1, self.service_checks)
+            self.assertServiceCheck(self.check.SERVICE_CHECK_NAME,
+                                    status=AgentCheck.CRITICAL,
+                                    tags=['url:http://localhost:4001/test/v2/stats/self'])
