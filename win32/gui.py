ca65086504e8ba592b3a65e7ff400f0dd7178e4f
Quentin Madec <quentin.madec@datadoghq.com>
2015-03-05 14:19:00 -0500

    [osx] Exclude all windows checks and cacti
    Because cacti needs `rrdtool`, which is a real pain to install

--- win32/gui.py
+++ win32/gui.py
@@ -121,8 +121,10 @@ EXCLUDED_WINDOWS_CHECKS = [
     'zk',
 ]
 
-WINDOWS_ONLY_CHECKS = [
+EXCLUDED_MAC_CHECKS = [
+    'cacti',
     'iis',
+    'sqlserver',
     'win32_event_log',
     'windows_service',
     'wmi_check',
@@ -165,7 +167,7 @@ def get_checks():
         if Platform.is_windows():
             excluded_checks = EXCLUDED_WINDOWS_CHECKS
         else:
-            excluded_checks = WINDOWS_ONLY_CHECKS
+            excluded_checks = EXCLUDED_MAC_CHECKS
         if filename.split('.')[0] in excluded_checks:
             continue
         if ext not in ('.yaml', '.example', '.disabled'):
