2915f6d21c85ac8a4f61be033935e37456fcc461
matt <matthew.p.boyle@gmail.com>
2015-04-15 17:30:10 -0400

    Add percent_used

--- checks.d/zfs.py
+++ checks.d/zfs.py
@@ -105,6 +105,7 @@ class Zfs(AgentCheck):
             ]
             self.gauge(Zfs.ZPOOL_VDEV_NAMESPACE + 'total', zpool_vdev_stats[vdev]['total'], tags=tags)
             self.gauge(Zfs.ZPOOL_VDEV_NAMESPACE + 'free', zpool_vdev_stats[vdev]['free'], tags=tags)
+            self.gauge(Zfs.ZPOOL_VDEV_NAMESPACE + 'percent_used', zpool_vdev_stats[vdev]['percent_used'], tags=tags)
 
     def _get_zpools(self):
         """
@@ -177,12 +178,18 @@ class Zfs(AgentCheck):
             stats[current_vdev] = {}
             total = properties[1]
             free = properties[2]
+            used = int(total) - int(free)
+            percent_used = int((used / int(total)) * 100)
+            if percent_used < 1:
+                percent_used = 1
+
             if re.match('^\d+[K,M,G,T]', free) or re.match('^\d+\.\d+[K,M,G,T]', free):
                 free = self._convert_human_to_bytes(free)
             if re.match('^\d+[K,M,G,T]', total) or re.match('^\d+\.\d+[K,M,G,T]', total):
                 total = self._convert_human_to_bytes(total)
             stats[current_vdev]['total'] = total
             stats[current_vdev]['free'] = free
+            stats[current_vdev]['percent_used'] = percent_used
             vdev_count += 1
         return stats
 
