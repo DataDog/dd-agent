774e97bd13edb06c1162fd1d62c42be45e7c4ad1
Léo Cavaillé <leo@datadoghq.com>
2015-05-05 19:36:02 -0400

    [process] prettify & cache AccessDenied failures
    Refactor a bit this check, two noticeable changes:
    1. on Windows especially, we get randomly some `AccessDenied` exceptions
    when trying to look at some processes. Because we iterate on all the
    processes at every run, it would trigger as many failures *every run*.
    Once we get this error and `ignore_denied_access` is set to true, we'll
    not look at it again until DEFAULT_AD_CACHE_DURATION or
    `access_denied_cache_duration` in the init config.
    
    2. instead of doing some sketchy try/catch over the code, all the
    psutil-related code that actually gets metric values is wrapped into
    `psutil_wrapper` which knows how to handle exceptions and known
    failures.
    
    Last remark: before we were setting some metrics to 0 even if we had no
    values parsed, this is not the case anymore.

--- tests/checks/integration/test_process.py
+++ tests/checks/integration/test_process.py
@@ -1,12 +1,36 @@
-import os
-from nose.plugins.attrib import attr
+""" Put in integration/
+because it requires psutil to function properly
+"""
 
-from checks import AgentCheck
+# stdlib
+import os
+
+# 3p
+from mock import patch
+import psutil
+
+# project
 from tests.checks.common import AgentCheckTest
 
 
-@attr('process')
-class ProcessTestCase(AgentCheckTest):
+# cross-platform switches
+_PSUTIL_IO_COUNTERS = True
+try:
+    p = psutil.Process(os.getpid())
+    p.io_counters()
+except Exception:
+    _PSUTIL_IO_COUNTERS = False
+
+_PSUTIL_MEM_SHARED = True
+try:
+    p = psutil.Process(os.getpid())
+    p.memory_info_ex().shared
+except Exception:
+    _PSUTIL_MEM_SHARED = False
+
+
+
+class ProcessCheckTest(AgentCheckTest):
     CHECK_NAME = 'process'
 
     CONFIG_STUBS = [
@@ -101,7 +125,84 @@ class ProcessTestCase(AgentCheckTest):
         'system.processes.voluntary_ctx_switches'
     ]
 
-    def mock_find_pids(self, search_string, exact_match=True, ignore_denied_access=True):
+    def get_psutil_proc(self):
+        return psutil.Process(os.getpid())
+
+    def test_psutil_wrapper_simple(self):
+        # Load check with empty config
+        self.run_check({})
+        name = self.check.psutil_wrapper(
+            self.get_psutil_proc(),
+            'name',
+            None
+        )
+
+        self.assertNotEquals(name, None)
+
+    def test_psutil_wrapper_simple_fail(self):
+        # Load check with empty config
+        self.run_check({})
+        name = self.check.psutil_wrapper(
+            self.get_psutil_proc(),
+            'blah',
+            None
+        )
+
+        self.assertEquals(name, None)
+
+    def test_psutil_wrapper_accessors(self):
+        # Load check with empty config
+        self.run_check({})
+        meminfo = self.check.psutil_wrapper(
+            self.get_psutil_proc(),
+            'memory_info',
+            ['rss', 'vms', 'foo']
+        )
+
+        self.assertIn('rss', meminfo)
+        self.assertIn('vms', meminfo)
+        self.assertNotIn('foo', meminfo)
+
+    def test_psutil_wrapper_accessors_fail(self):
+        # Load check with empty config
+        self.run_check({})
+        meminfo = self.check.psutil_wrapper(
+            self.get_psutil_proc(),
+            'memory_infoo',
+            ['rss', 'vms']
+        )
+
+        self.assertNotIn('rss', meminfo)
+        self.assertNotIn('vms', meminfo)
+
+    def test_ad_cache(self):
+        config = {
+            'instances': [{
+                'name': 'python',
+                'search_string': ['python'],
+                'ignore_denied_access': 'false'
+            }]
+        }
+
+        def deny_cmdline(obj):
+            raise psutil.AccessDenied()
+
+        try:
+            with patch.object(psutil.Process, 'name', deny_cmdline):
+                self.run_check(config)
+        except psutil.AccessDenied:
+            pass
+
+        self.assertTrue(len(self.check.ad_cache) > 0)
+        # The next run shoudn't throw an exception
+        self.run_check(config)
+
+        # Reset the cache now
+        self.check.last_ad_cache_ts = 0
+        self.assertRaises(Exception, lambda: self.run_check, config)
+
+    def mock_find_pids(self, search_string, exact_match=True, ignore_ad=True,
+                       refresh_ad_cache=True):
         idx = search_string[0].split('_')[1]
         # Use a real PID to get real metrics!
         return [os.getpid()] * self.CONFIG_STUBS[int(idx)]['mocked_processes']
@@ -117,6 +218,8 @@ class ProcessTestCase(AgentCheckTest):
         self.run_check(config, mocks=mocks)
 
         for stub in self.CONFIG_STUBS:
+            mocked_processes = stub['mocked_processes']
+
             # Assert metrics
             for mname in self.PROCESS_METRIC:
                 proc_name = stub['config']['name']
@@ -127,10 +230,23 @@ class ProcessTestCase(AgentCheckTest):
                     expected_tags += stub['config']['tags']
 
                 expected_value = None
-                if mname == 'system.processes.number':
-                    expected_value = stub['mocked_processes']
+                # cases where we don't actually expect some metrics
+                #  - if no processes are matched we don't send metrics except number
+                #  - if io_counters() is not available skip the metrics
+                #  - if memory_info_ex() is not available skip the metrics
+                if (mocked_processes == 0 and mname != 'system.processes.number')\
+                    or (not _PSUTIL_IO_COUNTERS and '.io' in mname)\
+                    or (not _PSUTIL_MEM_SHARED and 'mem.real' in mname):
+                    continue
 
-                self.assertMetric(mname, count=1, tags=expected_tags, value=expected_value)
+                if mname == 'system.processes.number':
+                    expected_value = mocked_processes
+
+                self.assertMetric(
+                    mname, count=1,
+                    tags=expected_tags,
+                    value=expected_value
+                )
 
             # Assert service checks
             expected_tags = ['process:{0}'.format(stub['config']['name'])]
@@ -139,13 +255,11 @@ class ProcessTestCase(AgentCheckTest):
             procs = stub['mocked_processes']
 
             if critical is not None and (procs < critical[0] or procs > critical[1]):
-                expected_status = AgentCheck.CRITICAL
+                self.assertServiceCheckCritical('process.up', count=1, tags=expected_tags)
             elif warning is not None and (procs < warning[0] or procs > warning[1]):
-                expected_status = AgentCheck.WARNING
+                self.assertServiceCheckWarning('process.up', count=1, tags=expected_tags)
             else:
-                expected_status = AgentCheck.OK
-            self.assertServiceCheck('process.up', status=expected_status,
-                                    count=1, tags=expected_tags)
+                self.assertServiceCheckOK('process.up', count=1, tags=expected_tags)
 
         # Raises when COVERAGE=true and coverage < 100%
         self.coverage_report()
@@ -166,9 +280,12 @@ class ProcessTestCase(AgentCheckTest):
 
         expected_tags = ['py', 'process_name:py']
         for mname in self.PROCESS_METRIC:
+            # See same tests comment in the function `test_check`
+            if (not _PSUTIL_IO_COUNTERS and '.io' in mname)\
+                or (not _PSUTIL_MEM_SHARED and 'mem.real' in mname):
+                continue
             self.assertMetric(mname, at_least=1, tags=expected_tags)
 
-        self.assertServiceCheck('process.up', status=AgentCheck.OK,
-                                count=1, tags=['process:py'])
+        self.assertServiceCheckOK('process.up', count=1, tags=['process:py'])
 
         self.coverage_report()
