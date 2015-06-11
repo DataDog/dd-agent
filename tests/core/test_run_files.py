bd098f4e971b9c765817f8b7c2edda8de095579b
talwai <aaditya.21@gmail.com>
2015-06-11 13:54:26 -0400

    [tests] add mock pid test for linux

--- tests/core/test_run_files.py
+++ tests/core/test_run_files.py
@@ -1,7 +1,5 @@
 # stdlib
 import os
-import shlex
-import subprocess
 import unittest
 
 # 3p
@@ -24,6 +22,7 @@ class TestRunFiles(unittest.TestCase):
     # Mac run directory expected location
     _my_dir = os.path.dirname(os.path.abspath(__file__))
     _mac_run_dir = '/'.join(_my_dir.split('/')[:-4])
+    _linux_run_dir = '/opt/datadog-agent/run'
 
     @mock.patch('utils.platform.Platform.is_win32', return_value=True)
     def test_agent_status_pickle_file_win32(self, *mocks):
@@ -45,4 +44,13 @@ class TestRunFiles(unittest.TestCase):
     def test_agent_status_pickle_file_mac_source(self, *mocks):
         ''' Test pickle file location when running a Mac source install '''
         expected_path = os.path.join('/a/test/tmp/dir', 'AgentStatus.pickle')
+        self.assertEqual(AgentStatus._get_pickle_path(), expected_path)
+
+    @mock.patch('os.path.isdir', return_value=True)
+    @mock.patch('utils.pidfile.PidFile.get_dir', return_value=_linux_run_dir)
+    @mock.patch('utils.platform.Platform.is_win32', return_value=False)
+    @mock.patch('utils.platform.Platform.is_mac', return_value=False)
+    def test_agent_status_pickle_file_linux(self, *mocks):
+        ''' Test pickle file location when running on Linux '''
+        expected_path = os.path.join('/opt/datadog-agent/run', 'AgentStatus.pickle')
         self.assertEqual(AgentStatus._get_pickle_path(), expected_path)
