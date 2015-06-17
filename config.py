f1a2971b360ac461f2b8592fe7b317f53ffcc5b6
Quentin Madec <quentin.madec@datadoghq.com>
2015-06-17 17:25:18 -0400

    [core] bump agent version to 5.4.2

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ from utils.subprocess_output import subprocess
 import yaml
 
 # CONSTANTS
-AGENT_VERSION = "5.4.1"
+AGENT_VERSION = "5.4.2"
 DATADOG_CONF = "datadog.conf"
 DEFAULT_CHECK_FREQUENCY = 15   # seconds
 LOGGING_MAX_BYTES = 5 * 1024 * 1024
