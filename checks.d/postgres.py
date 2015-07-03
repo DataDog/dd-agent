571d4457dd5c679215c13d6e43a3b96c9bffafd8
Kostas <kostas.evangelinos@gmail.com>
2015-07-03 15:53:02 -0400

    - [postgres] add schema tags; support schema based filtering

--- checks.d/postgres.py
+++ checks.d/postgres.py
@@ -140,6 +140,7 @@ SELECT relname,schemaname,%s
     IDX_METRICS = {
         'descriptors': [
             ('relname', 'table'),
+            ('schemaname', 'schema'),
             ('indexrelname', 'index')
         ],
         'metrics': {
@@ -149,6 +150,7 @@ SELECT relname,schemaname,%s
         },
         'query': """
 SELECT relname,
+       schemaname,
        indexrelname,
        %s
   FROM pg_stat_user_indexes
@@ -227,6 +229,7 @@ SELECT %s
     STATIO_METRICS = {
         'descriptors': [
             ('relname', 'table'),
+            ('schemaname', 'schema')
         ],
         'metrics': {
             'heap_blks_read'  : ('postgresql.heap_blocks_read', RATE),
@@ -240,6 +243,7 @@ SELECT %s
         },
         'query': """
 SELECT relname,
+       schemaname,
        %s
   FROM pg_statio_user_tables
  WHERE relname = ANY(%s)""",
@@ -354,6 +358,25 @@ SELECT relname,
             metrics = self.replication_metrics.get(key)
         return metrics
 
+    def _build_relations_config(self, yc):
+        """Builds a dictionary from relations configuration while maintaining compatibility
+        """
+        config = {}
+        for x in yc:
+            try:
+                if isinstance(x, str):
+                    config[x] = { 'relation_name': x, 'schemas': [] }
+                elif isinstance(x, dict):
+                    name = x['relation_name']
+                    config[name] = {}
+                    config[name]['schemas'] = x['schemas'].split()
+                    config[name]['relation_name'] = name
+                else:
+                    self.log.warn('Unhandled relations config type: %s' % str(x))
+            except:
+                pass
+        return config
+        
     def _collect_stats(self, key, db, instance_tags, relations, custom_metrics):
         """Query pg_stat_* for various metrics
         If relations is not an empty list, gather per-relation metrics
@@ -389,6 +412,7 @@ SELECT relname,
                 self.SIZE_METRICS,
                 self.STATIO_METRICS
             ]
+            relations_config = self._build_relations_config(relations)
 
         replication_metrics = self._get_replication_metrics(key, db)
         if replication_metrics is not None:
@@ -413,9 +437,10 @@ SELECT relname,
                 try:
                     # if this is a relation-specific query, we need to list all relations last
                     if scope['relation'] and len(relations) > 0:
+                        relnames = relations_config.keys()
                         query = scope['query'] % (", ".join(cols), "%s")  # Keep the last %s intact
-                        self.log.debug("Running query: %s with relations: %s" % (query, relations))
-                        cursor.execute(query, (relations, ))
+                        self.log.debug("Running query: %s with relations: %s" % (query, relnames))
+                        cursor.execute(query, (relnames, ))
                     else:
                         query = scope['query'] % (", ".join(cols))
                         self.log.debug("Running query: %s" % query)
@@ -441,15 +466,26 @@ SELECT relname,
                     self.gauge("postgresql.db.count", len(results),
                         tags=[t for t in instance_tags if not t.startswith("db:")])
 
+                desc = scope['descriptors']
+
                 # parse & submit results
                 # A row should look like this
                 # (descriptor, descriptor, ..., value, value, value, value, ...)
                 # with descriptor a PG relation or index name, which we use to create the tags
                 for row in results:
-                    # turn descriptors into tags
-                    desc = scope['descriptors']
                     # Check that all columns will be processed
                     assert len(row) == len(cols) + len(desc)
+
+                    # build a map of descriptors and their values
+                    desc_map = dict(zip([x[1] for x in desc], row[0:len(desc)]))
+                    if 'schema' in desc_map:
+                        try:
+                            relname = desc_map['table']
+                            config_schemas = relations_config[relname]['schemas']
+                            if config_schemas and desc_map['schema'] not in config_schemas:
+                                continue
+                        except KeyError:
+                            pass
 
                     # Build tags
                     # descriptors are: (pg_name, dd_tag_name): value
@@ -461,7 +497,7 @@ SELECT relname,
                     else:
                         tags = [t for t in instance_tags]
 
-                    tags += ["%s:%s" % (d[0][1], d[1]) for d in zip(desc, row[:len(desc)])]
+                    tags += [("%s:%s" % (k,v)) for (k,v) in desc_map.items()]
 
                     # [(metric-map, value), (metric-map, value), ...]
                     # metric-map is: (dd_name, "rate"|"gauge")
