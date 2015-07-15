59b4df0f25583ae3058435a4f60b4a1d8ad7e30a
Kostas <20986@gaffa.net>
2015-07-14 22:32:51 -0400

    - make postgres tests aware of schema tags

--- tests/checks/integration/test_postgres.py
+++ tests/checks/integration/test_postgres.py
@@ -146,6 +146,7 @@ class TestPostgres(AgentCheckTest):
         for inst in instances:
             for rel in inst.get('relations', []):
                 expected_tags = ['db:%s' % inst['dbname'], 'table:%s' % rel]
+                expected_rel_tags = ['db:%s' % inst['dbname'], 'table:%s' % rel, 'schema:public']
                 for mname in RELATION_METRICS:
                     count = 1
                     # We only build a test index and stimulate it on breed
@@ -153,7 +154,7 @@ class TestPostgres(AgentCheckTest):
                     # here.
                     if 'index' in mname and rel != 'breed':
                         count = 0
-                    self.assertMetric(mname, count=count, tags=expected_tags)
+                    self.assertMetric(mname, count=count, tags=expected_rel_tags)
 
                 for mname in SIZE_METRICS:
                     self.assertMetric(mname, count=1, tags=expected_tags)
@@ -168,7 +169,7 @@ class TestPostgres(AgentCheckTest):
                     if 'toast' in mname:
                         at_least = 0  # how to set easily a flaky metric, w/o impacting coverage
                         count = None
-                    self.assertMetric(mname, count=count, at_least=at_least, tags=expected_tags)
+                    self.assertMetric(mname, count=count, at_least=at_least, tags=expected_rel_tags)
 
         # Index metrics
         IDX_METRICS = [
@@ -178,7 +179,7 @@ class TestPostgres(AgentCheckTest):
         ]
 
         # we have a single index defined!
-        expected_tags = ['db:dogs', 'table:breed', 'index:breed_names']
+        expected_tags = ['db:dogs', 'table:breed', 'index:breed_names', 'schema:public']
         for mname in IDX_METRICS:
             self.assertMetric(mname, count=1, tags=expected_tags)
 
