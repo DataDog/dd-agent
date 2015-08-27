407116a366cb9a5e1688140042d7a97591f622d4
Kostas <kostas.evangelinos@gmail.com>
2015-08-26 21:48:19 -0400

    - use appropriate namespace, add code for docker api

--- checks.d/cadvisor.py
+++ checks.d/cadvisor.py
@@ -14,12 +14,43 @@ from util import json
 DEFAULT_BASEURL = "http://localhost:4194/"
 DEFAULT_METRICS_CMD = "/api/v1.3/subcontainers/"
 DEFAULT_EVENTS_CMD = "/api/v1.3/events/"
+DEFAULT_DOCKER_API_ENDPOINT = "unix://var/run/docker.sock/containers/json"
+
+DEFAULT_SOCKET_TIMEOUT = 5
 
 class cAdvisor(AgentCheck):
     """ Collect metrics and events from cAdvisor instance"""
 
     def __init__(self, name, init_config, agentConfig, instances=None):
         AgentCheck.__init__(self, name, init_config, agentConfig, instances)
+
+    # def _retrieve_docker_json(self, uri, params=None, multi=False):
+    #     if params:
+    #         uri = "%s?%s" % (uri, urllib.urlencode(params))
+    #     self.log.debug("Connecting to Docker API at: %s" % uri)
+    #     req = urllib2.Request(uri, None)
+
+    #     try:
+    #         request = self.url_opener.open(req)
+    #     except urllib2.URLError, e:
+    #         if "Errno 13" in str(e):
+    #             raise Exception("Unable to connect to socket. dd-agent user must be part of the 'docker' group")
+    #         raise
+
+    #     response = request.read()
+    #     response = response.replace('\n', '') # Some Docker API versions occassionally send newlines in responses
+    #     self.log.debug('Docker API response: %s', response)
+    #     if multi and "}{" in response: # docker api sometimes returns juxtaposed json dictionaries
+    #         response = "[{0}]".format(response.replace("}{", "},{"))
+
+    #     if not response:
+    #         return []
+
+    #     try:
+    #         return json.loads(response)
+    #     except Exception as e:
+    #         self.log.error('Failed to parse Docker API response: %s', response)
+    #         raise DockerJSONDecodeError
 
     def _retrieve_json(self, url):
         try:
@@ -39,7 +70,7 @@ class cAdvisor(AgentCheck):
         self.baseurl = instance.get('baseurl', DEFAULT_BASEURL)
         self.metrics_cmd = urljoin(self.baseurl, DEFAULT_METRICS_CMD)
         self.events_cmd = urljoin(self.baseurl, DEFAULT_EVENTS_CMD)
-        self._update_metrics()
+        self._update_metrics(instance)
 
     def _discover_metrics(self, metric, dat, tags):
         type_ = type(dat)
@@ -47,13 +78,22 @@ class cAdvisor(AgentCheck):
             self.rate(metric, long(dat), tags)
         elif type_ is dict:
             for k,v in dat.iteritems():
-                self._discover_metrics(metric+'.%s'%k, v, tags)
+                self._discover_metrics(metric+'.%s'%k.lower(), v, tags)
         elif type_ is list:
             self._discover_metrics(metric, dat[-1], tags)
         else:
             return
-    
-    def _update_metrics(self):
+
+    def _retrieve_docker_tags(self):
+        params = {'all': True}
+        docker_tags = {}
+        for container in _retrieve_docker_json("unix://var/run/docker.sock/containers/json", params=params):
+            id = container['Id']
+            names = container['Names']
+            docker_tags[id] = names
+        
+    def _update_metrics(self, instance):
+        # docker_tags = self._retrieve_docker_tags()
         for subcontainer in self._retrieve_json(self.metrics_cmd):
             name = subcontainer['name']
             tags = [ 'container_name:%s' % name ]
@@ -68,20 +108,20 @@ class cAdvisor(AgentCheck):
                 namespace = subcontainer['namespace']
                 tags.append('namespace:%s' % namespace)
             except KeyError:
-                pass
+                namespace = 'kubernetes'
             
             stats = subcontainer['stats'][-1]
             try:
-                self._discover_metrics('cadvisor.cpu', stats['cpu'], tags)
+                self._discover_metrics(namespace+'.cpu', stats['cpu'], tags)
             except KeyError:
                 pass
                 
             try:
-                self._discover_metrics('cadvisor.diskio', stats['diskio'], tags)
+                self._discover_metrics(namespace+'.diskio', stats['diskio'], tags)
             except KeyError:
                 pass
 
             try:
-                self._discover_metrics('cadvisor.network', stats['network'], tags)
+                self._discover_metrics(namespace+'.network', stats['network'], tags)
             except KeyError:
                 pass
