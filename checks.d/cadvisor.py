f090642c393fb94ac284875ace9155c075cfc386
Kostas <kostas.evangelinos@gmail.com>
2015-08-27 21:48:13 -0400

    - rename to kubernetes

--- checks.d/cadvisor.py
+++ checks.d/cadvisor.py
@@ -1,4 +1,4 @@
-"""cAdvisor check
+"""kubernetes check
 Collects metrics from cAdvisor instance
 """
 
@@ -14,43 +14,12 @@ from util import json
 DEFAULT_BASEURL = "http://localhost:4194/"
 DEFAULT_METRICS_CMD = "/api/v1.3/subcontainers/"
 DEFAULT_EVENTS_CMD = "/api/v1.3/events/"
-DEFAULT_DOCKER_API_ENDPOINT = "unix://var/run/docker.sock/containers/json"
 
-DEFAULT_SOCKET_TIMEOUT = 5
-
-class cAdvisor(AgentCheck):
-    """ Collect metrics and events from cAdvisor instance"""
+class Kubernetes(AgentCheck):
+    """ Collect metrics and events from Kubernetes instance"""
 
     def __init__(self, name, init_config, agentConfig, instances=None):
         AgentCheck.__init__(self, name, init_config, agentConfig, instances)
-
-    # def _retrieve_docker_json(self, uri, params=None, multi=False):
-    #     if params:
-    #         uri = "%s?%s" % (uri, urllib.urlencode(params))
-    #     self.log.debug("Connecting to Docker API at: %s" % uri)
-    #     req = urllib2.Request(uri, None)
-
-    #     try:
-    #         request = self.url_opener.open(req)
-    #     except urllib2.URLError, e:
-    #         if "Errno 13" in str(e):
-    #             raise Exception("Unable to connect to socket. dd-agent user must be part of the 'docker' group")
-    #         raise
-
-    #     response = request.read()
-    #     response = response.replace('\n', '') # Some Docker API versions occassionally send newlines in responses
-    #     self.log.debug('Docker API response: %s', response)
-    #     if multi and "}{" in response: # docker api sometimes returns juxtaposed json dictionaries
-    #         response = "[{0}]".format(response.replace("}{", "},{"))
-
-    #     if not response:
-    #         return []
-
-    #     try:
-    #         return json.loads(response)
-    #     except Exception as e:
-    #         self.log.error('Failed to parse Docker API response: %s', response)
-    #         raise DockerJSONDecodeError
 
     def _retrieve_json(self, url):
         try:
@@ -62,9 +31,6 @@ class cAdvisor(AgentCheck):
             return None
         
         return parsed_response
-
-    def _retrieve_json_mock(self, url):
-        return json.loads(open('cadvisor-subcontainers.json').read())
 
     def check(self, instance):
         self.baseurl = instance.get('baseurl', DEFAULT_BASEURL)
@@ -83,17 +49,8 @@ class cAdvisor(AgentCheck):
             self._discover_metrics(metric, dat[-1], tags)
         else:
             return
-
-    def _retrieve_docker_tags(self):
-        params = {'all': True}
-        docker_tags = {}
-        for container in _retrieve_docker_json("unix://var/run/docker.sock/containers/json", params=params):
-            id = container['Id']
-            names = container['Names']
-            docker_tags[id] = names
-        
+    
     def _update_metrics(self, instance):
-        # docker_tags = self._retrieve_docker_tags()
         for subcontainer in self._retrieve_json(self.metrics_cmd):
             name = subcontainer['name']
             tags = [ 'container_name:%s' % name ]
