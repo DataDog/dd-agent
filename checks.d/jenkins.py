7169a84a633e1c2ee662e654c7cb040a8cdddd50
John Zeller <johnlzeller@gmail.com>
2015-09-04 15:30:28 -0400

    [jenkins] Modify warning and fix typo

--- checks.d/jenkins.py
+++ checks.d/jenkins.py
@@ -141,11 +141,14 @@ class Jenkins(AgentCheck):
         DEPRECATED:
         This Jenkins check is deprecated and not actively developed anymore. It will be
         removed in a future version of the Datadog Agent. Please move to using the Datadog
-        plugin for Jenkins. More information can be found un the Jenkins Integration panel
+        plugin for Jenkins. More information can be found on the Jenkins Integration panel
         under the Configuration tab (https://app.datadoghq.com/account/settings#integrations/jenkins)
         """
         self.warning("This check is deprecated in favor of our Jenkins Datadog plugin."
-                     " It will be removed in a future version of the Datadog Agent.")
+                     " It will be removed in a future version of the Datadog Agent."
+                     " More information can be found on the Jenkins Integration panel"
+                     " under the Configuration tab"
+                     " (https://app.datadoghq.com/account/settings#integrations/jenkins)")
 
         if self.high_watermarks.get(instance.get('name'), None) is None:
             # On the first run of check(), prime the high_watermarks dict
