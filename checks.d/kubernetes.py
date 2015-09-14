1a6a22c9677af51d68696a2008be9675b74630f1
Kostas <kostas.evangelinos@gmail.com>
2015-09-13 22:34:55 -0400

    [kube] cache default router; add initial service checks

--- checks.d/kubernetes.py
+++ checks.d/kubernetes.py
@@ -6,12 +6,12 @@ from urlparse import urljoin
 import requests
 from checks import AgentCheck
 
-DEFAULT_PORT = 4194
+DEFAULT_CADVISOR_PORT = 4194
 DEFAULT_METRICS_CMD = '/api/v1.3/subcontainers/'
-DEFAULT_EVENTS_CMD = '/api/v1.3/events/'
 DEFAULT_MAX_DEPTH = 10
 DEFAULT_NAMESPACE = 'kubernetes'
-DEFAULT_HEALTHCHECK_PORT = 10255
+DEFAULT_KUBELET_PORT = 10255
+DEFAULT_MASTER_PORT = 8080
 DEFAULT_PUBLISH_CONTAINER_NAMES = False
 
 class Kubernetes(AgentCheck):
@@ -19,7 +19,8 @@ class Kubernetes(AgentCheck):
 
     def __init__(self, name, init_config, agentConfig, instances=None):
         AgentCheck.__init__(self, name, init_config, agentConfig, instances)
-
+        self.default_router = self._get_default_router()
+        
     def _retrieve_json(self, url):
         try:
             r = requests.get(url)
@@ -40,29 +41,57 @@ class Kubernetes(AgentCheck):
 
         return None
     
-    def _perform_health_check(self, url):
+    def _perform_kubelet_checks(self, url):
+        service_check_name = self.namespace+'.kubelet.check'
         try:
             r = requests.get(url)
-            return r.text.find('ok')==0
-        except Exception:
-            return False
-        
+            if r.text.find('ok')!=-1:
+                self.service_check(service_check_name, AgentCheck.OK)
+                return
+            reason = 'health_not_ok'
+        except Exception, e:
+            reason = str(e)
+        self.service_check(service_check_name, AgentCheck.CRITICAL, 'Kubelet health check failed: %s' % reason)
+
+    def _perform_master_checks(self, url):
+        try:
+            r = requests.get(url)
+            for nodeinfo in r.json()['items']:
+                nodename = nodeinfo['name']
+                service_check_name = self.namespace+'.master.'+nodename+'.check'
+                cond = nodeinfo['status'][-1]['type']
+                if cond!='Ready':
+                    self.service_check(service_check_name, AgentCheck.CRITICAL, cond)
+                else:
+                    self.service_check(service_check_name, AgentCheck.OK)
+        except Exception, e:
+            self.log.warning('master checks url=%s exception=%s' % (url, str(e)))
+    
     def check(self, instance):
-        host = instance.get('host', self._get_default_router())
-        port = instance.get('port', DEFAULT_PORT)
+        host = instance.get('host', self.default_router)
+        port = instance.get('port', DEFAULT_CADVISOR_PORT)
         method = instance.get('method', 'http')
-        self.baseurl = '%s://%s:%d' % (method, host, port)
-        self.metrics_cmd = urljoin(self.baseurl, DEFAULT_METRICS_CMD)
-        self.events_cmd = urljoin(self.baseurl, DEFAULT_EVENTS_CMD)
+        self.metrics_url = '%s://%s:%d' % (method, host, port)
+        self.metrics_cmd = urljoin(self.metrics_url, DEFAULT_METRICS_CMD)
         self.max_depth = instance.get('max_depth', DEFAULT_MAX_DEPTH)
         self.namespace = instance.get('namespace', DEFAULT_NAMESPACE)
 
-        healthcheck_port = instance.get('healthcheck_port', DEFAULT_HEALTHCHECK_PORT)
-        healthcheck_url = '%s://%s:%d' % (method, host, healthcheck_port)
-        if not self._perform_health_check(healthcheck_url):
-            self.log.warning('Kubelet health check failed, url=%s' % healthcheck_url)
+        # master health checks
+        if instance.get('enable_master_checks', False):
+            master_port = instance.get('master_port', DEFAULT_MASTER_PORT)
+            master_host = instance.get('master_host', 'localhost')
+            master_url = '%s://%s:%d/nodes' % (method, host, master_port)
+            self._perform_master_checks(master_url)
 
-        self._update_metrics(instance, instance.get('publish_container_names', DEFAULT_PUBLISH_CONTAINER_NAMES))
+        # kubelet health checks
+        if instance.get('enable_kubelet_checks', True):
+            kubelet_port = instance.get('kubelet_port', DEFAULT_KUBELET_PORT)
+            kubelet_url = '%s://%s:%d' % (method, host, kubelet_port)
+            self._perform_kubelet_checks(kubelet_url)
+
+        # kubelet metrics
+        publish_container_names = instance.get('publish_container_names', DEFAULT_PUBLISH_CONTAINER_NAMES)
+        self._update_metrics(instance, publish_container_names)
         
     def _discover_metrics(self, metric, dat, tags, depth=0):
         if depth>=self.max_depth:
@@ -82,10 +111,12 @@ class Kubernetes(AgentCheck):
     
     def _update_metrics(self, instance, publish_container_names):
         metrics = self._retrieve_json(self.metrics_cmd)
+        service_check_name = self.namespace+'.metrics_collection'
         if not metrics:
-            self.log.warning('Unable to retrieve metrics cmd=%s' % self.metrics_cmd)
+            self.service_check(service_check_name, AgentCheck.CRITICAL, 'No metrics retrieved')
             return
         
+        self.service_check(service_check_name, AgentCheck.OK)
         for subcontainer in metrics:
             tags = []
             if publish_container_names:
