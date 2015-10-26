ef296d552f6f92e243f630a88fa35000ea093dc0
Quentin Madec <quentin.madec@datadoghq.com>
2015-10-26 22:37:59 +0100

    [core] improve detection of agent process from pid
    Sometimes the agent fails to restart after a "hard" reboot because the pidfile
    contains the pid of a non-datadog running process, and the only check the agent
    did before this commit was to check if the pid was associated with a running
    process.
    
    This commit implements a new function `is_my_process` which verifies on Linux if the
    `psutil.Process.cmdline` ("command line this process has been called with") is
    the good one (corresponding to the current executable). This function is used
    instead of `pid_exists` when `psutil` is installed and checks the pid contained
    in the pidfile, thus solving the above-mentioned problem.

--- tests/core/test_utils_process.py
+++ tests/core/test_utils_process.py
@@ -3,13 +3,14 @@ import os
 import unittest
 
 # project
-from utils.process import pid_exists
+from utils.process import is_my_process, pid_exists
 
 
 class UtilsProcessTest(unittest.TestCase):
     def test_my_own_pid(self):
         my_pid = os.getpid()
         self.assertTrue(pid_exists(my_pid))
+        self.assertTrue(is_my_process(my_pid))
 
     def test_inexistant_pid(self):
         # There will be one point where we finally find a free PID
@@ -17,3 +18,6 @@ class UtilsProcessTest(unittest.TestCase):
             if not pid_exists(pid):
                 return
         raise Exception("Probably a bug in pid_exists or more than 30000 procs!!")
+
+    def test_existing_process(self):
+        self.assertFalse(is_my_process(1))
