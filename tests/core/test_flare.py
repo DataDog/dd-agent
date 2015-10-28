ed312c24f97b5e5252385b1f6af7839a180723fd
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2015-10-28 15:39:11 -0400

    Remove passwords from URIs.
    Modifying regex, POSIX classes unavailable. Correcting typo.
    
    Adding unit test for password in uri removal.
    
    Adding a new unit test, different approach.
    
    Adding another uri password unit test with punctuation in password.
    
    Fixing typo.

--- tests/core/test_flare.py
+++ tests/core/test_flare.py
@@ -1,6 +1,7 @@
 # stdlib
 import os.path
 import unittest
+import re
 
 # 3p
 import mock
@@ -27,6 +28,16 @@ def get_mocked_temp():
         'fixtures',
         'flare'
     )
+
+mock_cfgs = {
+    'uri_password' : 'password_uri.yaml',
+}
+
+password_tests = {
+    'uri_password' : '      -   server: mongodb://datadog:V3pZC7ghx1ne82XkyqLnOW36@localhost:27017/admin',
+    'uri_password_2' : '      -   server: mongodb://datadog:V3!pZC7ghx1ne8#-2XkyqLnOW36!?@localhost:27017/admin',
+    'uri_password_expected' : '      -   server: mongodb://datadog:********@localhost:27017/admin',
+}
 
 
 def mocked_strftime(t):
@@ -121,3 +132,33 @@ class FlareTest(unittest.TestCase):
             raise Exception('Should fail before')
         except Exception, e:
             self.assertEqual(str(e), "Your request is incorrect: Invalid inputs: 'API key unknown'")
+
+    @attr(requires='core_integration')
+    @mock.patch('utils.flare.strftime', side_effect=mocked_strftime)
+    @mock.patch('tempfile.gettempdir', side_effect=get_mocked_temp)
+    @mock.patch('utils.flare.get_config', side_effect=get_mocked_config)
+    def test_uri_password(self, mock_config, mock_tempdir, mock_strftime):
+        f = Flare()
+        _, password_found = f._strip_password(os.path.join(get_mocked_temp(), mock_cfgs['uri_password']))
+        self.assertEqual(
+            password_found,
+            " - this file contains a password in a uri which has been removed in the version collected"
+        )
+
+    @attr(requires='core_integration')
+    @mock.patch('utils.flare.strftime', side_effect=mocked_strftime)
+    @mock.patch('tempfile.gettempdir', side_effect=get_mocked_temp)
+    @mock.patch('utils.flare.get_config', side_effect=get_mocked_config)
+    def test_uri_password_regex(self, mock_config, mock_tempdir, mock_strftime):
+        f = Flare()
+        line = re.sub(f.URI_REGEX, r'\1://\2:********@', password_tests['uri_password'])
+        self.assertEqual(
+            line,
+            password_tests['uri_password_expected']
+        )
+
+        line = re.sub(f.URI_REGEX, r'\1://\2:********@', password_tests['uri_password_2'])
+        self.assertEqual(
+            line,
+            password_tests['uri_password_expected']
+        )
