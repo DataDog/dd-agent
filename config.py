f376588bea9321519350687e2e9c8c9ddcb88bfb
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2015-10-30 18:09:58 -0400

    [go-metro] Adding supervisor entry and sample yaml config.
    [go-metro] updating supervisor entry.
    
    [metro] updating init scripts.
    
    [metro] adding logs to flare (unix only).
    
    [go-metro] fixing configuration to use instances key for consistency.

--- config.py
+++ config.py
@@ -919,6 +919,7 @@ def get_logging_config(cfg_path=None):
         logging_config['forwarder_log_file'] = '/var/log/datadog/forwarder.log'
         logging_config['dogstatsd_log_file'] = '/var/log/datadog/dogstatsd.log'
         logging_config['jmxfetch_log_file'] = '/var/log/datadog/jmxfetch.log'
+        logging_config['go-metro_log_file'] = '/var/log/datadog/go-metro.log'
         logging_config['log_to_syslog'] = True
 
     config_path = get_config_path(cfg_path, os_name=system_os)
