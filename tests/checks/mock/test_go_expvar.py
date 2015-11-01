613c4de79454c094c815055a4a434649329f47c0
Tim Heckman <tim@pagerduty.com>
2015-10-31 18:12:53 -0700

    [go_expvar] add configuration option for custom metric namespace
    This change adds the ability to configure a custom namespace for your `go_expvar` metrics. By default, the old value (`go_expvar`) is used. The mock tests were updated to confirm that the new behavior works as expected.
    
    In addition to the changes around the check script, the Go utility the tests use was modified as well. A manual GC trigger was added to ensure garbage collection has run at least once.

--- tests/checks/mock/test_go_expvar.py
+++ tests/checks/mock/test_go_expvar.py
@@ -20,40 +20,40 @@ class TestGoExpVar(AgentCheckTest):
     CHECK_NAME = 'go_expvar'
 
     CHECK_GAUGES = [
-        'go_expvar.memstats.alloc',
-        'go_expvar.memstats.heap_alloc',
-        'go_expvar.memstats.heap_idle',
-        'go_expvar.memstats.heap_inuse',
-        'go_expvar.memstats.heap_objects',
-        'go_expvar.memstats.heap_released',
-        'go_expvar.memstats.heap_sys',
-        'go_expvar.memstats.total_alloc',
+        'memstats.alloc',
+        'memstats.heap_alloc',
+        'memstats.heap_idle',
+        'memstats.heap_inuse',
+        'memstats.heap_objects',
+        'memstats.heap_released',
+        'memstats.heap_sys',
+        'memstats.total_alloc',
     ]
 
     CHECK_GAUGES_DEFAULT = [
-        'go_expvar.memstats.pause_ns.95percentile',
-        'go_expvar.memstats.pause_ns.avg',
-        'go_expvar.memstats.pause_ns.count',
-        'go_expvar.memstats.pause_ns.max',
-        'go_expvar.memstats.pause_ns.median',
+        'memstats.pause_ns.95percentile',
+        'memstats.pause_ns.avg',
+        'memstats.pause_ns.count',
+        'memstats.pause_ns.max',
+        'memstats.pause_ns.median',
     ]
 
     CHECK_GAUGES_CUSTOM_MOCK = {
-        'go_expvar.gauge1': ['metric_tag1:metric_value1',
-                             'metric_tag2:metric_value2',
-                             'path:random_walk'],
-        'go_expvar.memstats.by_size.1.mallocs': []
+        'gauge1': ['metric_tag1:metric_value1',
+                   'metric_tag2:metric_value2',
+                   'path:random_walk'],
+        'memstats.by_size.1.mallocs': []
     }
 
     CHECK_RATES = [
-        'go_expvar.memstats.frees',
-        'go_expvar.memstats.lookups',
-        'go_expvar.memstats.mallocs',
-        'go_expvar.memstats.num_gc',
-        'go_expvar.memstats.pause_total_ns',
+        'memstats.frees',
+        'memstats.lookups',
+        'memstats.mallocs',
+        'memstats.num_gc',
+        'memstats.pause_total_ns',
     ]
 
-    CHECK_RATES_CUSTOM_MOCK = ['go_expvar.gc.pause']
+    CHECK_RATES_CUSTOM_MOCK = ['gc.pause']
 
     def __init__(self, *args, **kwargs):
         AgentCheckTest.__init__(self, *args, **kwargs)
@@ -123,14 +123,59 @@ class TestGoExpVar(AgentCheckTest):
         shared_tags = ['optionaltag1', 'optionaltag2', 'expvar_url:{0}'.format(self._expvar_url)]
 
         for gauge in self.CHECK_GAUGES + self.CHECK_GAUGES_DEFAULT:
-            self.assertMetric(gauge, count=1, tags=shared_tags)
+            self.assertMetric("{0}.{1}".format(self.CHECK_NAME, gauge), count=1, tags=shared_tags)
         for gauge, tags in self.CHECK_GAUGES_CUSTOM_MOCK.iteritems():
-            self.assertMetric(gauge, count=1, tags=shared_tags + tags)
+            self.assertMetric("{0}.{1}".format(self.CHECK_NAME, gauge), count=1, tags=shared_tags + tags)
 
         for rate in self.CHECK_RATES:
-            self.assertMetric(rate, count=1, tags=shared_tags)
+            self.assertMetric("{0}.{1}".format(self.CHECK_NAME, rate), count=1, tags=shared_tags)
         for rate in self.CHECK_RATES_CUSTOM_MOCK:
-            self.assertMetric(rate, count=1, tags=shared_tags + ['path:memstats.PauseTotalNs'])
+            self.assertMetric("{0}.{1}".format(self.CHECK_NAME, rate), count=1, tags=shared_tags + ['path:memstats.PauseTotalNs'])
+
+        self.coverage_report()
+
+    def test_go_expvar_mocked_namespace(self):
+        metric_namespace = "testingapp"
+
+        # adjust mock config to set a namespace value
+        self.mock_config = {
+            "instances": [{
+                "namespace": metric_namespace,
+                "expvar_url": self._expvar_url,
+                "tags": ["optionaltag1", "optionaltag2"],
+                "metrics": [
+                    {
+                        # Contains list traversal and default values
+                        "path": "memstats/BySize/1/Mallocs",
+                    },
+                    {
+                        "path": "memstats/PauseTotalNs",
+                        "alias": "{0}.gc.pause".format(metric_namespace),
+                        "type": "rate"
+                    },
+                    {
+                        "path": "random_walk",
+                        "alias": "{0}.gauge1".format(metric_namespace),
+                        "type": "gauge",
+                        "tags": ["metric_tag1:metric_value1", "metric_tag2:metric_value2"]
+                    }
+                ]
+            }]
+        }
+
+        self._run_check_twice()
+
+        shared_tags = ['optionaltag1', 'optionaltag2', 'expvar_url:{0}'.format(self._expvar_url)]
+
+        for gauge in self.CHECK_GAUGES + self.CHECK_GAUGES_DEFAULT:
+            self.assertMetric("{0}.{1}".format(metric_namespace, gauge), count=1, tags=shared_tags)
+        for gauge, tags in self.CHECK_GAUGES_CUSTOM_MOCK.iteritems():
+            self.assertMetric("{0}.{1}".format(metric_namespace, gauge), count=1, tags=shared_tags + tags)
+
+        for rate in self.CHECK_RATES:
+            self.assertMetric("{0}.{1}".format(metric_namespace, rate), count=1, tags=shared_tags)
+        for rate in self.CHECK_RATES_CUSTOM_MOCK:
+            self.assertMetric("{0}.{1}".format(metric_namespace, rate), count=1, tags=shared_tags + ['path:memstats.PauseTotalNs'])
 
         self.coverage_report()
 
@@ -143,7 +188,7 @@ class TestGoExpVar(AgentCheckTest):
 
         # Default metrics
         for gauge in self.CHECK_GAUGES_DEFAULT:
-            self.assertMetric(gauge, count=1, tags=shared_tags)
+            self.assertMetric("{0}.{1}".format(self.CHECK_NAME, gauge), count=1, tags=shared_tags)
         # And then check limitation, will fail at the coverage_report if incorrect
         self.assertMetric('go_expvar.memstats.alloc', count=1, tags=shared_tags)
 
