79328872caa7877fa19fea336fb890638f37f004
Alexandre Silva <asilva@ea.com>
2015-11-10 18:00:13 +1100

    Fixed Flake8 style issues and Unix domain socket location.

--- checks.d/php_fpm.py
+++ checks.d/php_fpm.py
@@ -1,5 +1,6 @@
 # 3p
-import requests, json, logging
+import requests
+import json
 
 # FPM Client - https://github.com/saltstack/salt-contrib/blob/master/modules/flup_fcgi_client.py
 import flup_fcgi_client as fcgi_client
@@ -17,7 +18,7 @@ class PHPFPMCheck(AgentCheck):
     See http://www.php.net/manual/de/install.fpm.configuration.php#pm.status-path for more details
     """
     instance = None
-    
+
     SERVICE_CHECK_NAME = 'php_fpm.can_ping'
 
     GAUGES = {
@@ -35,11 +36,10 @@ class PHPFPMCheck(AgentCheck):
 
     def check(self, instance):
         self.instance = instance
-        
         status_location = instance.get('status_location')
         ping_location = instance.get('ping_location')
         server = instance.get('server')
-        
+
         if status_location is None and ping_location is None and server is None:
             raise Exception("No status_location, ping_location or server specified for this instance")
 
@@ -48,10 +48,10 @@ class PHPFPMCheck(AgentCheck):
         password = instance.get('password')
 
         tags = instance.get('tags', [])
-        
+
         if user and password:
             auth = (user, password)
-            
+
         if ping_location is not None:
             self._process_ping(auth, tags)
 
@@ -62,36 +62,37 @@ class PHPFPMCheck(AgentCheck):
     def get_data(self, location, auth):
         server = self.instance.get('server')
         port = str(self.instance.get('port'))
+        listen = str(self.instance.get('listen'))
         monitoring_type = self.instance.get('monitoring_type')
 
         print "Getting data from : " + server + ":" + port + " with protocol: " + monitoring_type
 
         if "fcgi" in monitoring_type.lower():      # Lets talk directly with the FPM process
-            if server.lower() is "unix": 
+            if server.lower() is "unix":
                 fcgi = fcgi_client.FCGIApp(connect = listen)
             else:
                 fcgi = fcgi_client.FCGIApp(host = server, port = port)
             # We need the Json version of it
             env = {
-               'SCRIPT_FILENAME': location,
-               'QUERY_STRING': 'json',
-               'REQUEST_METHOD': 'GET',
-               'SCRIPT_NAME': location,
-               'REQUEST_URI': location,
-               'GATEWAY_INTERFACE': 'CGI/1.1',
-               'SERVER_SOFTWARE': 'ztc',
-               'REDIRECT_STATUS': '200',
-               'CONTENT_TYPE': '',
-               'CONTENT_LENGTH': '0',
-               #'DOCUMENT_URI': url,
-               'DOCUMENT_ROOT': '/',
-               'DOCUMENT_ROOT': '/var/www/'
+                'SCRIPT_FILENAME': location,
+                'QUERY_STRING': 'json',
+                'REQUEST_METHOD': 'GET',
+                'SCRIPT_NAME': location,
+                'REQUEST_URI': location,
+                'GATEWAY_INTERFACE': 'CGI/1.1',
+                'SERVER_SOFTWARE': 'ztc',
+                'REDIRECT_STATUS': '200',
+                'CONTENT_TYPE': '',
+                'CONTENT_LENGTH': '0',
+                #'DOCUMENT_URI': url,
+                'DOCUMENT_ROOT': '/',
+                'DOCUMENT_ROOT': '/var/www/'
             }
             print env
             try:
                 code, _headers, ret, err = fcgi(env)
             except Exception, e:
-                error = "{\"Error\" : \"Cannot get location %s on server %s port %s using protocol %s : %s\"}" %(location, server, port, monitoring_type, str(e))
+                error = "{\"Error\" : \"Cannot get location %s on server %s port %s using protocol %s : %s\"}" % (location, server, port, monitoring_type, str(e))
                 self.log.error(error)
                 return None, None, error
 
@@ -109,10 +110,10 @@ class PHPFPMCheck(AgentCheck):
                                     headers=headers(self.agentConfig),
                                     params={'json': True})
                 if resp.status_code is not 200:
-                    result = "{\"Error\" : \"Status code is %s\"}"% str(resp.status_code)
+                    result = "{\"Error\" : \"Status code is %s\"}" % str(resp.status_code)
                     print result
                     return None, resp.status_code, result
-                
+
                 try:
                     ret = resp.json()
                 except Exception, e:
@@ -124,15 +125,15 @@ class PHPFPMCheck(AgentCheck):
             except Exception as e:
                 self.log.error("Failed to get metrics from {0}.\nError {1}".format(url, e))
                 return None, None, e
-    
+
     def _process_status(self, auth, tags):
         data = {}
 
         status_location = self.instance.get('status_location')
-        
-        data, code, error  = self.get_data(status_location, auth)
+
+        data, code, error = self.get_data(status_location, auth)
         print data
-        
+
         pool_name = data.get('pool', 'default')
 
         metric_tags = tags + ["pool:{0}".format(pool_name)]
@@ -161,14 +162,14 @@ class PHPFPMCheck(AgentCheck):
             ping_reply = 'pong'
 
         sc_tags = ["ping_url:{0}".format(ping_location)]
-        
-        data, code, error  = self.get_data(ping_location, auth)
+
+        data, code, error = self.get_data(ping_location, auth)
         if code is not 200 or ping_reply not in data:
             self.service_check(self.SERVICE_CHECK_NAME,
                                AgentCheck.CRITICAL, tags=sc_tags, message=str(error))
         else:
             self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.OK, tags=sc_tags)
-            
+
 # There is only here to make it easy to test/dev the Check outside the DataDog Agent Environment, e.g. from a IDE
 if __name__ == '__main__':
     #logging.basicConfig()
@@ -178,4 +179,4 @@ if __name__ == '__main__':
         check.check(instance)
         if check.has_events():
             print 'Events: %s' % (check.get_events())
-        print 'Metrics: %s' % (check.get_metrics())            
+        print 'Metrics: %s' % (check.get_metrics())
