ce60248dd4e14ad96e2f19fca4ca7bef27dc4960
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2015-12-04 11:36:43 -0500

    Rubocop'd the flavored test.
    Fixing flake8 issue.
    
    Further fixes.
    
    Conflicts:
    
    	ci/mysql.rb

--- ci/mysql.rb
+++ ci/mysql.rb
@@ -43,30 +43,46 @@ namespace :ci do
     task before_script: ['ci:common:before_script'] do
       # does travis have any mysql instance already running? :X
       # use another port?
-      if `uname`.strip != 'Darwin'
-        @ld_path += "lib/x86_64-linux-gnu/"
-      end
+      @ld_path += 'lib/x86_64-linux-gnu/' if `uname`.strip != 'Darwin'
+
+      puts "Using LD_LIBRARY_PATH=#{@ld_path}".yellow
       sh %(mkdir -p #{mysql_rootdir}/data)
       sh %(mkdir -p #{mysql_rootdir}/data_replica)
       puts 'Initializing MySQL instances.'.yellow
       system({ 'LD_LIBRARY_PATH' => @ld_path },
-             "#{mysql_rootdir}/bin/mysqld --no-defaults --initialize-insecure --basedir=#{mysql_rootdir} --datadir=#{mysql_rootdir}/data" \
-             "--log-error=#{mysql_rootdir}/data/mysql.err --socket=#{mysql_rootdir}/data/mysql.sock --pid-file=#{mysql_rootdir}/data/mysqld_safe.pid" \
-             "--performance-schema")
+             "#{mysql_rootdir}/bin/mysqld --no-defaults "\
+             "--initialize-insecure --basedir=#{mysql_rootdir} "\
+             "--datadir=#{mysql_rootdir}/data "\
+             "--log-error=#{mysql_rootdir}/data/mysql.err "\
+             "--socket=#{mysql_rootdir}/data/mysql.sock "\
+             "--pid-file=#{mysql_rootdir}/data/mysqld_safe.pid --performance-schema")
       system({ 'LD_LIBRARY_PATH' => @ld_path },
-             "#{mysql_rootdir}/bin/mysqld --no-defaults --initialize-insecure --basedir=#{mysql_rootdir} --datadir=#{mysql_rootdir}/data_replica" \
-             "--log-error=#{mysql_rootdir}/data_replica/mysql.err --socket=#{mysql_rootdir}/data_replica/mysql.sock --pid-file=#{mysql_rootdir}/data_replica/mysqld_safe.pid" \
-             "--performance-schema")
+             "#{mysql_rootdir}/bin/mysqld --no-defaults "\
+             "--initialize-insecure --basedir=#{mysql_rootdir} "\
+             "--datadir=#{mysql_rootdir}/data_replica "\
+             "--log-error=#{mysql_rootdir}/data_replica/mysql.err "\
+             "--socket=#{mysql_rootdir}/data_replica/mysql.sock"\
+             "--pid-file=#{mysql_rootdir}/data_replica/mysqld_safe.pid --performance-schema")
       # let the init process complete
       sleep_for 2
       system({ 'LD_LIBRARY_PATH' => @ld_path },
-             "#{mysql_rootdir}/bin/mysqld --no-defaults --basedir=#{mysql_rootdir} --datadir=#{mysql_rootdir}/data --plugin-dir=#{mysql_rootdir}/lib/plugin" \
-             "--log-error=#{mysql_rootdir}/data/mysql.err --socket=#{mysql_rootdir}/data/mysql.sock --pid-file=#{mysql_rootdir}/data/mysqld_safe.pid --port=3308" \
-             "--log-bin=mysql-bin --server-id=1 --performance-schema --daemonize >/dev/null 2>&1")
+             "#{mysql_rootdir}/bin/mysqld --no-defaults "\
+             "--basedir=#{mysql_rootdir} "\
+             "--datadir=#{mysql_rootdir}/data "\
+             "--plugin-dir=#{mysql_rootdir}/lib/plugin "\
+             "--log-error=#{mysql_rootdir}/data/mysql.err --log-bin=mysql-bin "\
+             "--socket=#{mysql_rootdir}/data/mysql.sock "\
+             "--pid-file=#{mysql_rootdir}/data/mysqld_safe.pid "\
+             '--port=3308 --server-id=1 --performance-schema --daemonize >/dev/null 2>&1')
       system({ 'LD_LIBRARY_PATH' => @ld_path },
-             "#{mysql_rootdir}/bin/mysqld --no-defaults --basedir=#{mysql_rootdir} --datadir=#{mysql_rootdir}/data_replica --plugin-dir=#{mysql_rootdir}/lib/plugin" \
-             "--log-error=#{mysql_rootdir}/data_replica/mysql.err --socket=#{mysql_rootdir}/data_replica/mysql.sock --pid-file=#{mysql_rootdir}/data_replica/mysqld_safe.pid" \
-             "--port=3310 --server-id=2 --performance-schema --daemonize >/dev/null 2>&1")
+             "#{mysql_rootdir}/bin/mysqld --no-defaults "\
+             "--basedir=#{mysql_rootdir} "\
+             "--datadir=#{mysql_rootdir}/data_replica "\
+             "--plugin-dir=#{mysql_rootdir}/lib/plugin "\
+             "--log-error=#{mysql_rootdir}/data_replica/mysql.err "\
+             "--socket=#{mysql_rootdir}/data_replica/mysql.sock "\
+             "--pid-file=#{mysql_rootdir}/data_replica/mysqld_safe.pid "\
+             '--port=3310 --server-id=2 --performance-schema --daemonize >/dev/null 2>&1')
       Wait.for 33_08, 10
       Wait.for 33_10, 10
       # set-up replication
@@ -76,16 +92,19 @@ namespace :ci do
       sh %(#{mysql_rootdir}/bin/mysql -e "create user 'dog'@'localhost' identified by 'dog'" -uroot --socket=#{mysql_rootdir}/data/mysql.sock)
       sh %(#{mysql_rootdir}/bin/mysql -e "GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'dog'@'localhost' WITH MAX_USER_CONNECTIONS 5;" \
            -uroot --socket=#{mysql_rootdir}/data/mysql.sock)
-      sh %(#{mysql_rootdir}/bin/mysql -e "CHANGE MASTER TO MASTER_HOST='localhost', MASTER_PORT=3309, MASTER_USER='repl', MASTER_PASSWORD='slavedog', MASTER_LOG_FILE='', MASTER_LOG_POS=4;" \
-           -uroot --socket=#{mysql_rootdir}/data_replica/mysql.sock)
+      sql = "CHANGE MASTER TO MASTER_HOST='localhost', MASTER_PORT=3309, MASTER_USER='repl', MASTER_PASSWORD='slavedog', "
+      sql += "MASTER_LOG_FILE='', MASTER_LOG_POS=4;"
+      sh %(#{mysql_rootdir}/bin/mysql -e "#{sql}" -uroot --socket=#{mysql_rootdir}/data_replica/mysql.sock)
       sh %(#{mysql_rootdir}/bin/mysql -e "START SLAVE;" -uroot --socket=#{mysql_rootdir}/data_replica/mysql.sock)
       # lets add some data to our master.
       sh %(#{mysql_rootdir}/bin/mysql -e "CREATE DATABASE testdb;" -uroot --socket=#{mysql_rootdir}/data/mysql.sock)
       sh %(#{mysql_rootdir}/bin/mysql -e "CREATE TABLE testdb.users (name VARCHAR(20), age INT);" -uroot --socket=#{mysql_rootdir}/data/mysql.sock)
-      sh %(#{mysql_rootdir}/bin/mysql -e "GRANT SELECT ON testdb.users TO 'dog'@'localhost';" -uroot --socket=#{mysql_rootdir}/data/mysql.sock)
+      sh %(#{mysql_rootdir}/bin/mysql -e "GRANT SELECT ON testdb.users TO 'dog'@'localhost'; "\
+           -uroot --socket=#{mysql_rootdir}/data/mysql.sock)
       sh %(#{mysql_rootdir}/bin/mysql -e "INSERT INTO testdb.users (name,age) VALUES('Alice',25);" -uroot --socket=#{mysql_rootdir}/data/mysql.sock)
       sh %(#{mysql_rootdir}/bin/mysql -e "INSERT INTO testdb.users (name,age) VALUES('Bob',20);" -uroot --socket=#{mysql_rootdir}/data/mysql.sock)
-      sh %(#{mysql_rootdir}/bin/mysql -e "GRANT SELECT ON performance_schema.* TO 'dog'@'localhost';" -uroot --socket=#{mysql_rootdir}/data/mysql.sock)
+      sh %(#{mysql_rootdir}/bin/mysql -e "GRANT SELECT ON performance_schema.* TO 'dog'@'localhost';"\
+           -uroot --socket=#{mysql_rootdir}/data/mysql.sock)
       # generate some performance metrics....
       sh %(#{mysql_rootdir}/bin/mysql -e "USE testdb; SELECT * FROM users ORDER BY name;" -uroot --socket=#{mysql_rootdir}/data/mysql.sock)
     end
@@ -102,8 +121,6 @@ namespace :ci do
     task cache: ['ci:common:cache']
 
     task cleanup: ['ci:common:cleanup'] do
-      sh %(mysql -e "DROP USER 'dog'@'localhost';" -uroot --socket=#{mysql_rootdir}/data/mysql.sock)
-      sh %(mysql -e "DROP DATABASE testdb;" -uroot --socket=#{mysql_rootdir}/data/mysql.sock)
       `pgrep -f "#{mysql_rootdir}/bin/mysqld" `.split("\n").each do |spid|
         begin
           p = spid.to_i
@@ -115,8 +132,8 @@ namespace :ci do
       end
       puts 'Waiting for MySQL instances to shutdown.'.yellow
       sleep_for 2
-      sh %(rm -rf #{mysql_rootdir}/data)
-      sh %(rm -rf #{mysql_rootdir}/data_replica)
+      # sh %(rm -rf #{mysql_rootdir}/data)
+      # sh %(rm -rf #{mysql_rootdir}/data_replica)
     end
 
     task :execute do
