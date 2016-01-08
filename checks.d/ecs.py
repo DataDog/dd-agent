1a5f58c2ca2fe821d45d6ee0eb6ab5f24e2745af
Eric J. Holmes <eric@ejholmes.net>
2016-01-09 05:00:33 +0700

    More pythonic.

--- checks.d/ecs.py
+++ checks.d/ecs.py
@@ -15,37 +15,33 @@ METADATA_URL = 'http://127.0.0.1:51678/v1/metadata'
 SERVICE_CHECK = 'ecs.agent_connected'
 
 
+# Unfortunately, the stable release of boto doesn't include https://github.com/boto/boto/pull/3143.
+def regions():
+    return get_regions('ec2containerservice', connection_cls=EC2ContainerServiceConnection)
+
 class ECS(AgentCheck):
     """Tracks agent connection status
     """
-
-    def __init__(self, name, init_config, agentConfig, instances=None):
-        AgentCheck.__init__(self, name, init_config, agentConfig, instances)
 
     def check(self, instance):
         ecs = self.connect_to_region(instance.get('region'))
 
         metadata = requests.get(METADATA_URL).json()
         cluster = metadata.get('Cluster')
-        container_instance = metadata.get('ContainerInstanceArn')
+        container_instance = metadata['ContainerInstanceArn']
 
         desc = ecs.describe_container_instances(container_instance, cluster)
-        container_instances = desc.get('containerInstances')
+        container_instances = desc['containerInstances']
 
-        if len(container_instances) == 0:
+        if not container_instances:
             return self.service_check(SERVICE_CHECK, AgentCheck.UNKNOWN)
-        if container_instances[0].get('agentConnected') == True:
+        if container_instances[0].get('agentConnected'):
             return self.service_check(SERVICE_CHECK, AgentCheck.OK)
         else:
             return self.service_check(SERVICE_CHECK, AgentCheck.WARNING)
 
-    def connect_to_region(region_name, **kw_params):
+    def connect_to_region(region_name, **kwargs):
         for region in regions():
             if region.name == region_name:
-                return region.connect(**kw_params)
+                return region.connect(**kwargs)
         return None
-
-# Unfortunately, the stable release of boto doesn't include https://github.com/boto/boto/pull/3143.
-def regions():
-    return get_regions('ec2containerservice', connection_cls=EC2ContainerServiceConnection)
-
