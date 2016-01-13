7bfefebad930fb00adad07b833ebf0f05d4ccdf6
Kostas <kostas.evangelinos@gmail.com>
2016-01-12 21:12:40 -0500

    [ceph] flake8 cleanup

--- checks.d/ceph.py
+++ checks.d/ceph.py
@@ -26,16 +26,16 @@ class Ceph(AgentCheck):
         raw = fd.read()
         fd.close()
         return json.loads(raw)
-    
+
     def _collect_raw(self, ceph_cmd, instance):
         use_sudo = _is_affirmative(instance.get('use_sudo', False))
         if use_sudo:
             test_sudo = os.system('setsid sudo -l < /dev/null')
             if test_sudo != 0:
                 raise Exception('The dd-agent user does not have sudo access')
-                
+
         raw = {}
-        for cmd in ( 'mon_status', 'status', 'df detail', 'osd pool stats', 'osd perf' ):
+        for cmd in ('mon_status', 'status', 'df detail', 'osd pool stats', 'osd perf'):
             try:
                 if use_sudo:
                     args = ['sudo', ceph_cmd]
@@ -48,7 +48,7 @@ class Ceph(AgentCheck):
             except Exception, e:
                 self.log.warning('Unable to parse data from cmd=%s: %s' % (cmd, str(e)))
                 continue
-            
+
             name = cmd.replace(' ', '_')
             raw[name] = res
 
@@ -64,20 +64,20 @@ class Ceph(AgentCheck):
         name = instance.get('name', None)
         if name:
             tags.append(NAMESPACE + '_name:%s' % name)
-        
+
         self.tags = tags
-        
+
     def _extract_metrics(self, raw):
         if 'osd_perf' in raw:
             for osdperf in raw['osd_perf']['osd_perf_infos']:
-                tags = self.tags + [ 'ceph_osd:osd%s' % osdperf['id'] ]
+                tags = self.tags + ['ceph_osd:osd%s' % osdperf['id']]
                 for k,v in osdperf['perf_stats'].iteritems():
                     self.gauge(NAMESPACE + '.' + k, v, tags)
 
         if 'osd_pool_stats' in raw:
             for osdinfo in raw['osd_pool_stats']:
                 name = osdinfo['pool_name']
-                tags = self.tags + [ 'ceph_pool:%s' % name ]
+                tags = self.tags + ['ceph_pool:%s' % name]
                 for k,v in osdinfo['client_io_rate'].iteritems():
                     self.gauge(NAMESPACE + '.' + k, v, tags)
 
@@ -92,7 +92,7 @@ class Ceph(AgentCheck):
             for pgstate in pgstatus['pgs_by_state']:
                 s_name = pgstate['state_name'].replace("+", "_")
                 self.gauge(NAMESPACE + '.pgstate.' + s_name, pgstate['count'], self.tags)
-        
+
         if 'mon_status' in raw:
             num_mons = len(raw['mon_status']['monmap']['mons'])
             self.gauge(NAMESPACE + '.num_mons', num_mons, self.tags)
@@ -102,17 +102,17 @@ class Ceph(AgentCheck):
             self.gauge(NAMESPACE + '.total_objects', stats['total_objects'], self.tags)
             used = float(stats['total_used_bytes'])
             avail = float(stats['total_avail_bytes'])
-            if avail>0:
+            if avail > 0:
                 self.gauge(NAMESPACE + '.aggregate_pct_used', 100.0*used/avail, self.tags)
 
             l_pools = raw['df_detail']['pools']
             self.gauge(NAMESPACE + '.num_pools', len(l_pools), self.tags)
-            for pdata in l_pools:                
-                tags = list(self.tags + [ NAMESPACE + '_pool_name:%s' % pdata['name'] ])
+            for pdata in l_pools:
+                tags = list(self.tags + [NAMESPACE + '_pool_name:%s' % pdata['name']])
                 stats = pdata['stats']
                 used = float(stats['bytes_used'])
                 avail = float(stats['max_avail'])
-                if avail>0:
+                if avail > 0:
                     self.gauge(NAMESPACE + '.pct_used', 100.0*used/avail, tags)
                 self.gauge(NAMESPACE + '.num_objects', stats['objects'], tags)
                 self.rate(NAMESPACE + '.rd_bytes', stats['rd_bytes'], tags)
@@ -121,7 +121,7 @@ class Ceph(AgentCheck):
     def _perform_service_checks(self, raw):
         if 'status' in raw:
             s_status = raw['status']['health']['overall_status']
-            if s_status.find('_OK')!=-1:
+            if s_status.find('_OK') != -1:
                 status = AgentCheck.OK
             else:
                 status = AgentCheck.CRITICAL
