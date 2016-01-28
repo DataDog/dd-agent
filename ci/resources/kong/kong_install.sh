30842f3b0e05b182ce0d39780d2c36fcc4b2c6dd
Shashi Ranjan <shashi@mashape.com>
2016-01-27 20:56:47 -0800

    updated ci script

--- ci/resources/kong/kong_install.sh
+++ ci/resources/kong/kong_install.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+pwd
+echo $INTEGRATIONS_DIR
+mkdir $INTEGRATIONS_DIR/luajit
+mkdir $INTEGRATIONS_DIR/luarocks
+mkdir $INTEGRATIONS_DIR/openresty
+mkdir $INTEGRATIONS_DIR/serf
+mkdir $INTEGRATIONS_DIR/dnsmasq
+
 export LUA_VERSION=luajit-2.1
 export CASSANDRA_VERSION=2.2.4
 export LUAROCKS_VERSION=2.2.2
@@ -9,24 +17,38 @@ export OPENSSL_VERSION=1.0.2e
 export OPENRESTY_VERSION=1.9.3.1
 export SERF_VERSION=0.7.0
 export DNSMASQ_VERSION=2.75
-export LUAJIT_DIR=$INTEGRATIONS_DIR/kong/luajit
-export LUAROCKS_DIR=$INTEGRATIONS_DIR/kong/luarocks
-export OPENRESTY_DIR=$INTEGRATIONS_DIR/kong/openresty
-export SERF_DIR=$INTEGRATIONS_DIR/kong/serf
-export DNSMASQ_DIR=$INTEGRATIONS_DIR/kong/dnsmasq
+export LUAJIT_DIR=$INTEGRATIONS_DIR/luajit
+export LUAROCKS_DIR=$INTEGRATIONS_DIR/luarocks
+export OPENRESTY_DIR=$INTEGRATIONS_DIR/openresty
+export SERF_DIR=$INTEGRATIONS_DIR/serf
+export DNSMASQ_DIR=$INTEGRATIONS_DIR/dnsmasq
 export CASSANDRA_HOSTS=127.0.0.1
 
-wget -P .ci/ https://raw.githubusercontent.com/Mashape/kong/feat/invalidations/.ci/platform.sh
-wget -O - https://raw.githubusercontent.com/Mashape/kong/feat/invalidations/.ci/setup_lua.sh | bash
-wget -O - https://raw.githubusercontent.com/Mashape/kong/feat/invalidations/.ci/setup_openresty.sh | bash
-wget -O - https://raw.githubusercontent.com/Mashape/kong/feat/invalidations/.ci/setup_serf.sh | bash
-wget -O - https://raw.githubusercontent.com/Mashape/kong/feat/invalidations/.ci/setup_dnsmasq.sh | bash
+mkdir $INTEGRATIONS_DIR/.ci
+pushd $INTEGRATIONS_DIR
+
+wget -P $INTEGRATIONS_DIR/.ci/ https://raw.githubusercontent.com/Mashape/kong/feat/invalidations/.ci/platform.sh
+wget -P $INTEGRATIONS_DIR/ - https://raw.githubusercontent.com/Mashape/kong/feat/invalidations/.ci/setup_lua.sh | bash
+wget -P $INTEGRATIONS_DIR/ - https://raw.githubusercontent.com/Mashape/kong/feat/invalidations/.ci/setup_openresty.sh | bash
+wget -P $INTEGRATIONS_DIR/ - https://raw.githubusercontent.com/Mashape/kong/feat/invalidations/.ci/setup_serf.sh | bash
+wget -P $INTEGRATIONS_DIR/ - https://raw.githubusercontent.com/Mashape/kong/feat/invalidations/.ci/setup_dnsmasq.sh | bash
+
+
 
 export PATH="$LUAJIT_DIR/bin:$LUAROCKS_DIR/bin:$OPENRESTY_DIR/nginx/sbin:$SERF_DIR:$DNSMASQ_DIR/usr/local/sbin:$PATH"
 export LUA_PATH="./?.lua;$LUAROCKS_DIR/share/lua/5.1/?.lua;$LUAROCKS_DIR/share/lua/5.1/?/init.lua;$LUAROCKS_DIR/lib/lua/5.1/?.lua;$LUA_PATH"
 export LUA_CPATH="./?.so;$LUAROCKS_DIR/lib/lua/5.1/?.so;$LUA_CPATH"
 
-wget -O kong.tar.gz https://github.com/Mashape/kong/archive/0.6.0.tar.gz
-tar xzf kong.tar.gz
+wget -O $INTEGRATIONS_DIR/kong/kong.tar.gz https://github.com/Mashape/kong/archive/0.6.0.tar.gz
+tar xvzf $INTEGRATIONS_DIR/kong/kong.tar.gz -C $INTEGRATIONS_DIR/kong/ --strip-components=1
 
-luarocks make ./kong-0.6.0/kong-*.rockspec
+mkdir $INTEGRATIONS_DIR/kong/util
+wget -O $INTEGRATIONS_DIR/kong/util-linux-2.27.tar.gz https://www.kernel.org/pub/linux/utils/util-linux/v2.27/util-linux-2.27.tar.gz
+tar xvzf $INTEGRATIONS_DIR/kong/util-linux-2.27.tar.gz -C $INTEGRATIONS_DIR/kong/util --strip-components=1
+luarocks install lua_uuid UUID_LIBDIR=$INTEGRATIONS_DIR/kong/util/libuuid/src/
+
+pwd
+pushd $INTEGRATIONS_DIR/kong
+luarocks make kong-*.rockspec
+popd
+popd
