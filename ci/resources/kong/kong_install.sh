4d4a749efe25919295cdb186e58f14c08551d6a2
Shashi Ranjan <shashi@mashape.com>
2016-01-28 00:17:44 -0800

    updated ci script

--- ci/resources/kong/kong_install.sh
+++ ci/resources/kong/kong_install.sh
@@ -8,7 +8,7 @@ pushd $INTEGRATIONS_DIR/kong
 wget -O $VOLATILE_DIR/kong.tar.gz https://github.com/Mashape/kong/archive/0.6.0.tar.gz
 tar xvzf $VOLATILE_DIR/kong.tar.gz -C . --strip-components=1
 
-mkdir $INTEGRATIONS_DIR/util
+mkdir ./util
 wget -O $VOLATILE_DIR/util-linux-2.27.tar.gz https://www.kernel.org/pub/linux/utils/util-linux/v2.27/util-linux-2.27.tar.gz
 tar xvzf $VOLATILE_DIR/util-linux-2.27.tar.gz -C ./util --strip-components=1
 luarocks install lua_uuid UUID_LIBDIR=./util/libuuid/src/
