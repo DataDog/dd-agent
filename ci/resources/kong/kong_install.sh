9606c8a60857177ff29a318b6766514430edf50c
Shashi Ranjan <shashi@mashape.com>
2016-01-28 00:59:54 -0800

    updated ci script

--- ci/resources/kong/kong_install.sh
+++ ci/resources/kong/kong_install.sh
@@ -8,6 +8,7 @@ pushd $INTEGRATIONS_DIR/kong
 wget -O $VOLATILE_DIR/kong.tar.gz https://github.com/Mashape/kong/archive/0.6.0.tar.gz
 tar xvzf $VOLATILE_DIR/kong.tar.gz -C . --strip-components=1
 
+echo $PATH
 mkdir ./util
 wget -O $VOLATILE_DIR/util-linux-2.27.tar.gz https://www.kernel.org/pub/linux/utils/util-linux/v2.27/util-linux-2.27.tar.gz
 tar xvzf $VOLATILE_DIR/util-linux-2.27.tar.gz -C ./util --strip-components=1
