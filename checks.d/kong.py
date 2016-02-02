648272b4bdb6e70359d72c17160bdcbe2a1db30f
Shashi Ranjan <shashi@mashape.com>
2016-02-01 16:36:26 -0800

    integration(kong) Added Kong checks
    Kong is an open-source management layer for API. More details about Kong can be found at
    website:https://getkong.org/
    github:https://github.com/Mashape/kong

---
+++ checks.d/kong.py
@@ -0,0 +1,71 @@
+# stdlib
+import urlparse
+
+# 3rd party
+import requests
+import simplejson as json
+
+# project
+from checks import AgentCheck
+from util import headers
+
+
+class Kong(AgentCheck):
+    """ collects metrics for Kong """
+    def check(self, instance):
+        metrics = self._fetch_data(instance)
+        for row in metrics:
+            try:
+                name, value, tags = row
+                self.gauge(name, value, tags)
+            except Exception:
+                self.log.error(u'Could not submit metric: %s', row)
+
+    def _fetch_data(self, instance):
+        if 'kong_status_url' not in instance:
+            raise Exception('missing "kong_status_url" value')
+        tags = instance.get('tags', [])
+        url = instance.get('kong_status_url')
+
+        parsed_url = urlparse.urlparse(url)
+        host = parsed_url.hostname
+        port = parsed_url.port
+        service_check_name = 'kong.can_connect'
+        service_check_tags = ['kong_host:%s' % host, 'kong_port:%s' % port]
+
+        try:
+            self.log.debug(u"Querying URL: {0}".format(url))
+            response = requests.get(url, headers=headers(self.agentConfig))
+            self.log.debug(u"Kong status `response`: {0}".format(response))
+            response.raise_for_status()
+        except Exception:
+            self.service_check(service_check_name, AgentCheck.CRITICAL,
+                               tags=service_check_tags)
+            raise
+        else:
+            if response.status_code == 200:
+                self.service_check(service_check_name, AgentCheck.OK,
+                               tags=service_check_tags)
+            else:
+                self.service_check(service_check_name, AgentCheck.CRITICAL,
+                               tags=service_check_tags)
+
+        return self._parse_json(response.content, tags)
+
+    def _parse_json(self, raw, tags=None):
+        if tags is None:
+            tags = []
+        parsed = json.loads(raw)
+        metric_base = 'kong'
+        output = []
+        tagged_keys = ['database', 'server']
+        for key in tagged_keys:
+            metric_prefix = '%s.%s' % (metric_base, key)
+            metric_list = []
+            for name, data in parsed.get(key).items():
+                metric_name = '%s.%s' % (metric_prefix, name)
+                metric_list.append((metric_name, data, tags))
+
+            output.extend(metric_list)
+
+        return output
