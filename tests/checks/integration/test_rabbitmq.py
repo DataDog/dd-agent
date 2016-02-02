ac0a7fe20559e50c59293e215a3a15dcf7045640
Joshu Coats <joshu@stripe.com>
2016-02-02 15:52:39 -0800

    [rabbitmq] add a way to tag multiple queues in the same "family"

--- tests/checks/integration/test_rabbitmq.py
+++ tests/checks/integration/test_rabbitmq.py
@@ -28,12 +28,38 @@ CONFIG_REGEX = {
     ]
 }
 
+CONFIG_WITH_FAMILY = {
+    'init_config': {},
+    'instances': [
+        {
+            'rabbitmq_api_url': 'http://localhost:15672/api/',
+            'rabbitmq_user': 'guest',
+            'rabbitmq_pass': 'guest',
+            'tag_families': True,
+            'queues_regexes': ['(test)\d+'],
+        }
+    ]
+}
+
 COMMON_METRICS = [
     'rabbitmq.node.fd_used',
     'rabbitmq.node.mem_used',
     'rabbitmq.node.run_queue',
     'rabbitmq.node.sockets_used',
     'rabbitmq.node.partitions'
+]
+
+Q_METRICS = [
+    'consumers',
+    'memory',
+    'messages',
+    'messages.rate',
+    'messages_ready',
+    'messages_ready.rate',
+    'messages_unacknowledged',
+    'messages_unacknowledged.rate',
+    'messages.publish.count',
+    'messages.publish.rate',
 ]
 
 
@@ -53,18 +79,6 @@ class RabbitMQCheckTest(AgentCheckTest):
         # Queue attributes, should be only one queue fetched
         # TODO: create a 'fake consumer' and get missing metrics
         # active_consumers, acks, delivers, redelivers
-        Q_METRICS = [
-            'consumers',
-            'memory',
-            'messages',
-            'messages.rate',
-            'messages_ready',
-            'messages_ready.rate',
-            'messages_unacknowledged',
-            'messages_unacknowledged.rate',
-            'messages.publish.count',
-            'messages.publish.rate',
-        ]
         for mname in Q_METRICS:
             self.assertMetricTag('rabbitmq.queue.%s' %
                                  mname, 'rabbitmq_queue:test1', count=1)
@@ -80,18 +94,6 @@ class RabbitMQCheckTest(AgentCheckTest):
         for mname in COMMON_METRICS:
             self.assertMetricTagPrefix(mname, 'rabbitmq_node', count=1)
 
-        Q_METRICS = [
-            'consumers',
-            'memory',
-            'messages',
-            'messages.rate',
-            'messages_ready',
-            'messages_ready.rate',
-            'messages_unacknowledged',
-            'messages_unacknowledged.rate',
-            'messages.publish.count',
-            'messages.publish.rate',
-        ]
         for mname in Q_METRICS:
             self.assertMetricTag('rabbitmq.queue.%s' %
                                  mname, 'rabbitmq_queue:test1', count=1)
@@ -102,4 +104,19 @@ class RabbitMQCheckTest(AgentCheckTest):
 
         self.assertServiceCheckOK('rabbitmq.aliveness', tags=['vhost:/'])
 
+        self.coverage_report()
+
+    def test_family_tagging(self):
+        self.run_check(CONFIG_WITH_FAMILY)
+
+        # Node attributes
+        for mname in COMMON_METRICS:
+            self.assertMetricTagPrefix(mname, 'rabbitmq_node', count=1)
+
+        for mname in Q_METRICS:
+            self.assertMetricTag('rabbitmq.queue.%s' %
+                                 mname, 'rabbitmq_queue_family:test', count=2)
+
+        self.assertServiceCheckOK('rabbitmq.aliveness', tags=['vhost:/'])
+        
         self.coverage_report()
