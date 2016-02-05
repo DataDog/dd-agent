e869cbc61590d6ce01f6f04337c1baeee0d043f8
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2016-02-05 13:30:17 -0500

    [wmi] make freeze() python2.6 compatible. Iterate w/ iteritems() instead of items().

--- utils/containers.py
+++ utils/containers.py
@@ -6,7 +6,7 @@ def freeze(o):
     Accepts nested dictionaries.
     """
     if isinstance(o, dict):
-        return frozenset({k:freeze(v) for k,v in o.items()}.items())
+        return frozenset(dict([(k, freeze(v)) for k,v in o.iteritems()]).iteritems())
 
     if isinstance(o, list):
         return tuple([freeze(v) for v in o])
