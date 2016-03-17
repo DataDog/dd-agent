2b26f1416b817f133276bf7f4c869ed19976ad5d
Fabrice Vaillant <fabrice.vaillant@datadoghq.com>
2016-03-17 16:57:17 -0400

    Basic working statsd sender
    It only treats gauge

--- statsdemitter.py
+++ statsdemitter.py
@@ -3,4 +3,32 @@ import statsd
 def emitter(message, log, agentConfig, endpoint):
     "Send payload"
     log.debug('statsd_emitter: attempting postback to {host}:{port}'.format(host=agentConfig['statsd_host'], port=agentConfig['statsd_port']))
-    statsd.StatsClient(agentConfig['statsd_host'], agentConfig['statsd_port'], agentConfig['statsd_prefix'], agentConfig['statsd_maxudpsize'])
+    c = statsd.StatsClient(agentConfig['statsd_host'], agentConfig['statsd_port'], agentConfig['statsd_prefix'], agentConfig['statsd_maxudpsize'])
+
+    value_by_name, type_by_name = _aggregate_metrics(message['metrics'])
+    for name, value in value_by_name.iteritems():
+        if type_by_name[name] == 'gauge':
+            print "name : {name}, value : {value}".format(name=name, value=value)
+            c.gauge(name, value)
+
+def _aggregate_metrics(metrics):
+    "To deal with metric with the same name and different tags we just average them"
+    value_by_name = {}
+    type_by_name = {}
+
+    for metric in metrics:
+        name = metric[0]
+        # We are going to assume to assume 2 metrics with the same name share the same type
+        type_by_name[name] = metric[3]['type']
+        value_by_name.setdefault(name, [])
+        value_by_name[name].append(metric[2])
+
+    for name in value_by_name:
+        value_by_name[name] = list_average(value_by_name[name])
+    return value_by_name, type_by_name
+
+def list_average(list):
+    sum = 0
+    for item in list:
+        sum += item
+    return sum/len(list)
