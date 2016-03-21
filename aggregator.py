331c507f164ec7bd256a32beb3889d4fafb0ba7e
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2016-03-21 19:03:05 -0400

    [dogstatsd][sc] Fix parsing of tag key ending with `m`
    On service checks, tags which keys finished with `m:` were mistaken for
    the beginning of a message metadata field.
    
    Fix the behavior, and add a test.

--- aggregator.py
+++ aggregator.py
@@ -558,7 +558,7 @@ class Aggregator(object):
                 'status': int(status)
             }
 
-            message_delimiter = '|m:' if '|m:' in metadata else 'm:'
+            message_delimiter = 'm:' if metadata.startswith('m:') else '|m:'
             if message_delimiter in metadata:
                 meta, message = metadata.rsplit(message_delimiter, 1)
                 service_check['message'] = self._unescape_sc_content(message)
