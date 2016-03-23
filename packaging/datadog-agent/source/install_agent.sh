fa450b6e9639be0426cdcbfc8f5fdaaf7ac3eb92
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2016-03-23 11:48:02 -0400

    [install script] Ignore `apt-get update` failures
    We don't really care whether `apt-get update` succeeds or not when
    installing apt-transport-https, but we do want to avoid having the
    whole install script fail when that fails.
    
    [skip ci]

--- packaging/datadog-agent/source/install_agent.sh
+++ packaging/datadog-agent/source/install_agent.sh
@@ -128,7 +128,7 @@ if [ $OS = "RedHat" ]; then
     $sudo_cmd yum -y --disablerepo='*' --enablerepo='datadog' install datadog-agent
 elif [ $OS = "Debian" ]; then
     printf "\033[34m\n* Installing apt-transport-https\n\033[0m\n"
-    $sudo_cmd apt-get update
+    $sudo_cmd apt-get update || printf "\033[31m'apt-get update' failed, the script will not install the latest version of apt-transport-https.\033[0m\n"
     $sudo_cmd apt-get install -y apt-transport-https
     printf "\033[34m\n* Installing APT package sources for Datadog\n\033[0m\n"
     $sudo_cmd sh -c "echo 'deb http://apt.datadoghq.com/ stable main' > /etc/apt/sources.list.d/datadog.list"
