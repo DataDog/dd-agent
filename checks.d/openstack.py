d1ffd092e6148d7414f406fd6084aa05f119c63b
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2016-05-19 18:49:23 -0400

    [openstack] enhanced logging for auth failures.
    [openstack] try alternative json auth payload with name instead of id keys.

--- checks.d/openstack.py
+++ checks.d/openstack.py
@@ -137,10 +137,32 @@ class OpenStackProjectScope(object):
         auth_scope = cls.get_auth_scope(instance_config)
         identity = cls.get_user_identity(instance_config)
 
+        exception_msg = None
         try:
             auth_resp = cls.request_auth_token(auth_scope, identity, keystone_server_url, ssl_verify)
         except (requests.exceptions.HTTPError, requests.exceptions.Timeout, requests.exceptions.ConnectionError):
-            raise KeystoneUnreachable()
+            exception_msg = "Failed kaystone auth with identity:{id} scope:{scope} @{url}".format(
+                id=identity,
+                scope=auth_scope,
+                url=keystone_server_url)
+
+        if exception_msg:
+            try:
+                identity['password']['user']['domain']['name'] = identity['password']['user']['domain'].pop('id')
+
+                if 'domain' in auth_scope['project']:
+                    auth_scope['project']['domain']['name'] = auth_scope['project']['domain'].pop('id')
+                else:
+                    auth_scope['project']['name'] = auth_scope['project'].pop('id')
+                auth_resp = cls.request_auth_token(auth_scope, identity, keystone_server_url, ssl_verify)
+            except (requests.exceptions.HTTPError, requests.exceptions.Timeout, requests.exceptions.ConnectionError) as e:
+                exception_msg = "{msg} and also failed kaystone auth with identity:{id} scope:{scope} @{url}: {ex}".format(
+                    msg=exception_msg,
+                    id=identity,
+                    scope=auth_scope,
+                    url=keystone_server_url,
+                    ex=e)
+                raise KeystoneUnreachable(exception_msg)
 
         auth_token = auth_resp.headers.get('X-Subject-Token')
 
@@ -696,8 +718,9 @@ class OpenStackCheck(AgentCheck):
             try:
                 instance_scope = OpenStackProjectScope.from_config(self.init_config, instance)
                 self.service_check(self.IDENTITY_API_SC, AgentCheck.OK, tags=["server:%s" % self.init_config.get("keystone_server_url")])
-            except KeystoneUnreachable:
+            except KeystoneUnreachable as e:
                 self.warning("The agent could not contact the specified identity server at %s . Are you sure it is up at that address?" % self.init_config.get("keystone_server_url"))
+                self.log.debug("Problem grabbing auth token: %s", e)
                 self.service_check(self.IDENTITY_API_SC, AgentCheck.CRITICAL, tags=["server:%s" % self.init_config.get("keystone_server_url")])
 
                 # If Keystone is down/unreachable, we default the Nova and Neutron APIs to UNKNOWN since we cannot access the service catalog
