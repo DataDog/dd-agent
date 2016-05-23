c736841677d5e31e080f61d91ad6012ff986a7de
Ryan Handy <ryan.handy@datadoghq.com>
2016-05-22 18:41:47 -0700

    Remove trailing whitespace

--- checks.d/oracle.py
+++ checks.d/oracle.py
@@ -5,23 +5,23 @@ import cx_Oracle
 class Oracle(AgentCheck):
     SERVICE_CHECK_NAME = 'oracle.can_connect'
     SYS_METRICS = {
-        'Buffer Cache Hit Ratio':           'oracle.buffer_cachehit_ratio', 
-        'Cursor Cache Hit Ratio':           'oracle.cursor_cachehit_ratio', 
-        'Library Cache Hit Ratio':          'oracle.library_cachehit_ratio', 
-        'Shared Pool Free %':               'oracle.shared_pool_free', 
-        'Physical Reads Per Sec':           'oracle.physical_reads', 
-        'Physical Writes Per Sec':          'oracle.physical_writes', 
-        'Enqueue Timeouts Per Sec':         'oracle.enqueue_timeouts', 
-        'GC CR Block Received Per Second':  'oracle.gc_cr_receive_time', 
-        'Global Cache Blocks Corrupted':    'oracle.cache_blocks_corrupt', 
-        'Global Cache Blocks Lost':         'oracle.cache_blocks_lost', 
-        'Logons Per Sec':                   'oracle.logons', 
-        'Average Active Sessions':          'oracle.active_sessions', 
-        'Long Table Scans Per Sec':         'oracle.long_table_scans', 
-        'SQL Service Response Time':        'oracle.service_response_time', 
-        'User Rollbacks Per Sec':           'oracle.user_rollbacks', 
-        'Total Sorts Per User Call':        'oracle.sorts_per_user_call', 
-        'Rows Per Sort':                    'oracle.rows_per_sort', 
+        'Buffer Cache Hit Ratio':           'oracle.buffer_cachehit_ratio',
+        'Cursor Cache Hit Ratio':           'oracle.cursor_cachehit_ratio',
+        'Library Cache Hit Ratio':          'oracle.library_cachehit_ratio',
+        'Shared Pool Free %':               'oracle.shared_pool_free',
+        'Physical Reads Per Sec':           'oracle.physical_reads',
+        'Physical Writes Per Sec':          'oracle.physical_writes',
+        'Enqueue Timeouts Per Sec':         'oracle.enqueue_timeouts',
+        'GC CR Block Received Per Second':  'oracle.gc_cr_receive_time',
+        'Global Cache Blocks Corrupted':    'oracle.cache_blocks_corrupt',
+        'Global Cache Blocks Lost':         'oracle.cache_blocks_lost',
+        'Logons Per Sec':                   'oracle.logons',
+        'Average Active Sessions':          'oracle.active_sessions',
+        'Long Table Scans Per Sec':         'oracle.long_table_scans',
+        'SQL Service Response Time':        'oracle.service_response_time',
+        'User Rollbacks Per Sec':           'oracle.user_rollbacks',
+        'Total Sorts Per User Call':        'oracle.sorts_per_user_call',
+        'Rows Per Sort':                    'oracle.rows_per_sort',
         'Disk Sort Per Sec':                'oracle.disk_sorts',
         'Memory Sorts Ratio':               'oracle.memroy_sorts_ratio',
         'Database Wait Time Ratio':         'oracle.database_wait_time_ratio',
@@ -42,7 +42,7 @@ class Oracle(AgentCheck):
 
         self._get_sys_metrics(con)
 
-        self._get_tablespace_metrics(con)        
+        self._get_tablespace_metrics(con)
 
     def _get_config(self, instance):
         self.server = instance.get('server', None)
@@ -72,7 +72,7 @@ class Oracle(AgentCheck):
         query = "SELECT METRIC_NAME, VALUE, BEGIN_TIME FROM GV$SYSMETRIC " \
             "ORDER BY BEGIN_TIME"
         cur = con.cursor()
-        cur.execute(query)       
+        cur.execute(query)
         for row in cur:
             metric_name = row[0]
             metric_value = row[1]
