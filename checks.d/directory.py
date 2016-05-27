d9511abece4fb9214c887ed54df14c4a0983eae1
Cory Watson <cory@stripe.com>
2016-05-26 19:26:41 -0700

    Add subdirectory support.

--- checks.d/directory.py
+++ checks.d/directory.py
@@ -27,10 +27,14 @@ class DirectoryCheck(AgentCheck):
         "directory" - string, the directory to gather stats for. required
         "name" - string, the name to use when tagging the metrics. defaults to the "directory"
         "dirtagname" - string, the name of the tag used for the directory. defaults to "name"
+        "subdirtagname" - string, the name of the tag used for the subdirectory. defaults to "subdir"
         "filetagname" - string, the name of the tag used for each file. defaults to "filename"
         "filegauges" - boolean, when true stats will be an individual gauge per file (max. 20 files!) and not a histogram of the whole directory. default False
+        "subdirgauges" - boolean, when true a total stat will be emitted for each subdirectory. default False
+        "histograms" - boolean, when true histograms for filesizes and times will be generated. default True
         "pattern" - string, the `fnmatch` pattern to use when reading the "directory"'s files. default "*"
         "recursive" - boolean, when true the stats will recurse into directories. default False
+        "recursive_limit" - integer, sets a limit for how deeply to recurse. default 100
     """
 
     SOURCE_TYPE_NAME = 'system'
@@ -44,21 +48,28 @@ class DirectoryCheck(AgentCheck):
         name = instance.get("name", directory)
         pattern = instance.get("pattern", "*")
         recursive = _is_affirmative(instance.get("recursive", False))
+        recursive_limit = instance.get("recursive_limit", 100)
         dirtagname = instance.get("dirtagname", "name")
+        subdirtagname = instance.get("subdirtagname", "subdir")
         filetagname = instance.get("filetagname", "filename")
         filegauges = _is_affirmative(instance.get("filegauges", False))
+        subdirgauges = _is_affirmative(instance.get("subdirgauges", False))
+        histograms = _is_affirmative(instance.get("histograms", True))
         countonly = _is_affirmative(instance.get("countonly", False))
 
         if not exists(abs_directory):
             raise Exception("DirectoryCheck: the directory (%s) does not exist" % abs_directory)
 
-        self._get_stats(abs_directory, name, dirtagname, filetagname, filegauges, pattern, recursive, countonly)
+        self._get_stats(abs_directory, name, dirtagname, subdirtagname, filetagname, filegauges, subdirgauges, histograms, pattern, recursive, recursive_limit, countonly)
 
-    def _get_stats(self, directory, name, dirtagname, filetagname, filegauges, pattern, recursive, countonly):
+    def _get_stats(self, directory, name, dirtagname, subdirtagname, filetagname, filegauges, subdirgauges, histograms, pattern, recursive, recursive_limit, countonly):
         dirtags = [dirtagname + ":%s" % name]
         directory_bytes = 0
         directory_files = 0
+        recursive_count = 0
         for root, dirs, files in walk(directory):
+            subdir_bytes = 0
+            subdirtags = [subdirtagname + ":%s" % root] + dirtags
             for filename in files:
                 filename = join(root, filename)
                 # check if it passes our filter
@@ -78,22 +89,28 @@ class DirectoryCheck(AgentCheck):
                     self.warning("DirectoryCheck: could not stat file %s - %s" % (filename, ose))
                 else:
                     # file specific metrics
-                    directory_bytes += file_stat.st_size
+                    subdir_bytes += file_stat.st_size
+                    directory_bytes += subdir_bytes
                     if filegauges and directory_files <= 20:
                         filetags = list(dirtags)
                         filetags.append(filetagname + ":%s" % filename)
                         self.gauge("system.disk.directory.file.bytes", file_stat.st_size, tags=filetags)
                         self.gauge("system.disk.directory.file.modified_sec_ago", time.time() - file_stat.st_mtime, tags=filetags)
                         self.gauge("system.disk.directory.file.created_sec_ago", time.time() - file_stat.st_ctime, tags=filetags)
-                    elif not filegauges:
-                        self.histogram("system.disk.directory.file.bytes", file_stat.st_size, tags=dirtags)
+                    elif not filegauges and histograms:
+                        self.histogram("system.disk.directory.file.bytes", file_stat.st_size, tags=subdirtags)
                         self.histogram("system.disk.directory.file.modified_sec_ago", time.time() - file_stat.st_mtime, tags=dirtags)
                         self.histogram("system.disk.directory.file.created_sec_ago", time.time() - file_stat.st_ctime, tags=dirtags)
+
+            if recursive_count > 0 and subdirgauges:
+                # If we've descended in to a subdir then let's emit total for the subdir
+                self.gauge("system.disk.directory.bytes", subdir_bytes, tags=subdirtags)
 
             # os.walk gives us all sub-directories and their files
             # if we do not want to do this recursively and just want
             # the top level directory we gave it, then break
-            if not recursive:
+            recursive_count += 1
+            if not recursive and recursive_count <= recursive_limit:
                 break
 
         # number of files
