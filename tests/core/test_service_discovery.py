179004c4bd283f6afeff5f155e893b39efc1b738
Haissam Kaj <haissam.kaj@gmail.com>
2016-05-31 13:53:04 +0000

    Improve logging, tests, and interpolation of template variables

--- tests/core/test_service_discovery.py
+++ tests/core/test_service_discovery.py
@@ -31,7 +31,7 @@ class Response(object):
 
 def _get_container_inspect(c_id):
     """Return a mocked container inspect dict from self.container_inspects."""
-    for co, _, _ in TestServiceDiscovery.container_inspects:
+    for co, _, _, _ in TestServiceDiscovery.container_inspects:
         if co.get('Id') == c_id:
             return co
         return None
@@ -81,9 +81,9 @@ class TestServiceDiscovery(unittest.TestCase):
     }
     container_inspects = [
         # (inspect_dict, expected_ip, expected_port)
-        (docker_container_inspect, '172.17.0.21', ['80', '443']),
-        (kubernetes_container_inspect, None, ['6379']),  # arbitrarily defined in the mocked pod_list
-        (malformed_container_inspect, None, KeyError)
+        (docker_container_inspect, '172.17.0.21', 'port', '443'),
+        (kubernetes_container_inspect, None, 'port', '6379'),  # arbitrarily defined in the mocked pod_list
+        (malformed_container_inspect, None, 'port', KeyError)
     ]
 
     # templates with variables already extracted
@@ -152,7 +152,7 @@ class TestServiceDiscovery(unittest.TestCase):
 
     @mock.patch('utils.http.requests.get')
     @mock.patch('utils.kubeutil.check_yaml')
-    def test_get_host(self, mock_check_yaml, mock_get):
+    def test_get_host_address(self, mock_check_yaml, mock_get):
         kubernetes_config = {'instances': [{'kubelet_port': 1337}]}
         pod_list = {
             'items': [{
@@ -165,25 +165,64 @@ class TestServiceDiscovery(unittest.TestCase):
             }]
         }
 
+        # (inspect, tpl_var, expected_result)
+        ip_address_inspects = [
+            ({'NetworkSettings': {}}, 'host', None),
+            ({'NetworkSettings': {'IPAddress': ''}}, 'host', None),
+
+            ({'NetworkSettings': {'IPAddress': '127.0.0.1'}}, 'host', '127.0.0.1'),
+            ({'NetworkSettings': {'IPAddress': '127.0.0.1', 'Networks': {}}}, 'host', '127.0.0.1'),
+            ({'NetworkSettings': {
+                'IPAddress': '127.0.0.1',
+                'Networks': {'bridge': {'IPAddress': '127.0.0.1'}}}},
+             'host', '127.0.0.1'),
+            ({'NetworkSettings': {
+                'IPAddress': '',
+                'Networks': {'bridge': {'IPAddress': '127.0.0.1'}}}},
+             'host_bridge', '127.0.0.1'),
+            ({'NetworkSettings': {
+                'IPAddress': '127.0.0.1',
+                'Networks': {
+                    'bridge': {'IPAddress': '172.17.0.2'},
+                    'foo': {'IPAddress': '192.168.0.2'}}}},
+             'host', '127.0.0.1'),
+
+            ({'NetworkSettings': {'Networks': {}}}, 'host', None),
+            ({'NetworkSettings': {'Networks': {}}}, 'host_bridge', None),
+            ({'NetworkSettings': {'Networks': {'bridge': {}}}}, 'host', None),
+            ({'NetworkSettings': {'Networks': {'bridge': {}}}}, 'host_bridge', None),
+            ({'NetworkSettings': {
+                'Networks': {
+                    'bridge': {'IPAddress': '172.17.0.2'}
+                }}},
+             'host_bridge', '172.17.0.2'),
+            ({'NetworkSettings': {
+                'Networks': {
+                    'bridge': {'IPAddress': '172.17.0.2'},
+                    'foo': {'IPAddress': '192.168.0.2'}
+                }}},
+             'host_foo', '192.168.0.2')
+        ]
+
         mock_check_yaml.return_value = kubernetes_config
         mock_get.return_value = Response(pod_list)
 
-        for c_ins, expected_ip, _ in self.container_inspects:
+        for c_ins, tpl_var, expected_ip in ip_address_inspects:
             with mock.patch.object(AbstractConfigStore, '__init__', return_value=None):
                 with mock.patch('utils.dockerutil.DockerUtil.client', return_value=None):
                     with mock.patch('utils.kubeutil.get_conf_path', return_value=None):
                         sd_backend = get_sd_backend(agentConfig=self.auto_conf_agentConfig)
-                        self.assertEqual(sd_backend._get_host(c_ins), expected_ip)
+                        self.assertEquals(sd_backend._get_host_address(c_ins, tpl_var), expected_ip)
                         clear_singletons(self.auto_conf_agentConfig)
 
-    def test_get_ports(self):
+    def test_get_port(self):
         with mock.patch('utils.dockerutil.DockerUtil.client', return_value=None):
-            for c_ins, _, expected_ports in self.container_inspects:
+            for c_ins, _, var_tpl, expected_ports in self.container_inspects:
                 sd_backend = get_sd_backend(agentConfig=self.auto_conf_agentConfig)
-                if isinstance(expected_ports, list):
-                    self.assertEqual(sd_backend._get_ports(c_ins), expected_ports)
+                if isinstance(expected_ports, str):
+                    self.assertEquals(sd_backend._get_port(c_ins, var_tpl), expected_ports)
                 else:
-                    self.assertRaises(expected_ports, sd_backend._get_ports, c_ins)
+                    self.assertRaises(expected_ports, sd_backend._get_port, c_ins, var_tpl)
                 clear_singletons(self.auto_conf_agentConfig)
 
     @mock.patch('docker.Client.inspect_container', side_effect=_get_container_inspect)
@@ -191,8 +230,8 @@ class TestServiceDiscovery(unittest.TestCase):
     def test_get_check_configs(self, mock_inspect_container, mock_get_conf_tpls):
         """Test get_check_config with mocked container inspect and config template"""
         with mock.patch('utils.dockerutil.DockerUtil.client', return_value=None):
-            with mock.patch.object(SDDockerBackend, '_get_host', return_value='127.0.0.1'):
-                with mock.patch.object(SDDockerBackend, '_get_ports', return_value=['1337']):
+            with mock.patch.object(SDDockerBackend, '_get_host_address', return_value='127.0.0.1'):
+                with mock.patch.object(SDDockerBackend, '_get_port', return_value='1337'):
                     c_id = self.docker_container_inspect.get('Id')
                     for image in self.mock_templates.keys():
                         sd_backend = get_sd_backend(agentConfig=self.auto_conf_agentConfig)
@@ -239,29 +278,78 @@ class TestServiceDiscovery(unittest.TestCase):
         ]
 
         with mock.patch('utils.dockerutil.DockerUtil.client', return_value=None):
-            for agentConfig in self.agentConfigs:
-                sd_backend = get_sd_backend(agentConfig=agentConfig)
-                for tpl, res in valid_configs:
-                    init, instance, variables = tpl
-                    config = sd_backend._render_template(init, instance, variables)
-                    self.assertEquals(config, res)
-                for init, instance, variables in invalid_configs:
-                    config = sd_backend._render_template(init, instance, variables)
-                    self.assertEquals(config, None)
-                    clear_singletons(agentConfig)
+            with mock.patch.object(EtcdStore, 'get_client', return_value=None):
+                with mock.patch.object(ConsulStore, 'get_client', return_value=None):
+                    for agentConfig in self.agentConfigs:
+                        sd_backend = get_sd_backend(agentConfig=agentConfig)
+                        for tpl, res in valid_configs:
+                            init, instance, variables = tpl
+                            config = sd_backend._render_template(init, instance, variables)
+                            self.assertEquals(config, res)
+                        for init, instance, variables in invalid_configs:
+                            config = sd_backend._render_template(init, instance, variables)
+                            self.assertEquals(config, None)
+                            clear_singletons(agentConfig)
 
     def test_fill_tpl(self):
-        """Test _fill_tpl with mock _get_ports"""
+        """Test _fill_tpl with mocked docker client"""
+
         valid_configs = [
             # ((inspect, instance_tpl, variables, tags), (expected_instance_tpl, expected_var_values))
+            (({}, {'host': 'localhost'}, [], None), ({'host': 'localhost'}, {})),
             (
-                ({}, {'host': 'localhost'}, [], None),
+                ({'NetworkSettings': {'IPAddress': ''}}, {'host': 'localhost'}, [], None),
+                ({'host': 'localhost'}, {})
+            ),
+            (
+                ({'NetworkSettings': {'Networks': {}}}, {'host': 'localhost'}, [], None),
+                ({'host': 'localhost'}, {})
+            ),
+            (
+                ({'NetworkSettings': {'Networks': {'bridge': {}}}}, {'host': 'localhost'}, [], None),
                 ({'host': 'localhost'}, {})
             ),
             (
                 ({'NetworkSettings': {'IPAddress': '127.0.0.1'}},
                  {'host': '%%host%%', 'port': 1337}, ['host'], ['foo', 'bar:baz']),
-                ({'host': '%%host%%', 'port': 1337, 'tags': ['foo', 'bar:baz']}, {'host': '127.0.0.1'})
+                ({'host': '%%host%%', 'port': 1337, 'tags': ['foo', 'bar:baz']}, {'host': '127.0.0.1'}),
+            ),
+            (
+                ({'NetworkSettings': {'IPAddress': '127.0.0.1', 'Networks': {}}},
+                 {'host': '%%host%%', 'port': 1337}, ['host'], ['foo', 'bar:baz']),
+                ({'host': '%%host%%', 'port': 1337, 'tags': ['foo', 'bar:baz']}, {'host': '127.0.0.1'}),
+            ),
+            (
+                ({'NetworkSettings': {
+                    'IPAddress': '127.0.0.1',
+                    'Networks': {'bridge': {'IPAddress': '172.17.0.2'}}}
+                  },
+                 {'host': '%%host%%', 'port': 1337}, ['host'], ['foo', 'bar:baz']),
+                ({'host': '%%host%%', 'port': 1337, 'tags': ['foo', 'bar:baz']}, {'host': '127.0.0.1'}),
+            ),
+            (
+                ({'NetworkSettings': {
+                    'IPAddress': '',
+                    'Networks': {
+                        'bridge': {'IPAddress': '172.17.0.2'},
+                        'foo': {'IPAddress': '192.168.0.2'}
+                    }}
+                  },
+                 {'host': '%%host_bridge%%', 'port': 1337}, ['host_bridge'], ['foo', 'bar:baz']),
+                ({'host': '%%host_bridge%%', 'port': 1337, 'tags': ['foo', 'bar:baz']},
+                 {'host_bridge': '172.17.0.2'}),
+            ),
+            (
+                ({'NetworkSettings': {
+                    'IPAddress': '',
+                    'Networks': {
+                        'bridge': {'IPAddress': '172.17.0.2'},
+                        'foo': {'IPAddress': '192.168.0.2'}
+                    }}
+                  },
+                 {'host': '%%host_foo%%', 'port': 1337}, ['host_foo'], ['foo', 'bar:baz']),
+                ({'host': '%%host_foo%%', 'port': 1337, 'tags': ['foo', 'bar:baz']},
+                 {'host_foo': '192.168.0.2'}),
             ),
             (
                 ({'NetworkSettings': {'IPAddress': '127.0.0.1', 'Ports': {'42/tcp': None, '22/tcp': None}}},
@@ -271,25 +359,115 @@ class TestServiceDiscovery(unittest.TestCase):
                  {'host': '127.0.0.1', 'port_1': '42'})
             )
         ]
+
+        # should not fail but return something specific
+        edge_cases = [
+            # ((inspect, instance_tpl, variables, tags), (expected_instance_tpl, expected_var_values))
+
+            # specify bridge but there is also a default IPAddress (networks should be preferred)
+            (
+                ({'NetworkSettings': {
+                    'IPAddress': '127.0.0.1',
+                    'Networks': {'bridge': {'IPAddress': '172.17.0.2'}}}},
+                 {'host': '%%host_bridge%%', 'port': 1337}, ['host_bridge'], ['foo', 'bar:baz']),
+                ({'host': '%%host_bridge%%', 'port': 1337, 'tags': ['foo', 'bar:baz']},
+                 {'host_bridge': '172.17.0.2'})
+            ),
+            # specify index but there is a default IPAddress (there's a specifier, even if it's wrong, walking networks should be preferred)
+            (
+                ({'NetworkSettings': {
+                    'IPAddress': '127.0.0.1',
+                    'Networks': {'bridge': {'IPAddress': '172.17.0.2'}}}},
+                 {'host': '%%host_0%%', 'port': 1337}, ['host_0'], ['foo', 'bar:baz']),
+                ({'host': '%%host_0%%', 'port': 1337, 'tags': ['foo', 'bar:baz']}, {'host_0': '172.17.0.2'}),
+            ),
+            # missing key for host, bridge network should be preferred
+            (
+                ({'NetworkSettings': {'Networks': {
+                    'bridge': {'IPAddress': '127.0.0.1'},
+                    'foo': {'IPAddress': '172.17.0.2'}}}},
+                 {'host': '%%host_bar%%', 'port': 1337}, ['host_bar'], []),
+                ({'host': '%%host_bar%%', 'port': 1337}, {'host_bar': '127.0.0.1'}),
+            ),
+            # missing index for port
+            (
+                ({'NetworkSettings': {'IPAddress': '127.0.0.1', 'Ports': {'42/tcp': None, '22/tcp': None}}},
+                 {'host': '%%host%%', 'port': '%%port_2%%', 'tags': ['env:test']},
+                 ['host', 'port_2'], ['foo', 'bar:baz']),
+                ({'host': '%%host%%', 'port': '%%port_2%%', 'tags': ['env:test', 'foo', 'bar:baz']},
+                 {'host': '127.0.0.1', 'port_2': '42'})
+            )
+        ]
+
+        # should raise
+        invalid_config = [
+            # ((inspect, instance_tpl, variables, tags), expected_exception)
+
+            # template variable but no IPAddress available
+            (
+                ({'NetworkSettings': {'Networks': {}}},
+                 {'host': '%%host%%', 'port': 1337}, ['host'], ['foo', 'bar:baz']),
+                Exception,
+            ),
+            # index but no IPAddress available
+            (
+                ({'NetworkSettings': {'Networks': {}}},
+                 {'host': '%%host_0%%', 'port': 1337}, ['host_0'], ['foo', 'bar:baz']),
+                Exception,
+            ),
+            # key but no IPAddress available
+            (
+                ({'NetworkSettings': {'Networks': {}}},
+                 {'host': '%%host_foo%%', 'port': 1337}, ['host_foo'], ['foo', 'bar:baz']),
+                Exception,
+            ),
+
+            # template variable but no port available
+            (
+                ({'NetworkSettings': {'Networks': {}}},
+                 {'host': 'localhost', 'port': '%%port%%'}, ['port'], []),
+                Exception,
+            ),
+            # index but no port available
+            (
+                ({'NetworkSettings': {'Networks': {}}},
+                 {'host': 'localhost', 'port_0': '%%port%%'}, ['port_0'], []),
+                Exception,
+            ),
+            # key but no port available
+            (
+                ({'NetworkSettings': {'Networks': {}}},
+                 {'host': 'localhost', 'port': '%%port_foo%%'}, ['port_foo'], []),
+                Exception,
+            )
+        ]
+
         with mock.patch('utils.dockerutil.DockerUtil.client', return_value=None):
-            for ac in self.agentConfigs:
-                sd_backend = get_sd_backend(agentConfig=ac)
-                try:
-                    for co in valid_configs:
-                        inspect, tpl, variables, tags = co[0]
-                        instance_tpl, var_values = sd_backend._fill_tpl(inspect, tpl, variables, tags)
-                        for key in instance_tpl.keys():
-                            if isinstance(instance_tpl[key], list):
-                                self.assertEquals(len(instance_tpl[key]), len(co[1][0].get(key)))
-                                for elem in instance_tpl[key]:
-                                    self.assertTrue(elem in co[1][0].get(key))
-                            else:
-                                self.assertEquals(instance_tpl[key], co[1][0].get(key))
-                        self.assertEquals(var_values, co[1][1])
-                    clear_singletons(ac)
-                except Exception:
-                    clear_singletons(ac)
-                    raise
+            with mock.patch.object(EtcdStore, 'get_client', return_value=None):
+                with mock.patch.object(ConsulStore, 'get_client', return_value=None):
+                    for ac in self.agentConfigs:
+                        sd_backend = get_sd_backend(agentConfig=ac)
+                        try:
+                            for co in valid_configs + edge_cases:
+                                inspect, tpl, variables, tags = co[0]
+                                instance_tpl, var_values = sd_backend._fill_tpl(inspect, tpl, variables, tags)
+                                for key in instance_tpl.keys():
+                                    if isinstance(instance_tpl[key], list):
+                                        self.assertEquals(len(instance_tpl[key]), len(co[1][0].get(key)))
+                                        for elem in instance_tpl[key]:
+                                            self.assertTrue(elem in co[1][0].get(key))
+                                    else:
+                                        self.assertEquals(instance_tpl[key], co[1][0].get(key))
+                                self.assertEquals(var_values, co[1][1])
+
+                            for co in invalid_config:
+                                inspect, tpl, variables, tags = co[0]
+                                self.assertRaises(co[1], sd_backend._fill_tpl(inspect, tpl, variables, tags))
+
+                            clear_singletons(ac)
+                        except Exception:
+                            clear_singletons(ac)
+                            raise
 
     # config_stores tests
 
@@ -297,7 +475,9 @@ class TestServiceDiscovery(unittest.TestCase):
         """Test _get_auto_config"""
         expected_tpl = {
             'redis': ('redisdb', None, {"host": "%%host%%", "port": "%%port%%"}),
-            'consul': ('consul', None, {"url": "http://%%host%%:%%port%%", "catalog_checks": True, "new_leader_checks": True}),
+            'consul': ('consul', None, {
+                "url": "http://%%host%%:%%port%%", "catalog_checks": True, "new_leader_checks": True
+            }),
             'foobar': None
         }
 
