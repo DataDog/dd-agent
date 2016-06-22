eafec6b94f849ba9fc584e71a969fb047ef818a0
Haissam Kaj <haissam.kaj@gmail.com>
2016-06-22 15:53:59 +0000

    Revert "Seek DATADOG_ID in kubernetes annotations"
    This reverts commit fa30d9f314492dbc1bb7962844aa010543d4b08d.

--- utils/service_discovery/sd_docker_backend.py
+++ utils/service_discovery/sd_docker_backend.py
@@ -12,6 +12,7 @@ from utils.kubeutil import KubeUtil, is_k8s
 from utils.service_discovery.abstract_sd_backend import AbstractSDBackend
 from utils.service_discovery.config_stores import get_config_store, TRACE_CONFIG
 
+DATADOG_ID = 'com.datadoghq.sd.check.id'
 log = logging.getLogger(__name__)
 
 
@@ -144,7 +145,9 @@ class SDDockerBackend(AbstractSDBackend):
 
         for image, cid, labels in containers:
             try:
-                check_configs = self._get_check_configs(cid, image, trace_config=trace_config) or []
+                # value of the DATADOG_ID tag or the image name if the label is missing
+                identifier = self.get_config_id(image, labels)
+                check_configs = self._get_check_configs(cid, identifier, trace_config=trace_config) or []
                 for conf in check_configs:
                     if trace_config and conf is not None:
                         source, conf = conf
@@ -172,7 +175,11 @@ class SDDockerBackend(AbstractSDBackend):
                               ' discovery failed, leaving it alone.' % (cid[:12], image))
         return configs
 
-    def _get_check_configs(self, c_id, image, trace_config=False):
+    def get_config_id(self, image, labels):
+        """Look for a DATADOG_ID label, return its value or the image name if missing"""
+        return labels.get(DATADOG_ID) or image
+
+    def _get_check_configs(self, c_id, identifier, trace_config=False):
         """Retrieve configuration templates and fill them with data pulled from docker and tags."""
         inspect = self.docker_client.inspect_container(c_id)
         config_templates = self._get_config_templates(image, trace_config=trace_config)
