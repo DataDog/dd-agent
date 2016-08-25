0f13bd52007458f2197a012762626efaeae0d3d9
Brad Heller <brad.heller@gmail.com>
2016-08-25 14:51:11 -0700

    Clean up some linty stuff.

--- checks.d/marathon.py
+++ checks.d/marathon.py
@@ -16,70 +16,70 @@ from checks import AgentCheck
 
 class Marathon(AgentCheck):
 
-	DEFAULT_TIMEOUT = 5
-	SERVICE_CHECK_NAME = 'marathon.can_connect'
+    DEFAULT_TIMEOUT = 5
+    SERVICE_CHECK_NAME = 'marathon.can_connect'
 
-	APP_METRICS = [
-		'backoffFactor',
-		'backoffSeconds',
-		'cpus',
-		'disk',
-		'instances',
-		'mem',
-		'taskRateLimit',
-		'tasksRunning',
-		'tasksStaged'
-	]
+    APP_METRICS = [
+        'backoffFactor',
+        'backoffSeconds',
+        'cpus',
+        'disk',
+        'instances',
+        'mem',
+        'taskRateLimit',
+        'tasksRunning',
+        'tasksStaged'
+    ]
 
-	def check(self, instance):
-		if 'url' not in instance:
-			raise Exception('Marathon instance missing "url" value.')
+    def check(self, instance):
+        if 'url' not in instance:
+            raise Exception('Marathon instance missing "url" value.')
 
-		# Load values from the instance config
-		url = instance['url']
-		user = instance.get('user')
-		password = instance.get('password')
-		if user is not None and password is not None:
-			auth = (user,password)
-		else:
-			auth = None
-		instance_tags = instance.get('tags', [])
-		default_timeout = self.init_config.get('default_timeout', self.DEFAULT_TIMEOUT)
-		timeout = float(instance.get('timeout', default_timeout))
+        # Load values from the instance config
+        url = instance['url']
+        user = instance.get('user')
+        password = instance.get('password')
+        if user is not None and password is not None:
+            auth = (user,password)
+        else:
+            auth = None
+        instance_tags = instance.get('tags', [])
+        default_timeout = self.init_config.get('default_timeout', self.DEFAULT_TIMEOUT)
+        timeout = float(instance.get('timeout', default_timeout))
 
-		response = self.get_json(urljoin(url, "v2/apps"), timeout, auth)
-		if response is not None:
-			self.gauge('marathon.apps', len(response['apps']), tags=instance_tags)
-			for app in response['apps']:
-				tags = ['app_id:' + app['id'], 'version:' + app['version']] + instance_tags
-				for attr in self.APP_METRICS:
-					if attr in app:
-						self.gauge('marathon.' + attr, app[attr], tags=tags)
+        response = self.get_json(urljoin(url, "v2/apps"), timeout, auth)
+        if response is not None:
+            self.gauge('marathon.apps', len(response['apps']), tags=instance_tags)
+            for app in response['apps']:
+                tags = ['app_id:' + app['id'], 'version:' + app['version']] + instance_tags
+                for attr in self.APP_METRICS:
+                    if attr in app:
+                        self.gauge('marathon.' + attr, app[attr], tags=tags)
 
-		response = self.get_json(urljoin(url, "v2/deployments"), timeout, auth)
-		if response is not None:
-			self.gauge('marathon.deployments', len(response), tags=instance_tags)
+        response = self.get_json(urljoin(url, "v2/deployments"), timeout, auth)
+        if response is not None:
+            self.gauge('marathon.deployments', len(response), tags=instance_tags)
 
-	def get_json(self, url, timeout, auth):
-		try:
-			r = requests.get(url, timeout=timeout, auth=auth)
-			r.raise_for_status()
-		except requests.exceptions.Timeout:
-			# If there's a timeout
-			self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.CRITICAL,
-				message='%s timed out after %s seconds.' % (url, timeout),
-				tags = ["url:{0}".format(url)])
-			raise Exception("Timeout when hitting %s" % url)
+    def get_json(self, url, timeout, auth):
+        try:
+            r = requests.get(url, timeout=timeout, auth=auth)
+            r.raise_for_status()
+        except requests.exceptions.Timeout:
+            # If there's a timeout
+            self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.CRITICAL,
+                message='%s timed out after %s seconds.' % (url, timeout),
+                tags = ["url:{0}".format(url)])
+            raise Exception("Timeout when hitting %s" % url)
 
-		except requests.exceptions.HTTPError:
-			self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.CRITICAL,
-				message='%s returned a status of %s' % (url, r.status_code),
-				tags = ["url:{0}".format(url)])
-			raise Exception("Got %s when hitting %s" % (r.status_code, url))
+        except requests.exceptions.HTTPError:
+            self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.CRITICAL,
+                message='%s returned a status of %s' % (url, r.status_code),
+                tags = ["url:{0}".format(url)])
+            raise Exception("Got %s when hitting %s" % (r.status_code, url))
 
-		else:
-			self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.OK,
-				tags = ["url:{0}".format(url)]
-			)
+        else:
+            self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.OK,
+                tags = ["url:{0}".format(url)]
+            )
 
-		return r.json()
+        return r.json()
