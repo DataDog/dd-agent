6522107c74e67bf7f6e77d3569144cc8deeb7f36
Quentin Madec <quentin.madec@datadoghq.com>
2016-09-06 11:49:57 -0400

    [ci] add a way to ack a broken flare endpoint

--- tests/core/test_flare.py
+++ tests/core/test_flare.py
@@ -1,5 +1,5 @@
 # stdlib
-import os.path
+import os
 import unittest
 import re
 
@@ -173,6 +173,8 @@ class FlareTest(unittest.TestCase):
     @mock.patch('tempfile.gettempdir', side_effect=get_mocked_temp)
     @mock.patch('utils.flare.get_config', side_effect=get_mocked_config)
     def test_endpoint(self, mock_config, mock_temp, mock_stfrtime):
+        if os.environ['FLARE_BROKEN']:
+            raise unittest.case.SkipTest('Flare broken, acknowledged')
         f = Flare()
         f._ask_for_email = lambda: None
         f._open_tarfile()
