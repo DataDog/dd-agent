75a461a27345a94189575ae6ba29ad6a8a6ac673
Joe Williams <joe.williams@github.com>
2016-09-16 10:11:55 -0700

    no need for this really

--- checks.d/powerdns_authoritative.py
+++ checks.d/powerdns_authoritative.py
@@ -7,65 +7,8 @@ from checks import AgentCheck
 # 3p
 import requests
 
-
 class PowerDNSAuthoritativeCheck(AgentCheck):
     # See https://doc.powerdns.com/md/authoritative/stats/ for metrics explanation
-    GAUGE_METRICS = [
-        'corrupt-packets',
-        'deferred-cache-inserts',
-        'deferred-cache-lookup',
-        'dnsupdate-answers',
-        'dnsupdate-changes',
-        'dnsupdate-queries',
-        'dnsupdate-refused',
-        'incoming-notifications',
-        'packetcache-hit',
-        'packetcache-miss',
-        'packetcache-size',
-        'query-cache-hit',
-        'query-cache-miss',
-        'rd-queries',
-        'recursing-answers',
-        'recursing-questions',
-        'recursion-unanswered',
-        'security-status',
-        'servfail-packets',
-        'signatures',
-        'tcp-answers',
-        'tcp-answers-bytes',
-        'tcp-queries',
-        'tcp4-answers',
-        'tcp4-answers-bytes',
-        'tcp4-queries',
-        'tcp6-answers',
-        'tcp6-answers-bytes',
-        'tcp6-queries',
-        'timedout-packets',
-        'udp-answers',
-        'udp-answers-bytes',
-        'udp-do-queries',
-        'udp-queries',
-        'udp4-answers',
-        'udp4-answers-bytes',
-        'udp4-queries',
-        'udp6-answers',
-        'udp6-answers-bytes',
-        'udp6-queries',
-        'fd-usage',
-        'key-cache-size',
-        'latency',
-        'meta-cache-size',
-        'qsize-q',
-        'real-memory-usage',
-        'signature-cache-size',
-        'sys-msec',
-        'udp-in-errors',
-        'udp-noport-errors',
-        'udp-recvbuf-errors',
-        'udp-sndbuf-errors',
-        'uptime',
-        'user-msec'
-    ]
 
     SERVICE_CHECK_NAME = 'powerdns.authoritative.can_connect'
 
@@ -74,11 +17,7 @@ class PowerDNSAuthoritativeCheck(AgentCheck):
         stats = self._get_pdns_stats(config)
         for stat in stats:
             self.log.debug('powerdns.authoritative.{}:{}'.format(stat['name'], stat['value']))
-
-            if stat['name'] in PowerDNSAuthoritativeCheck.GAUGE_METRICS:
-                self.gauge('powerdns.authoritative.{}'.format(stat['name']), float(stat['value']), tags=tags)
-            elif stat['name'] in PowerDNSAuthoritativeCheck.RATE_METRICS:
-                self.rate('powerdns.authoritative.{}'.format(stat['name']), float(stat['value']), tags=tags)
+            self.gauge('powerdns.authoritative.{}'.format(stat['name']), float(stat['value']), tags=tags)
 
     def _get_config(self, instance):
         required = ['host', 'port', 'api_key']
