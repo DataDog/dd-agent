c1d60e9a30572481967c66c8bccd534898ce9137
Joe Williams <joe.williams@github.com>
2016-09-16 15:31:26 -0700

    metric types

--- checks.d/unbound.py
+++ checks.d/unbound.py
@@ -35,7 +35,11 @@ class UnboundCheck(AgentCheck):
             if 'histogram' not in stat[0]: # dont send histogram metrics
                 self.log.debug('unbound.{}:{}'.format(stat[0], stat[1]))
 
-                if 'unbound.unwanted.' or '.num.' in stat[0]:
+                if '.time.' in stat[0]:
+                    self.gauge('unbound.{}'.format(stat[0]), float(stat[1]), tags=tags)
+                elif 'unbound.unwanted.' in stat[0]:
+                    self.rate('unbound.{}'.format(stat[0]), float(stat[1]), tags=tags)
+                elif '.num.' in stat[0]:
                     self.rate('unbound.{}'.format(stat[0]), float(stat[1]), tags=tags)
                 else:
                     self.gauge('unbound.{}'.format(stat[0]), float(stat[1]), tags=tags)
