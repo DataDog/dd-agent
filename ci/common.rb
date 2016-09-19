6e46abab7f3a0aedec645c508acf9079d81c4f3c
Joe Williams <joe.williams@github.com>
2016-09-19 09:28:54 -0700

    Revert "fix metric names"
    This reverts commit 488ec7dceab2d4a23b4d3005f9c4c019f7a51d54.

--- ci/common.rb
+++ ci/common.rb
@@ -24,7 +24,7 @@ def section(name)
 end
 
 def install_requirements(req_file, pip_options = nil, output = nil, use_venv = nil)
-  pip_command = 'venv/bin/pip'
+  pip_command = use_venv ? 'venv/bin/pip' : 'pip'
   redirect_output = output ? "2>&1 >> #{output}" : ''
   pip_options = '' if pip_options.nil?
   File.open(req_file, 'r') do |f|
@@ -137,15 +137,15 @@ namespace :ci do
 
     task :install do |t|
       section('INSTALL')
-      sh %(venv/bin/pip install --upgrade pip setuptools)
-      sh %(venv/bin/pip install\
+      sh %(#{'python -m ' if Gem.win_platform?}pip install --upgrade pip setuptools)
+      sh %(pip install\
            -r requirements.txt\
            --cache-dir #{ENV['PIP_CACHE']}\
            2>&1 >> #{ENV['VOLATILE_DIR']}/ci.log)
       install_requirements('requirements-opt.txt',
                            "--cache-dir #{ENV['PIP_CACHE']}",
-                           "#{ENV['VOLATILE_DIR']}/ci.log", true)
-      sh %(venv/bin/pip install\
+                           "#{ENV['VOLATILE_DIR']}/ci.log")
+      sh %(pip install\
            --upgrade\
            -r requirements-test.txt\
            --cache-dir #{ENV['PIP_CACHE']}\
@@ -202,7 +202,7 @@ namespace :ci do
         # separate dir we symlink stuff in the rootdir
         path = %(PATH="#{ENV['INTEGRATIONS_DIR']}/bin:#{ENV['PATH']}" )
       end
-      sh %(venv/bin/nosetests -s -v -A "#{nose}" #{tests_directory})
+      sh %(#{path}nosetests -s -v -A "#{nose}" #{tests_directory})
       t.reenable
     end
 
