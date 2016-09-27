52e7eb6032edca337bbb102c6e555c44443b688d
Quentin Madec <degemer@users.noreply.github.com>
2016-09-27 15:44:05 -0400

    Revert "[packaging] revert to old hostname sed" (#2765)

--- packaging/datadog-agent/source/install_agent.sh
+++ packaging/datadog-agent/source/install_agent.sh
@@ -174,7 +174,7 @@ else
     $sudo_cmd sh -c "sed 's/api_key:.*/api_key: $apikey/' /etc/dd-agent/datadog.conf.example > /etc/dd-agent/datadog.conf"
     if [ $dd_hostname ]; then
         printf "\033[34m\n* Adding your HOSTNAME to the Agent configuration: /etc/dd-agent/datadog.conf\n\033[0m\n"
-        $sudo_cmd sh -c "sed -i 's/#hostname:.*/hostname: $dd_hostname/' /etc/dd-agent/datadog.conf"
+        $sudo_cmd sh -c "sed -i 's/# hostname:.*/hostname: $dd_hostname/' /etc/dd-agent/datadog.conf"
     fi
     $sudo_cmd chown dd-agent:root /etc/dd-agent/datadog.conf
     $sudo_cmd chmod 640 /etc/dd-agent/datadog.conf
