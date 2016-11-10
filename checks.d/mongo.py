de21b0351b3d9996ff0998c938951ab07c50c127
Evan Broder <evan@stripe.com>
2016-11-10 11:28:45 -0800

    [mongo] Use db.current_op instead of manually querying
    MongoDB 3.2 introduced a new find command, which standard query
    operations in pymongo will use if it's available. That command doesn't
    support the legacy `$cmd.sys.inprog` pseudo-collection, which is
    replaced by a `currentOp` command.
    
    Fortunately there's a built-in API to handle this. Use it.

--- checks.d/mongo.py
+++ checks.d/mongo.py
@@ -430,12 +430,6 @@ class MongoDb(AgentCheck):
         else:
             return 'UNKNOWN'
 
-    def get_version(self, client):
-        """
-        Return MongoDB version.
-        """
-        return tuple(map(int, client.server_info()['version'].split('.')))
-
     def _report_replica_set_state(self, state, clean_server_name, replset_name, agentConfig):
         """
         Report the member's replica set state
@@ -737,11 +731,7 @@ class MongoDb(AgentCheck):
         if status['ok'] == 0:
             raise Exception(status['errmsg'].__str__())
 
-        if self.get_version(cli) >= (3, 2, 0):
-            ops = admindb.command('currentOp', True)
-        else:
-            ops = db['$cmd.sys.inprog'].find_one()
-
+        ops = db.current_op()
         status['fsyncLocked'] = 1 if ops.get('fsyncLock') else 0
 
         status['stats'] = db.command('dbstats')
