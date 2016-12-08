38b9d19b75368054fee7bfd66c5f47b9c0ec33a9
James Lamb <admin@oranged.to>
2016-12-08 15:53:05 +1100

    [powerdns_recursor] updated flake8 test for indentation

--- tests/checks/integration/test_powerdns_recursor.py
+++ tests/checks/integration/test_powerdns_recursor.py
@@ -150,8 +150,7 @@ class TestPowerDNSRecursorCheck(AgentCheckTest):
             "host": "127.0.0.1",
             "port": "8082",
             "api_key": "pdns_api_key"
-        },
-        {
+        },{
             "host": "127.0.0.1",
             "port": "8082",
             "api_key": "pdns_api_key",
