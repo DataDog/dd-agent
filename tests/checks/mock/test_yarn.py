6ed94f34b30a5d8e770bb413dc4b7299ac32f0e4
Alan Bover <alan.bover@schibsted.com>
2016-12-15 19:34:10 +0100

    [yarn] Add metrics for capacityScheduler

--- tests/checks/mock/test_yarn.py
+++ tests/checks/mock/test_yarn.py
@@ -17,6 +17,7 @@ RM_ADDRESS = 'http://localhost:8088'
 YARN_CLUSTER_METRICS_URL = urljoin(RM_ADDRESS, '/ws/v1/cluster/metrics')
 YARN_APPS_URL = urljoin(RM_ADDRESS, '/ws/v1/cluster/apps') + '?states=RUNNING'
 YARN_NODES_URL = urljoin(RM_ADDRESS, '/ws/v1/cluster/nodes')
+YARN_SCHEDULER_URL = urljoin(RM_ADDRESS, '/ws/v1/cluster/scheduler')
 
 
 def requests_get_mock(*args, **kwargs):
@@ -44,6 +45,11 @@ def requests_get_mock(*args, **kwargs):
 
     elif args[0] == YARN_NODES_URL:
         with open(Fixtures.file('nodes_metrics'), 'r') as f:
+            body = f.read()
+            return MockResponse(body, 200)
+
+    elif args[0] == YARN_SCHEDULER_URL:
+        with open(Fixtures.file('scheduler_metrics'), 'r') as f:
             body = f.read()
             return MockResponse(body, 200)
 
@@ -128,6 +134,50 @@ class YARNCheck(AgentCheckTest):
         'opt_key:opt_value'
     ]
 
+    YARN_ROOT_QUEUE_METRICS_VALUES = {
+        'yarn.queue.root.max_capacity': 100,
+        'yarn.queue.root.used_capacity': 35.012,
+        'yarn.queue.root.capacity': 100
+    }
+
+    YARN_ROOT_QUEUE_METRICS_TAGS = [
+        'cluster_name:%s' % CLUSTER_NAME,
+        'queue_name:root',
+        'opt_key:opt_value'
+    ]
+
+    YARN_QUEUE_METRICS_VALUES = {
+        'yarn.queue.num_pending_applications': 0,
+        'yarn.queue.user_am_resource_limit.memory': 2587968,
+        'yarn.queue.user_am_resource_limit.vcores': 688,
+        'yarn.queue.absolute_capacity': 52.12,
+        'yarn.queue.user_limit_factor': 1,
+        'yarn.queue.user_limit': 100,
+        'yarn.queue.num_applications': 3,
+        'yarn.queue.used_am_resource.memory': 2688,
+        'yarn.queue.used_am_resource.vcores': 3,
+        'yarn.queue.absolute_used_capacity': 31.868685,
+        'yarn.queue.resources_used.memory': 3164800,
+        'yarn.queue.resources_used.vcores': 579,
+        'yarn.queue.am_resource_limit.vcores': 688,
+        'yarn.queue.am_resource_limit.memory': 2587968,
+        'yarn.queue.capacity': 52.12,
+        'yarn.queue.num_active_applications': 3,
+        'yarn.queue.absolute_max_capacity': 52.12,
+        'yarn.queue.used_capacity': 61.14484,
+        'yarn.queue.num_containers': 75,
+        'yarn.queue.max_capacity': 52.12,
+        'yarn.queue.max_applications': 5212,
+        'yarn.queue.max_applications_per_user': 5212
+    }
+
+    YARN_QUEUE_METRICS_TAGS = [
+        'cluster_name:%s' % CLUSTER_NAME,
+        'queue_name:clientqueue',
+        'opt_key:opt_value'
+    ]
+
+
     @mock.patch('requests.get', side_effect=requests_get_mock)
     def test_check(self, mock_requests):
         config = {
@@ -153,3 +203,15 @@ class YARNCheck(AgentCheckTest):
             self.assertMetric(metric,
                 value=value,
                 tags=self.YARN_NODE_METRICS_TAGS)
+
+        # Check the YARN Root Queue Metrics
+        for metric, value in self.YARN_ROOT_QUEUE_METRICS_VALUES.iteritems():
+            self.assertMetric(metric,
+                value=value,
+                tags=self.YARN_ROOT_QUEUE_METRICS_TAGS)
+
+        # Check the YARN Custom Queue Metrics
+        for metric, value in self.YARN_QUEUE_METRICS_VALUES.iteritems():
+            self.assertMetric(metric,
+                value=value,
+                tags=self.YARN_QUEUE_METRICS_TAGS)
