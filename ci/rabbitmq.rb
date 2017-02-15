a24969365db0abb1dea5abf5da5b76a8b6aedcb2
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-02-15 15:07:19 -0500

    [rabbitmq][test] I think we've got multiple hits for find.

--- ci/rabbitmq.rb
+++ ci/rabbitmq.rb
@@ -36,10 +36,10 @@ namespace :ci do
       sh %(#{rabbitmq_rootdir}/sbin/rabbitmq-plugins enable rabbitmq_management)
       sh %(#{rabbitmq_rootdir}/sbin/rabbitmq-plugins enable rabbitmq_management)
       %w(test1 test5 tralala).each do |q|
-        sh %(python `find #{rabbitmq_rootdir} -name rabbitmqadmin` declare queue name=#{q})
-        sh %(python `find #{rabbitmq_rootdir} -name rabbitmqadmin` publish exchange=amq.default routing_key=#{q} payload="hello, world")
+        sh %(python `find #{rabbitmq_rootdir} -name rabbitmqadmin | head -n 1` declare queue name=#{q})
+        sh %(python `find #{rabbitmq_rootdir} -name rabbitmqadmin | head -n 1` publish exchange=amq.default routing_key=#{q} payload="hello, world")
       end
-      sh %(python `find #{rabbitmq_rootdir} -name rabbitmqadmin` list queues)
+      sh %(python `find #{rabbitmq_rootdir} -name rabbitmqadmin | head -n 1` list queues)
     end
 
     task script: ['ci:common:script'] do
