7383547fdb09106cb1fc29f662874d0c913e0259
Quentin Madec <quentin.madec@datadoghq.com>
2017-02-27 13:26:05 -0500

    [mac] disable gohai
    As it is misbehaving (eating way more CPU than needed/expected).

--- checks/collector.py
+++ checks/collector.py
@@ -779,6 +779,9 @@ class Collector(object):
         return self._run_gohai(['--only', 'processes'])
 
     def _run_gohai(self, options):
+        # Gohai is disabled on Mac for now
+        if Platform.is_mac():
+            return None
         output = None
         try:
             output, err, _ = get_subprocess_output(["gohai"] + options, log)
