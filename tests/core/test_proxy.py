69c9ba4630547a29d10a3633d876119e091c59ba
Hippolyte HENRY <hippolyte.henry@dadadoghq.com>
2017-03-01 16:28:40 -0500

    test starting squid container

--- tests/core/test_proxy.py
+++ tests/core/test_proxy.py
@@ -1,12 +1,18 @@
 # stdlib
 from unittest import TestCase
+import logging
 
 # 3p
 from requests.utils import get_environ_proxies
 
 # project
 from utils.proxy import set_no_proxy_settings
+from utils.dockerutil import DockerUtil
 
+log = logging.getLogger('tests')
+
+CONTAINER_TO_RUN = "datadog/squid"
+CONTAINER_NAME = "test-squid"
 
 class TestProxy(TestCase):
     def test_no_proxy(self):
@@ -44,3 +50,21 @@ class TestProxy(TestCase):
         env.pop("https_proxy", None)
         env.pop("HTTP_PROXY", None)
         env.pop("HTTPS_PROXY", None)
+
+    def test_proxy(self):
+        self.assertEquals(2, 1)
+
+    def setUp(self):
+        self.docker_client = DockerUtil().client
+
+        for line in self.docker_client.pull(CONTAINER_TO_RUN, stream=True):
+            log.info(line)
+
+        self.container = self.docker_client.create_container(CONTAINER_TO_RUN, detach=True, name=CONTAINER_NAME)
+
+        log.info("Starting container: {0}".format(CONTAINER_TO_RUN))
+        self.docker_client.start(CONTAINER_NAME)
+
+    def tearDown(self):
+        log.info("Stopping container: {0}".format(CONTAINER_TO_RUN))
+        self.docker_client.remove_container('CONTAINER_NAME', force=True)
