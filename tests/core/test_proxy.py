efa3fbfc39c3ecee245c0cc58addc84a6a8626e2
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2017-03-02 14:40:35 -0500

    test services : docker

--- tests/core/test_proxy.py
+++ tests/core/test_proxy.py
@@ -15,7 +15,7 @@ log = logging.getLogger('tests')
 CONTAINER_TO_RUN = "datadog/squid"
 CONTAINER_NAME = "test-squid"
 
-class TestProxy(TestCase):
+class TestNoProxy(TestCase):
     def test_no_proxy(self):
         """
         Starting with Agent 5.0.0, there should always be a local forwarder
@@ -52,21 +52,22 @@ class TestProxy(TestCase):
         env.pop("HTTP_PROXY", None)
         env.pop("HTTPS_PROXY", None)
 
-    # @attr(requires='core_integration')
-    # def test_proxy(self):
-    #     self.assertEquals(2, 1)
+# class TestProxy(TestCase):
+#     @attr(requires='core_integration')
+#     def test_proxy(self):
+#         self.assertEquals(2, 1)
 
-    # def setUp(self):
-    #     self.docker_client = DockerUtil().client
+#     def setUp(self):
+#         self.docker_client = DockerUtil().client
 
-    #     for line in self.docker_client.pull(CONTAINER_TO_RUN, stream=True):
-    #         log.info(line)
+#         for line in self.docker_client.pull(CONTAINER_TO_RUN, stream=True):
+#             log.info(line)
 
-    #     self.container = self.docker_client.create_container(CONTAINER_TO_RUN, detach=True, name=CONTAINER_NAME)
+#         self.container = self.docker_client.create_container(CONTAINER_TO_RUN, detach=True, name=CONTAINER_NAME)
 
-    #     log.info("Starting container: {0}".format(CONTAINER_TO_RUN))
-    #     self.docker_client.start(CONTAINER_NAME)
+#         log.info("Starting container: {0}".format(CONTAINER_TO_RUN))
+#         self.docker_client.start(CONTAINER_NAME)
 
-    # def tearDown(self):
-    #     log.info("Stopping container: {0}".format(CONTAINER_TO_RUN))
-    #     self.docker_client.remove_container(CONTAINER_NAME, force=True)
+#     def tearDown(self):
+#         log.info("Stopping container: {0}".format(CONTAINER_TO_RUN))
+#         self.docker_client.remove_container(CONTAINER_NAME, force=True)
