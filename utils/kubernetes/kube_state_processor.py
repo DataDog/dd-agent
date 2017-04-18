582be88f5efd94ebdc3df8d18faebbe8e2cf024d
Haissam Kaj <haissam.kaj@gmail.com>
2017-04-18 22:42:13 +0200

    Add more kube-state-metrics

--- utils/kubernetes/kube_state_processor.py
+++ utils/kubernetes/kube_state_processor.py
@@ -28,21 +28,43 @@ class KubeStateProcessor:
         # these metrics will be extracted with all their labels and reported as-is with their corresponding metric name
         self.metric_to_gauge = {
             # message.metric: datadog metric name
+            # nodes
             'kube_node_status_capacity_cpu_cores': NAMESPACE + '.node.cpu_capacity',
             'kube_node_status_capacity_memory_bytes': NAMESPACE + '.node.memory_capacity',
             'kube_node_status_capacity_pods': NAMESPACE + '.node.pods_capacity',
             'kube_node_status_allocatable_cpu_cores': NAMESPACE + '.node.cpu_allocatable',
             'kube_node_status_allocatable_memory_bytes': NAMESPACE + '.node.memory_allocatable',
             'kube_node_status_allocatable_pods': NAMESPACE + '.node.pods_allocatable',
+            # deployments
+            'kube_deployment_status_replicas': NAMESPACE + '.deployment.replicas',
             'kube_deployment_status_replicas_available': NAMESPACE + '.deployment.replicas_available',
             'kube_deployment_status_replicas_unavailable': NAMESPACE + '.deployment.replicas_unavailable',
             'kube_deployment_status_replicas_updated': NAMESPACE + '.deployment.replicas_updated',
             'kube_deployment_spec_replicas': NAMESPACE + '.deployment.replicas_desired',
+            'kube_deployment_spec_paused': NAMESPACE + '.deployment.paused',
+            'kube_deployment_spec_strategy_rollingupdate_max_unavailable': NAMESPACE + '.deployment.rollingupdate.max_unavailable',
+            # daemonsets
+            'kube_daemonset_status_current_number_scheduled': NAMESPACE + '.daemonset.scheduled',
+            'kube_daemonset_status_number_misscheduled': NAMESPACE + '.daemonset.misscheduled',
+            'kube_daemonset_status_desired_number_scheduled': NAMESPACE + '.daemonset.desired',
+            # pods
+            'kube_pod_status_ready' : NAMESPACE + '.pod.ready',
+            'kube_pod_status_scheduled': NAMESPACE + '.pod.scheduled',
+            # containers
+            'kube_pod_container_status_ready': NAMESPACE + '.container.ready',
+            'kube_pod_container_status_running': NAMESPACE + '.container.running',
+            'kube_pod_container_status_terminated': NAMESPACE + '.container.terminated',
+            'kube_pod_container_status_waiting': NAMESPACE + '.container.waiting',
+            'kube_pod_container_status_restarts': NAMESPACE + '.container.restarts',
             'kube_pod_container_resource_requests_cpu_cores': NAMESPACE + '.container.cpu_requested',
             'kube_pod_container_resource_requests_memory_bytes': NAMESPACE + '.container.memory_requested',
             'kube_pod_container_resource_limits_cpu_cores': NAMESPACE + '.container.cpu_limit',
             'kube_pod_container_resource_limits_memory_bytes': NAMESPACE + '.container.memory_limit',
-            'kube_pod_container_status_restarts': NAMESPACE + '.container.restarts'
+            # replicasets
+            'kube_replicaset_status_replicas': NAMESPACE + '.replicaset.replicas',
+            'kube_replicaset_status_fully_labeled_replicas': NAMESPACE + '.replicaset.fully_labeled_replicas',
+            'kube_replicaset_status_ready_replicas': NAMESPACE + '.replicaset.replicas_ready',
+            'kube_replicaset_spec_replicas': NAMESPACE + '.replicaset.replicas_desired',
         }
 
     def process(self, message, **kwargs):
@@ -116,15 +138,6 @@ class KubeStateProcessor:
         else:
             self.log.error("Metric type %s unsupported for metric %s." % (message.type, message.name))
 
-    # TODO: implement kube_pod_container_status_ready
-    # TODO: implement kube_pod_container_status_running
-    # TODO: implement kube_pod_container_status_terminated
-    # TODO: implement kube_pod_container_status_waiting
-
-    # TODO: implement kube_pod_info
-    # TODO: implement kube_pod_status_ready
-    # TODO: implement kube_pod_status_scheduled
-
     # Labels attached: namespace, pod, phase=Pending|Running|Succeeded|Failed|Unknown
     # The phase gets not passed through; rather, it becomes the service check suffix.
     def kube_pod_status_phase(self, message, **kwargs):
@@ -181,3 +194,54 @@ class KubeStateProcessor:
                 self.gauge(metric_name, 1, tags)  # metric value is always one, value is on the tags
         else:
             self.log.error("Metric type %s unsupported for metric %s" % (message.type, message.name))
+
+    def kube_resourcequota(self, message, **kwargs):
+        """ Quota and current usage by resource type. """
+        metric_base_name = NAMESPACE + '.resourcequota.{}.{}'
+        suffixes = {'used': 'used', 'hard': 'limit'}
+        if message.type < len(METRIC_TYPES):
+            for metric in message.metric:
+                mtype = self._extract_label_value("type", metric.label)
+                resource = self._extract_label_value("resource", metric.label)
+                tags = [
+                    'namespace:%s' % self._extract_label_value("namespace", metric.label),
+                    'resourcequota:%s' % self._extract_label_value("resourcequota", metric.label)
+                ]
+                val = getattr(metric, METRIC_TYPES[message.type]).value
+                self.gauge(metric_base_name.format(resource, suffixes[mtype]), val, tags)
+        else:
+            self.log.error("Metric type %s unsupported for metric %s" % (message.type, message.name))
+
+    # TODO: uncomment when they are released
+    # def kube_limitrange(self, message, **kwargs):
+    #     """ Resource limits by consumer type. """
+    #     # type's cardinality is low: https://github.com/kubernetes/kubernetes/blob/v1.6.1/pkg/api/v1/types.go#L3872-L3879
+    #     # idem for resource: https://github.com/kubernetes/kubernetes/blob/v1.6.1/pkg/api/v1/types.go#L3342-L3352
+    #     # idem for constraint: https://github.com/kubernetes/kubernetes/blob/v1.6.1/pkg/api/v1/types.go#L3882-L3901
+    #     metric_base_name = NAMESPACE + '.limitrange.{}.{}'
+    #     constraints = {
+    #         'min': 'min',
+    #         'max': 'max',
+    #         'default': 'default',
+    #         'defaultRequest': 'default_request',
+    #         'maxLimitRequestRatio': 'max_limit_request_ratio',
+
+    #     }
+    #     if message.type < len(METRIC_TYPES):
+    #         for metric in message.metric:
+    #             constraint = self._extract_label_value("constraint", metric.label)
+    #             if constraint in constraints:
+    #                 constraint = constraints[constraint]
+    #             else:
+    #                 self.error("Constraint %s unsupported for metric %s" % (constraint, message.name))
+    #                 continue
+    #             resource = self._extract_label_value("resource", metric.label)
+    #             tags = [
+    #                 'namespace:%s' % self._extract_label_value("namespace", metric.label),
+    #                 'resourcequota:%s' % self._extract_label_value("resourcequota", metric.label),
+    #                 'consumer_type:%s' % self._extract_label_value("type", metric.label)
+    #             ]
+    #             val = getattr(metric, METRIC_TYPES[message.type]).value
+    #             self.gauge(metric_base_name.format(resource, constraint), val, tags)
+    #     else:
+    #         self.log.error("Metric type %s unsupported for metric %s" % (message.type, message.name))
