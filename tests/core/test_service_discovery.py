f6e78cc5609af581e2aa3ddc0eaba32afb212034
Xavier Vello <xavier.vello@datadoghq.com>
2017-05-10 16:00:14 +0200

    [docker]  use DockerUtil.image_name_extractor for all code paths (#3334)
    * use image_name_extractor for _get_checks_to_refresh too
    
    * use image_name_extractor for flares too
    
    * use image_name_extractor for unit test

--- tests/core/test_service_discovery.py
+++ tests/core/test_service_discovery.py
@@ -17,6 +17,7 @@ from utils.service_discovery.abstract_config_store import AbstractConfigStore, \
     _TemplateCache, CONFIG_FROM_KUBE, CONFIG_FROM_TEMPLATE, CONFIG_FROM_AUTOCONF
 from utils.service_discovery.sd_backend import get_sd_backend
 from utils.service_discovery.sd_docker_backend import SDDockerBackend, _SDDockerBackendConfigFetchState
+from utils.dockerutil import DockerUtil
 
 
 def clear_singletons(agentConfig):
@@ -587,7 +588,7 @@ class TestServiceDiscovery(unittest.TestCase):
             for c_ins, _, _, _, expected_ident in self.container_inspects:
                 sd_backend = get_sd_backend(agentConfig=self.auto_conf_agentConfig)
                 self.assertEqual(
-                    sd_backend.get_config_id(c_ins.get('Image'), c_ins.get('Config', {}).get('Labels', {})),
+                    sd_backend.get_config_id(DockerUtil().image_name_extractor(c_ins), c_ins.get('Config', {}).get('Labels', {})),
                     expected_ident)
                 clear_singletons(self.auto_conf_agentConfig)
 
