3e456e8eb16a985ce8387a8f6a5dd0462a93171c
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-07-03 15:34:07 +0200

    [dogstatsd6][info] add dsd6 stats to info command.

--- utils/http.py
+++ utils/http.py
@@ -12,3 +12,15 @@ def retrieve_json(url, timeout=DEFAULT_TIMEOUT, verify=True):
     r = requests.get(url, timeout=timeout, verify=verify)
     r.raise_for_status()
     return r.json()
+
+# Get expvar stats
+def get_expvar_stats(key, port):
+    try:
+        json = retrieve_json("http://127.0.0.1:{port}/debug/vars".format(port=port))
+    except requests.exceptions.RequestException as e:
+        raise e
+
+    if key:
+        return json.get(key)
+
+    return json
