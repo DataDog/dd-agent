5203a782f7cb90ad9ca409b17b32d2ef7ec0193b
Remi Hakim <remi@datadoghq.com>
2017-07-20 17:43:13 -0400

    [emitter] Split check runs from main agent payload

--- tests/core/test_emitter.py
+++ tests/core/test_emitter.py
@@ -23,7 +23,7 @@ class TestEmitter(unittest.TestCase):
         with open(FIXTURE_PATH + '/legacy_payload.json') as f:
             legacy_payload = json.load(f)
 
-        legacy_payload_split, metrics_payload = split_payload(dict(legacy_payload))
+        legacy_payload_split, metrics_payload, checkruns_payload = split_payload(dict(legacy_payload))
         series = metrics_payload['series']
         legacy_payload_split['metrics'] = []
 
@@ -43,6 +43,11 @@ class TestEmitter(unittest.TestCase):
             legacy_payload_split['metrics'].append(formatted_sample)
 
         self.assertEqual(legacy_payload, legacy_payload_split)
+
+        with open(FIXTURE_PATH + '/sc_payload.json') as f:
+            expected_sc_payload = json.load(f)
+
+        self.assertEqual(checkruns_payload, expected_sc_payload)
 
     def test_remove_control_chars(self):
         messages = [
