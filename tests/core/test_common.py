f16dee74134d2d146f82098b1b8655b757441df7
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2017-08-22 14:59:52 -0400

    [collector] Only add service check fields if non-null
    The `api/v1/check_run` endpoint doesn't accept payloads with
    a `message` value of `null`, so avoid that. Do the same thing on the
    `tags` and `hostname` fields as it makes sense to be consistent.

--- tests/core/test_common.py
+++ tests/core/test_common.py
@@ -126,7 +126,24 @@ class TestCore(unittest.TestCase):
         timestamp = time.time()
 
         check = AgentCheck('test', {}, {'checksd_hostname':'foo'})
-        check.service_check(check_name, status, tags, timestamp, host_name)
+        # No "message"/"tags" field
+        check.service_check(check_name, status, timestamp=timestamp, hostname=host_name)
+        self.assertEquals(len(check.service_checks), 1, check.service_checks)
+        val = check.get_service_checks()
+        self.assertEquals(len(val), 1)
+        check_run_id = val[0].get('id', None)
+        self.assertNotEquals(check_run_id, None)
+        self.assertEquals([{
+            'id': check_run_id,
+            'check': check_name,
+            'status': status,
+            'host_name': host_name,
+            'timestamp': timestamp,
+        }], val)
+        self.assertEquals(len(check.service_checks), 0, check.service_checks)
+
+        # With "message" field
+        check.service_check(check_name, status, tags, timestamp, host_name, message='foomessage')
         self.assertEquals(len(check.service_checks), 1, check.service_checks)
         val = check.get_service_checks()
         self.assertEquals(len(val), 1)
@@ -139,9 +156,10 @@ class TestCore(unittest.TestCase):
             'host_name': host_name,
             'tags': tags,
             'timestamp': timestamp,
-            'message': None,
+            'message': 'foomessage',
         }], val)
         self.assertEquals(len(check.service_checks), 0, check.service_checks)
+
 
     def test_no_proxy(self):
         """ Starting with Agent 5.0.0, there should always be a local forwarder
