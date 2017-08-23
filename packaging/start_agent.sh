12e0f0937b15bbbecd673a447f2c76e9bc3ed09e
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-08-23 19:03:48 +0200

    [systemd] do not source anything in starter script - use unit file.

--- packaging/start_agent.sh
+++ packaging/start_agent.sh
@@ -3,15 +3,11 @@
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
 
-AGENTSYSCONFIG="/etc/sysconfig/datadog-agent"
 PATH=/opt/datadog-agent/embedded/bin:/opt/datadog-agent/bin:$PATH
 
-# pull in sysconfig settings
-[ -f $AGENTSYSCONFIG ] && . $AGENTSYSCONFIG
-
 if [ "$DATADOG_ENABLED" = "no" ]; then
-        echo "Disabled via $AGENTSYSCONFIG. Exiting."
-        exit 0
+    echo "Disabled via $AGENTSYSCONFIG. Exiting."
+    exit 0
 fi
 
 exec /opt/datadog-agent/bin/supervisord -c /etc/dd-agent/supervisor.conf
