4c320ef1e313a78b93bdd6a13264badb53c4304d
Derek Brown <derek.brown@datadoghq.com>
2017-08-28 11:36:58 -0700

    Creates new PDH integration.  Uses the Windows PDH (Performance Counters) library to provide access to various metrics.

--- checks/libs/win/winpdh.py
+++ checks/libs/win/winpdh.py
@@ -1,37 +1,47 @@
 import time
 import win32pdh
 
-DATA_POINT_INTERVAL = 0.01
+DATA_POINT_INTERVAL = 0.10
 
 class WinPDHCounter(object):
-    def __init__(self, class_name, instance_name):
+    def __init__(self, class_name, instance_name, log):
         self._class_name = class_name
         self._instance_name = instance_name
+        self._is_single_instance = False
         self.hq = win32pdh.OpenQuery()
+        self.logger = log
+        self.counterdict = {}
+        counters, instances = win32pdh.EnumObjectItems(None, None, self._class_name, win32pdh.PERF_DETAIL_WIZARD)
+        if len(instances) > 0:
+            for inst in instances:
+                path = win32pdh.MakeCounterPath((None, self._class_name, inst, None, 0, self._instance_name))
+                self.counterdict[inst] = win32pdh.AddCounter(self.hq, path)
+                self.logger.debug("Path: %s" % str(path))
+        else:
+            path = win32pdh.MakeCounterPath((None, self._class_name, None, None, 0, self._instance_name))
+            self.logger.debug("Path: %s\n" % str(path))
+            try:
+                self.counterdict["__single_instance"] = win32pdh.AddCounter(self.hq, path)
+            except Exception as e:
+                self.logger.fatal("Failed to create counter.  No instances of %s\%s" % (
+                    self._class_name, self._instance_name))
+                raise
+            self._is_single_instance = True
 
     def __del__(self):
         if(self.hq):
             win32pdh.CloseQuery(self.hq)
 
-class WinPDHSingleCounter(WinPDHCounter):
-    def __init__(self, class_name, instance_name):
-        WinPDHCounter.__init__(self, class_name, instance_name)
-        path = win32pdh.MakeCounterPath((None, self._class_name, None, None, 0, self._instance_name))
-        self.counter_handle = win32pdh.AddCounter(self.hq, path)
+    def is_single_instance(self):
+        return self._is_single_instance
 
     def get_single_value(self):
-        win32pdh.CollectQueryData(self.hq)
-        t, val = win32pdh.GetFormattedCounterValue(self.counter_handle, win32pdh.PDH_FMT_LARGE)
-        return val
+        if not self.is_single_instance():
+            raise ValueError('counter is not single instance %s %s' % (
+                self._class_name, self._instance_name))
 
-class WinPDHMultiCounter(WinPDHCounter):
-    def __init__(self, class_name, instance_name):
-        WinPDHCounter.__init__(self, class_name, instance_name)
-        self.counterdict = {}
-        counters, instances = win32pdh.EnumObjectItems(None, None, self._class_name, win32pdh.PERF_DETAIL_WIZARD)
-        for inst in instances:
-            path = win32pdh.MakeCounterPath((None, self._class_name, inst, None, 0, self._instance_name))
-            self.counterdict[inst] = win32pdh.AddCounter(self.hq, path)
+        vals = self.get_all_values()
+        return vals["__single_instance"]
 
     def get_all_values(self):
         ret = {}
