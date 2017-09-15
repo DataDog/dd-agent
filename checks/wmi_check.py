f47807fe2634b18b9c63208420291706413c1671
Derek Brown <derek.brown@datadoghq.com>
2017-09-15 08:39:13 -0700

    Add checks/wmi_check back with correct import line

---
+++ checks/wmi_check.py
@@ -0,0 +1,13 @@
+# provided for backward compatibility.  wmi_check.py renamed to winwmi_check.py
+# to prevent collision with the actual wmi check; provide the redirect for
+# any agent check that uses the base library
+#
+# this file will be deprecated in Agent6
+from checks.winwmi_check import (
+    InvalidWMIQuery,
+    MissingTagBy,
+    TagQueryUniquenessFailure,
+    WinWMICheck,
+    to_time,
+    from_time,
+)
