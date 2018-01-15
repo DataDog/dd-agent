3cd24b5a0ab1f78986d693af760eacd486cc90bb
Pierre <mfpierre@gmail.com>
2018-01-15 15:08:05 -0500

    Move create check logic

--- checks/generic_prometheus_check.py
+++ checks/generic_prometheus_check.py
@@ -23,36 +23,11 @@ class GenericPrometheusCheck(AgentCheck):
         AgentCheck.__init__(self, name, init_config, agentConfig, instances)
         self.check_map = {}
         for instance in instances:
-            # Check mandatory settings
-            endpoint = instance.get("prometheus_url", None)
-            if endpoint is None:
-                raise CheckException("Unable to find prometheus URL in config file.")
-            namespace = instance.get("namespace", None)
-            if namespace is None:
-                raise CheckException("You have to define a namespace for each prometheus check")
-            # Instanciate check
-            check = PrometheusCheck(name, init_config, agentConfig, instance)
-            check.NAMESPACE = namespace
-            # metrics are preprocessed if no mapping
-            metrics_mapper = {}
-            for metric in instance.get("metrics", []):
-                if isinstance(metric, basestring):
-                    metrics_mapper[metric] = metric
-                else:
-                    metrics_mapper.update(metric)
-            check.metrics_mapper = metrics_mapper
-            check.labels_mapper = instance.get("labels_mapper", {})
-            check.label_joins = instance.get("label_joins", {})
-            check.exclude_labels = instance.get("exclude_labels", [])
-            check.label_to_hostname = instance.get("label_to_hostname", None)
-            check.health_service_check = instance.get("health_service_check", True)
-            # use the parent aggregator
-            check.aggregator = self.aggregator
-            self.check_map[instance["prometheus_url"]] = check
+            self.get_instance_check(instance)
 
     def check(self, instance):
         endpoint = instance["prometheus_url"]
-        check = self.check_map[endpoint]
+        check = self.get_instance_check(instance)
         if not check.metrics_mapper:
             raise CheckException("You have to collect at least one metric from the endpoint: " + endpoint)
         check.process(
@@ -61,3 +36,37 @@ class GenericPrometheusCheck(AgentCheck):
             instance=instance,
             ignore_unmapped=True
         )
+
+    def get_instance_check(self, instance):
+        endpoint = instance.get("prometheus_url", "")
+        # If we already created the PrometheusCheck, return it
+        if endpoint in self.check_map:
+            return self.check_map[endpoint]
+
+        # Otherwise we create the PrometheusCheck
+        if endpoint == "":
+            raise CheckException("Unable to find prometheus URL in config file.")
+        namespace = instance.get("namespace", "")
+        if namespace == "":
+            raise CheckException("You have to define a namespace for each prometheus check")
+        # Instanciate check
+        check = PrometheusCheck(self.name, self.init_config, self.agentConfig, instance)
+        check.NAMESPACE = namespace
+        # Metrics are preprocessed if no mapping
+        metrics_mapper = {}
+        for metric in instance.get("metrics", []):
+            if isinstance(metric, basestring):
+                metrics_mapper[metric] = metric
+            else:
+                metrics_mapper.update(metric)
+        check.metrics_mapper = metrics_mapper
+        check.labels_mapper = instance.get("labels_mapper", {})
+        check.label_joins = instance.get("label_joins", {})
+        check.exclude_labels = instance.get("exclude_labels", [])
+        check.label_to_hostname = instance.get("label_to_hostname", None)
+        check.health_service_check = instance.get("health_service_check", True)
+        # Use the parent aggregator
+        check.aggregator = self.aggregator
+        self.check_map[instance["prometheus_url"]] = check
+
+        return check
