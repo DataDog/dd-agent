1d0dc34ce7dd3f114ddeed99540b655d97ee879b
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-03-19 11:27:53 -0400

    Bump JMXFetch version to 0.19.0 (#3711)

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ from utils.windows_configuration import get_registry_conf, get_windows_sdk_check
 
 # CONSTANTS
 AGENT_VERSION = "5.23.0"
-JMX_VERSION = "0.18.1"
+JMX_VERSION = "0.19.0"
 DATADOG_CONF = "datadog.conf"
 UNIX_CONFIG_PATH = '/etc/dd-agent'
 MAC_CONFIG_PATH = '/opt/datadog-agent/etc'
