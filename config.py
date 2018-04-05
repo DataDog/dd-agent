00a4ca846946f907ffcab105dff042e768ec6ccd
Maxime Mouial <maxime.mouial@gmail.com>
2018-04-05 14:53:01 -0400

    Bump agent version to the next version

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ from utils.windows_configuration import get_registry_conf, get_windows_sdk_check
 
 
 # CONSTANTS
-AGENT_VERSION = "5.23.0"
+AGENT_VERSION = "5.24.0"
 JMX_VERSION = "0.19.0"
 DATADOG_CONF = "datadog.conf"
 UNIX_CONFIG_PATH = '/etc/dd-agent'
