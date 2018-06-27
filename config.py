e9e7fcdea8e4fae2f1e88a1508900778701ee3b0
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2018-06-27 18:35:15 -0400

    [config] bump agent version to 5.25.1

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ from utils.windows_configuration import get_registry_conf, get_windows_sdk_check
 
 
 # CONSTANTS
-AGENT_VERSION = "5.25.0"
+AGENT_VERSION = "5.25.1"
 JMX_VERSION = "0.20.1"
 DATADOG_CONF = "datadog.conf"
 UNIX_CONFIG_PATH = '/etc/dd-agent'
