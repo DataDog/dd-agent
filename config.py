0548b6e71c00c87c5bf8543f3548483b08c32f70
Maxime Mouial <maxime.mouial@gmail.com>
2018-07-13 13:58:45 -0400

    Bumping AGENT_VERSION

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ from utils.windows_configuration import get_registry_conf, get_windows_sdk_check
 
 
 # CONSTANTS
-AGENT_VERSION = "5.25.2"
+AGENT_VERSION = "5.25.3"
 JMX_VERSION = "0.20.1"
 DATADOG_CONF = "datadog.conf"
 UNIX_CONFIG_PATH = '/etc/dd-agent'
