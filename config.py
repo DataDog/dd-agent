dc3d4689b0fb4cbe73740da274f9d80deebdd92b
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2018-09-18 12:53:14 +0200

    [config] bumping nightlies to 5.28.0

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ from utils.windows_configuration import get_registry_conf, get_windows_sdk_check
 
 
 # CONSTANTS
-AGENT_VERSION = "5.27.0"
+AGENT_VERSION = "5.28.0"
 JMX_VERSION = "0.20.2"
 DATADOG_CONF = "datadog.conf"
 UNIX_CONFIG_PATH = '/etc/dd-agent'
