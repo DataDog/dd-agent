f64240d971ea4c2b1ba98eaa6293dcf3bb2c5522
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2019-05-27 15:50:30 +0200

    [jmxfetch] Bump to 0.26.2

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ from utils.windows_configuration import get_registry_conf, get_windows_sdk_check
 
 # CONSTANTS
 AGENT_VERSION = "5.32.3"
-JMX_VERSION = "0.26.1"
+JMX_VERSION = "0.26.2"
 DATADOG_CONF = "datadog.conf"
 UNIX_CONFIG_PATH = '/etc/dd-agent'
 MAC_CONFIG_PATH = '/opt/datadog-agent/etc'
