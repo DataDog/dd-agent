311e0f35e1821a87c233a4b6e322e0d034aeaf6f
Albert Vaca <albert.vaca@datadoghq.com>
2019-11-08 12:31:09 +0100

    Bump dev version

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ from utils.windows_configuration import get_registry_conf, get_windows_sdk_check
 
 
 # CONSTANTS
-AGENT_VERSION = "5.32.5"
+AGENT_VERSION = "5.32.6"
 JMX_VERSION = "0.26.3"
 DATADOG_CONF = "datadog.conf"
 UNIX_CONFIG_PATH = '/etc/dd-agent'
