9212fb9b6375778c24d68bb81520f736d8dbd185
fazalpsinfo-cmyk <fazalpsinfo@gmail.com>
2026-07-20 14:20:43 +0530

    fix: replace deprecated collections.MutableMapping with collections.abc.MutableMapping

--- checks/collector.py
+++ checks/collector.py
@@ -4,6 +4,7 @@
 
 # stdlib
 import collections
+import collections.abc
 import locale
 import logging
 import pprint
@@ -63,7 +64,7 @@ def a7_compatible_to_int(status):
     return 0
 
 
-class AgentPayload(collections.MutableMapping):
+class AgentPayload(collections.abc.MutableMapping):
     """
     AgentPayload offers a single payload interface but manages two payloads:
     * A metadata payload
